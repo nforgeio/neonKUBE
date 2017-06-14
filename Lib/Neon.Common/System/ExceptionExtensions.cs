5d22cb3743c795550d68abca4f9f16c1a98276c5
Jeff Lill <jeff@lilltek.com>
2017-06-14 10:30:51 -0700

    Added Exception.GetTrigger() and TriggeredBy() extension methods.

---
+++ Lib/Neon.Common/System/ExceptionExtensions.cs
@@ -0,0 +1,85 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    ExceptionExtensions.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2017 by NeonForge, LLC.  All rights reserved.
+
+using System;
+
+namespace System.IO
+{
+    /// <summary>
+    /// <see cref="Exception"/> extensions.
+    /// </summary>
+    public static class ExceptionExtensions
+    {
+        /// <summary>
+        /// Determines whether the exception has a specified type or was triggered by
+        /// an underlying exception of the type.  This is useful for checking whether
+        /// an exception has a specific inner exception or whether an <see cref="AggregateException"/>
+        /// was triggered with a specific exception type.
+        /// </summary>
+        /// <typeparam name="T">The target exception type.</typeparam>
+        /// <param name="e">The exception being tested.</param>
+        /// <returns><c>true</c> if the exception was triggered by an exception of type <typeparamref name="T"/>.</returns>
+        public static bool TriggeredBy<T>(this Exception e)
+            where T : Exception
+        {
+            return GetTrigger<T>(e) != null;
+        }
+
+        /// <summary>
+        /// Searches an exception to for an underlying exception of a specific type.  This is useful for 
+        /// checking whether an exception has a specific inner exception or whether an <see cref="AggregateException"/>
+        /// was triggered with a specific exception type.
+        /// </summary>
+        /// <typeparam name="T">The target exception type.</typeparam>
+        /// <param name="e">The exception being tested.</param>
+        /// <returns>The underlying exception or <c>null</c>.</returns>
+        public static T GetTrigger<T>(this Exception e)
+            where T : Exception
+        {
+            if (e == null)
+            {
+                return null;
+            }
+
+            var value = e as T;
+
+            if (value != null)
+            {
+                return value;
+            }
+
+            var aggregate = e as AggregateException;
+
+            if (aggregate != null)
+            {
+                foreach (var inner in aggregate.InnerExceptions)
+                {
+                    if (inner != null)
+                    {
+                        var found = inner.GetTrigger<T>();
+
+                        if (found != null)
+                        {
+                            return found;
+                        }
+                    }
+                }
+
+                return null;
+            }
+            else
+            {
+                if (e.InnerException != null)
+                {
+                    return e.InnerException.GetTrigger<T>();
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ Test/Test.Neon.Common.shared/System/Test_Exception.cs
@@ -0,0 +1,57 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Test_Exception.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2017 by NeonForge, LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Neon.Common;
+using Neon.Retry;
+
+using Xunit;
+
+namespace TestCommon
+{
+    public class Test_Exception
+    {
+        [Fact]
+        public void TriggeredBy()
+        {
+            Assert.False(((Exception)null).TriggeredBy<Exception>());
+
+            Assert.True(new KeyNotFoundException().TriggeredBy<KeyNotFoundException>());
+            Assert.False(new KeyNotFoundException().TriggeredBy<IndexOutOfRangeException>());
+
+            Assert.True(new IndexOutOfRangeException("message", new KeyNotFoundException("key")).TriggeredBy<KeyNotFoundException>());
+            Assert.True(new IndexOutOfRangeException("message", new IndexOutOfRangeException("message", new KeyNotFoundException("key"))).TriggeredBy<KeyNotFoundException>());
+            Assert.False(new IndexOutOfRangeException("message", new KeyNotFoundException("key")).TriggeredBy<FormatException>());
+
+            Assert.False(new AggregateException().TriggeredBy<KeyNotFoundException>());
+            Assert.True(new AggregateException(new KeyNotFoundException("key")).TriggeredBy<KeyNotFoundException>());
+            Assert.True(new AggregateException(new IndexOutOfRangeException("message", new KeyNotFoundException("key"))).TriggeredBy<KeyNotFoundException>());
+        }
+
+        [Fact]
+        public void GetTrigger()
+        {
+            Assert.Null (((Exception)null).GetTrigger<Exception>());
+
+            Assert.NotNull(new KeyNotFoundException().GetTrigger<KeyNotFoundException>());
+            Assert.Null(new KeyNotFoundException().GetTrigger<IndexOutOfRangeException>());
+
+            Assert.Equal("key", new IndexOutOfRangeException("message", new KeyNotFoundException("key")).GetTrigger<KeyNotFoundException>().Message);
+            Assert.Equal("key", new IndexOutOfRangeException("message", new IndexOutOfRangeException("message", new KeyNotFoundException("key"))).GetTrigger<KeyNotFoundException>().Message);
+            Assert.Null(new IndexOutOfRangeException("message", new KeyNotFoundException("key")).GetTrigger<FormatException>());
+
+            Assert.Null(new AggregateException().GetTrigger<KeyNotFoundException>());
+            Assert.Equal("key", new AggregateException(new KeyNotFoundException("key")).GetTrigger<KeyNotFoundException>().Message);
+            Assert.Equal("key", new AggregateException(new IndexOutOfRangeException("message", new KeyNotFoundException("key"))).GetTrigger<KeyNotFoundException>().Message);
+        }
+    }
+}
