315fc14a4e0e5ede39791ed71a8bd1f734fd8455
Jeff Lill <jeff@lilltek.com>
2017-07-23 10:25:37 -0700

    Fixed Nuget public publish for versions with REVISION=0

--- ToolBin/nuget-neonforge-public.ps1
+++ ToolBin/nuget-neonforge-public.ps1
@@ -12,8 +12,22 @@ function Publish
 	text pack-version "$env:NF_ROOT\nuget-version.txt" "$env:NF_ROOT\Lib\$project\$project.csproj"
 	dotnet pack "$env:NF_ROOT\Lib\$project\$project.csproj" -c Release -o "$env:NF_build\nuget"
 
+	# It looks like [dotnet pack] doesn't include a zero revision number when
+	# nameing the output file.  So {Neon.Common] built as version [0.0.5.0] will
+	# generate [Neon.Common.0.0.5.nupkg] not [Neon.Common.0.0.5.0.nupkg].
+	#
+	# We need to strip the last ".0" off the version string in this case,
+	# so [nuget push] will be able to find the file.
+
 	$version = Get-Content "$env:NF_ROOT\nuget-version.txt" -First 1
-	nuget push "$env:NF_BUILD\nuget\$project.$version.nupkg" $env:NF_NUGET_API_KEY
+	$fields  = [array]$version.Split('.')
+
+	if ($fields.Length -eq 4 -and $version.EndsWith(".0"))
+	{
+		$version = $version.Substring(0, $version.Length - 2)
+	}
+
+	nuget push -Source nuget.org "$env:NF_BUILD\nuget\$project.$version.nupkg" $env:NF_NUGET_API_KEY
 }
 
 Publish Neon.Cluster
