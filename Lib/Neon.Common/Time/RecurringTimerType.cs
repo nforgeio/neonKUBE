85174d04cd0f4196c74475a2a48285a5d8372cc1
Jeff Lill <jeff@lilltek.com>
2017-07-28 09:39:51 -0700

    RecurringTimer: Imported from LillTek.

---
+++ Lib/Neon.Common/Time/RecurringTimerType.cs
@@ -0,0 +1,50 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:        RecurringTimerType.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:   Copyright (c) 2016-2017 by NeonForge, LLC.  All rights reserved.
+
+using System;
+using System.Text;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Neon.Time
+{
+    /// <summary>
+    /// Enumerates the possible <see cref="RecurringTimer" /> types.
+    /// </summary>
+    public enum RecurringTimerType
+    {
+        /// <summary>
+        /// The timer never fires.
+        /// </summary>
+        Disabled,
+
+        /// <summary>
+        /// The timer will be fired once per minute. 
+        /// </summary>
+        Minute,
+
+        /// <summary>
+        /// The timer will be fired once every 15 minutes.
+        /// </summary>
+        QuarterHour,
+
+        /// <summary>
+        /// The timer will be fired once per hour.
+        /// </summary>
+        Hourly,
+
+        /// <summary>
+        /// The timer will be fired once per day.
+        /// </summary>
+        Daily,
+
+        /// <summary>
+        /// The timer is fired on a specified interval rather than a 
+        /// specific period offset.  This is similar to how <see cref="PolledTimer" />
+        /// works.
+        /// </summary>
+        Interval
+    }
+}
---
+++ Test/Test.Neon.Common.shared/Time/Test_RecurringTimer.cs
@@ -0,0 +1,166 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Test_RecurringTimer.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2017 by NeonForge, LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Neon.Common;
+using Neon.Time;
+
+using Xunit;
+
+// $todo(jeff.lill): Need to add tests for MINUTE and QUARTERHOUR.
+
+namespace TestCommon
+{
+    public class Test_RecurringTimer
+    {
+        [Fact]
+        public void RecurringTimer_Disabled()
+        {
+            RecurringTimer timer;
+
+            timer = new RecurringTimer(RecurringTimerType.Disabled, TimeSpan.FromSeconds(1));
+            Assert.False(timer.HasFired(new DateTime(2010, 10, 23, 10, 10, 0)));   // Should never fire when disabled
+            Assert.False(timer.HasFired(new DateTime(2010, 10, 24, 9, 0, 0)));
+            Assert.False(timer.HasFired(new DateTime(2010, 10, 24, 10, 1, 0)));
+            Assert.False(timer.HasFired(new DateTime(2010, 10, 24, 10, 1, 0)));
+            Assert.False(timer.HasFired(new DateTime(2010, 10, 25, 10, 1, 0)));
+
+            Assert.Equal("Disabled", timer.ToString());
+        }
+
+        [Fact]
+        public void RecurringTimer_Hourly()
+        {
+            RecurringTimer timer;
+
+            timer = new RecurringTimer(RecurringTimerType.Hourly, TimeSpan.FromMinutes(4));
+
+            Assert.False(timer.HasFired(new DateTime(2011, 08, 20, 10, 0, 0)));    // Never fires on the first poll
+            Assert.True(timer.HasFired(new DateTime(2011, 08, 20, 10, 5, 0)));
+            Assert.False(timer.HasFired(new DateTime(2011, 08, 20, 11, 1, 0)));    // Still before offset
+            Assert.False(timer.HasFired(new DateTime(2011, 08, 20, 11, 3, 0)));    // Still before offset
+            Assert.True(timer.HasFired(new DateTime(2011, 08, 20, 11, 4, 0)));     // Right at offset
+            Assert.False(timer.HasFired(new DateTime(2011, 08, 20, 11, 4, 0)));    // Doesn't fire until the next hour
+            Assert.False(timer.HasFired(new DateTime(2011, 08, 20, 11, 15, 0)));
+            Assert.False(timer.HasFired(new DateTime(2011, 08, 20, 11, 30, 0)));
+            Assert.False(timer.HasFired(new DateTime(2011, 08, 20, 11, 55, 0)));
+            Assert.False(timer.HasFired(new DateTime(2011, 08, 20, 12, 0, 0)));
+            Assert.True(timer.HasFired(new DateTime(2011, 08, 20, 12, 5, 0)));     // Just past the next firing time
+
+            Assert.Equal("Hourly:04:00", timer.ToString());
+
+            timer = new RecurringTimer("Hourly:10:11");
+            Assert.Equal("Hourly:10:11", timer.ToString());
+        }
+
+        [Fact]
+        public void RecurringTimer_Daily()
+        {
+            RecurringTimer timer;
+
+            timer = new RecurringTimer(new TimeOfDay("10:00:00"));
+            Assert.False(timer.HasFired(new DateTime(2011, 10, 23, 10, 10, 0)));   // Verify that we don't fire until we see the transition
+            Assert.False(timer.HasFired(new DateTime(2011, 10, 24, 9, 0, 0)));     // Still before the scheduled time
+            Assert.True(timer.HasFired(new DateTime(2011, 10, 24, 10, 1, 0)));     // Should have seen the transition
+            Assert.False(timer.HasFired(new DateTime(2011, 10, 24, 10, 1, 0)));    // Should be false now because we already handled this time
+            Assert.True(timer.HasFired(new DateTime(2011, 10, 25, 10, 1, 0)));     // Should fire for the next day
+
+            timer = new RecurringTimer(RecurringTimerType.Daily, new TimeSpan(10, 0, 0));
+            Assert.False(timer.HasFired(new DateTime(2011, 10, 23, 10, 10, 0)));   // Verify that we don't fire until we see the transition
+            Assert.False(timer.HasFired(new DateTime(2011, 10, 24, 9, 0, 0)));     // Still before the scheduled time
+            Assert.True(timer.HasFired(new DateTime(2011, 10, 24, 10, 1, 0)));     // Should have seen the transition
+            Assert.False(timer.HasFired(new DateTime(2011, 10, 24, 10, 1, 0)));    // Should be false now because we already handled this time
+            Assert.True(timer.HasFired(new DateTime(2011, 10, 25, 10, 1, 0)));     // Should fire for the next day
+
+            Assert.Equal("Daily:10:00:00", timer.ToString());
+
+            timer = new RecurringTimer("Daily:10:11:12");
+            Assert.Equal("Daily:10:11:12", timer.ToString());
+        }
+
+        [Fact]
+        public void RecurringTimer_Interval()
+        {
+            RecurringTimer timer;
+
+            timer = new RecurringTimer(RecurringTimerType.Interval, TimeSpan.FromSeconds(10));
+            timer.Start(new DateTime(2011, 8, 26, 0, 0, 0));
+            Assert.False(timer.HasFired(new DateTime(2011, 8, 26, 0, 0, 0)));
+            Assert.False(timer.HasFired(new DateTime(2011, 8, 26, 0, 0, 9)));
+            Assert.True(timer.HasFired(new DateTime(2011, 8, 26, 0, 0, 10)));
+            Assert.False(timer.HasFired(new DateTime(2011, 8, 26, 0, 0, 19)));
+            Assert.True(timer.HasFired(new DateTime(2011, 8, 26, 0, 0, 21)));
+
+            Assert.Equal("Interval:00:00:10", timer.ToString());
+
+            timer = new RecurringTimer("Interval:48:11:12");
+            Assert.Equal("Interval:48:11:12", timer.ToString());
+        }
+
+        [Fact]
+        public void RecurringTimer_Parse()
+        {
+            RecurringTimer timer;
+
+            timer = new RecurringTimer("Disabled");
+            Assert.Equal(RecurringTimerType.Disabled, timer.Type);
+
+            timer = new RecurringTimer("Minute");
+            Assert.Equal(RecurringTimerType.Minute, timer.Type);
+            Assert.Equal(TimeSpan.Zero, timer.TimeOffset);
+
+            timer = new RecurringTimer("QuarterHour");
+            Assert.Equal(RecurringTimerType.QuarterHour, timer.Type);
+            Assert.Equal(TimeSpan.Zero, timer.TimeOffset);
+
+            timer = new RecurringTimer("QuarterHour:10");
+            Assert.Equal(RecurringTimerType.QuarterHour, timer.Type);
+            Assert.Equal(TimeSpan.FromMinutes(10), timer.TimeOffset);
+
+            timer = new RecurringTimer("QuarterHour:10:11");
+            Assert.Equal(RecurringTimerType.QuarterHour, timer.Type);
+            Assert.Equal(TimeSpan.FromMinutes(10) + TimeSpan.FromSeconds(11), timer.TimeOffset);
+
+            timer = new RecurringTimer("Hourly");
+            Assert.Equal(RecurringTimerType.Hourly, timer.Type);
+            Assert.Equal(TimeSpan.Zero, timer.TimeOffset);
+
+            timer = new RecurringTimer("Hourly:10");
+            Assert.Equal(RecurringTimerType.Hourly, timer.Type);
+            Assert.Equal(TimeSpan.FromMinutes(10), timer.TimeOffset);
+
+            timer = new RecurringTimer("Hourly:10:11");
+            Assert.Equal(RecurringTimerType.Hourly, timer.Type);
+            Assert.Equal(TimeSpan.FromMinutes(10) + TimeSpan.FromSeconds(11), timer.TimeOffset);
+
+            timer = new RecurringTimer("Daily");
+            Assert.Equal(RecurringTimerType.Daily, timer.Type);
+            Assert.Equal("00:00:00", timer.TimeOffset.ToString());
+
+            timer = new RecurringTimer("Daily:10:11");
+            Assert.Equal(RecurringTimerType.Daily, timer.Type);
+            Assert.Equal(TimeSpan.FromHours(10) + TimeSpan.FromMinutes(11), timer.TimeOffset);
+
+            timer = new RecurringTimer("Daily:10:11:12");
+            Assert.Equal(RecurringTimerType.Daily, timer.Type);
+            Assert.Equal(TimeSpan.FromHours(10) + TimeSpan.FromMinutes(11) + TimeSpan.FromSeconds(12), timer.TimeOffset);
+
+            timer = new RecurringTimer("Interval:10:11:12");
+            Assert.Equal(RecurringTimerType.Interval, timer.Type);
+            Assert.Equal(TimeSpan.FromHours(10) + TimeSpan.FromMinutes(11) + TimeSpan.FromSeconds(12), timer.TimeOffset);
+
+            timer = new RecurringTimer("Interval:48:11:12");
+            Assert.Equal(RecurringTimerType.Interval, timer.Type);
+            Assert.Equal(TimeSpan.FromHours(48) + TimeSpan.FromMinutes(11) + TimeSpan.FromSeconds(12), timer.TimeOffset);
+        }
+    }
+}
