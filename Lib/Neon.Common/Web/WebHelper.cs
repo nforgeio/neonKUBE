a865359766c93c58c745156a2ac182278f93284f
Jeff Lill <jeff@lilltek.com>
2017-08-15 07:59:06 -0700

    WebHelper: Temporarily added to Neon.Common.

---
+++ Lib/Neon.Common/Web/WebHelper.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    WebHelper.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2017 by NeonForge, LLC.  All rights reserved.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Threading;
+
+using Neon.Common;
+using Neon.Diagnostics;
+
+namespace Neon.AspNetCore
+{
+    /// <summary>
+    /// Utility methods for <b>AspNetCore</b> applications.
+    /// </summary>
+    public static class WebHelper
+    {
+        /// <summary>
+        /// Performs common web app initialization including setting the correct
+        /// working directory.  Call this method from your application's main
+        /// entrypoint method.
+        /// </summary>
+        public static void Initialize()
+        {
+            // AspNetCore expects the current working directory to be where
+            // the main executable is located.
+
+            Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+        }
+    }
+}
