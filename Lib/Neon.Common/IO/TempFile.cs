d56dea643c976297be6159877cdd92732887207a
Jeff Lill <jeff@lilltek.com>
2017-11-17 18:26:59 -0800

    Added: TempFile class.

---
+++ Lib/Neon.Common/IO/TempFile.cs
@@ -0,0 +1,93 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    PreprocessReader.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2017 by NeonForge, LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+using Neon.Common;
+
+namespace Neon.IO
+{
+    /// <summary>
+    /// Generates a globally unique temporary file name and then 
+    /// ensures that the file is removed when the instance is 
+    /// disposed.
+    /// </summary>
+    public sealed class TempFile : IDisposable
+    {
+        private bool isDisposed = false;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="suffix">
+        /// Optioonally specifies the file suffix (including the leading period) to be
+        /// appended to the generated file name.  This defaults to <b>.tmp</b>.
+        /// </param>
+        /// <param name="folder">
+        /// Optionaly specifies the target folder.  This defaults to the standard
+        /// temporary directory for the current user.
+        /// </param>
+        public TempFile(string suffix = null, string folder = null)
+        {
+            if (suffix == null)
+            {
+                suffix = ".tmp";
+            }
+            else if (suffix.Length > 0 && !suffix.StartsWith("."))
+            {
+                throw new ArgumentException($"Non-empty [{nameof(suffix)}] arguments must be prefixed with a period.");
+            }
+
+            if (string.IsNullOrEmpty(folder))
+            {
+                folder = System.IO.Path.GetTempPath();
+            }
+
+            Directory.CreateDirectory(folder);
+
+            Path = System.IO.Path.GetFullPath(System.IO.Path.Combine(folder, Guid.NewGuid().ToString("D") + suffix));
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+
+            try
+            {
+                if (File.Exists(Path))
+                {
+                    File.Delete(Path);
+                }
+            }
+            catch
+            {
+                // We're going to ignore any errors deleting the file.
+            }
+
+            isDisposed = true;
+
+            GC.SuppressFinalize(this);
+        }
+
+        /// <summary>
+        /// Returns the fully qualified path to the temporary file.
+        /// </summary>
+        public string Path { get; private set; }
+    }
+}
---
+++ Test/Test.Neon.Common.shared/IO/Test_TempFile.cs
@@ -0,0 +1,113 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Test_TempFile.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2017 by NeonForge, LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Neon.Common;
+using Neon.IO;
+
+using Xunit;
+
+namespace TestCommon
+{
+    public class Test_TempFile
+    {
+        [Fact]
+        public void Basic()
+        {
+            var tempFile = new TempFile();
+
+            Assert.False(File.Exists(tempFile.Path));
+            Assert.StartsWith(Path.GetTempPath(), tempFile.Path);
+            Assert.EndsWith( ".tmp", tempFile.Path);
+
+            File.WriteAllText(tempFile.Path, "Hello World!");
+
+            Assert.True(File.Exists(tempFile.Path));
+
+            tempFile.Dispose();
+
+            Assert.False(File.Exists(tempFile.Path));
+
+            // Make sure that there's no exception if we dispose more than once.
+
+            tempFile.Dispose();
+        }
+
+        [Fact]
+        public void Unique()
+        {
+            var tempFile1 = new TempFile();
+            var tempFile2 = new TempFile();
+
+            Assert.NotEqual(tempFile1.Path, tempFile2.Path);
+        }
+
+        [Fact]
+        public void DisposeWhenNone()
+        {
+            var tempFile = new TempFile();
+
+            // Make sure that there's no exception when there's no file.
+
+            tempFile.Dispose();
+        }
+
+        [Fact]
+        public void DisposeWhenLocked()
+        {
+            var tempFile = new TempFile();
+
+            // Make sure that there's no exception when the file 
+            // is open.
+
+            using (var stream = new FileStream(tempFile.Path, FileMode.Create, FileAccess.ReadWrite))
+            {
+                stream.Write(Encoding.UTF8.GetBytes("Hello World!"));
+
+                // Make sure that there's no exception when the file 
+                // is open.
+
+                tempFile.Dispose();
+            }
+
+            // We can delete it now that the stream is closed.
+
+            File.Delete(tempFile.Path);
+        }
+
+        [Fact]
+        public void CustomSuffix()
+        {
+            var tempFile = new TempFile(suffix: ".txt");
+
+            Assert.StartsWith(Path.GetTempPath(), tempFile.Path);
+            Assert.EndsWith(".txt", tempFile.Path);
+        }
+
+        [Fact]
+        public void CustomFolder()
+        {
+            var tempFile = new TempFile(folder: "C:\\temp");
+
+            Assert.StartsWith("C:\\temp", tempFile.Path);
+            Assert.EndsWith(".tmp", tempFile.Path);
+        }
+
+        [Fact]
+        public void CustomFolderAndSuffix()
+        {
+            var tempFile = new TempFile(suffix: ".txt", folder: "C:\\temp");
+
+            Assert.StartsWith("C:\\temp", tempFile.Path);
+            Assert.EndsWith(".txt", tempFile.Path);
+        }
+    }
+}
