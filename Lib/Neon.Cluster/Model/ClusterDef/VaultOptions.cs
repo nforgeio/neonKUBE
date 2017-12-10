f355ffd193b70a3716ef1c7aa02b5de7fb09bc94
Jeff Lill <jeff@lilltek.com>
2017-12-10 14:19:16 -0800

    Upgrade: Vault 0.8.3 --> 0.9.0

--- Lib/Neon.Cluster/Model/ClusterDef/VaultOptions.cs
+++ Lib/Neon.Cluster/Model/ClusterDef/VaultOptions.cs
@@ -29,7 +29,7 @@ namespace Neon.Cluster
     /// </summary>
     public class VaultOptions
     {
-        private const string    defaultVersion      = "0.8.3";
+        private const string    defaultVersion      = "0.9.0";
         private const int       defaultKeyCount     = 1;
         private const int       defaultKeyThreshold = 1;
         private const string    defaultLease        = "0";
