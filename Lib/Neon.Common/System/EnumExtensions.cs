892715064928aa021a1f651f29761e90ffd1e1d3
Jeff Lill <jeff@lilltek.com>
2018-01-26 08:56:34 -0800

    [Neon.Common]: Implemented [Enum.ToMemberStringe()] that honors any attached [EnumMember] attributes.  The built-in [Enum.ToString()] method doesn't do this.

---
+++ Lib/Neon.Common/System/EnumExtensions.cs
@@ -0,0 +1,45 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    EnumExtensions.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2018 by neonFORGE, LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    /// <summary>
+    /// <see cref="Enum"/> extensions.
+    /// </summary>
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Converts an enumeration value into a string, using the <see cref="EnumMemberAttribute"/>
+        /// value if one was specified for the value in the enumeration type definition otherwise
+        /// the default enumeration value name will be returned.
+        /// </summary>
+        /// <param name="value">The enumeration value to be converted.</param>
+        /// <returns>The member or default string value.</returns>
+        /// <remarks>
+        /// This is useful because <see cref="Enum.ToString()"/> ignores any <see cref="EnumMemberAttribute"/>
+        /// attributes.
+        /// </remarks>
+        public static string ToMemberString(this Enum value)
+        {
+            var type       = value.GetType();
+            var info       = type.GetField(value.ToString());
+            var attributes = (EnumMemberAttribute[])(info.GetCustomAttributes(typeof(EnumMemberAttribute), false));
+
+            if (attributes.Length > 0)
+                return attributes.First().Value;
+            else
+                return value.ToString();
+        }
+    }
+}
---
+++ Test/Test.Neon.Common.shared/System/Test_Enum.cs
@@ -0,0 +1,39 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Test_Enum.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2018 by neonFORGE, LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Neon.Common;
+using Neon.Retry;
+
+using Xunit;
+
+namespace TestCommon
+{
+    public class Test_Enum
+    {
+        private enum Foo
+        {
+            Bar,
+
+            [EnumMember(Value = "FOO-BAR")]
+            FooBar
+        };
+
+        [Fact]
+        public void ToMemberString()
+        {
+            Assert.Equal("Bar", Foo.Bar.ToMemberString());          // [Foo.Bar] doesn't have an [EnumMember] attribute so the enum identifer will be returned.
+            Assert.Equal("FOO-BAR", Foo.FooBar.ToMemberString());   // [Foo.FooBar] has an [EnumMember] attribute so that value will be returned.
+        }
+    }
+}
