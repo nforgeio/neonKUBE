fb58cb887e6825e0d3e7539c96288a5f5af944cc
Jeff Lill <jeff@lilltek.com>
2018-02-14 15:58:43 -0800

    [neon-volume]: Stubbed GOLANG project.

---
+++ Tools/neon-volume/Program.cs
@@ -0,0 +1,29 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Program.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2018 by neonFORGE, LLC.  All rights reserved.
+//
+// This project is simply used to hold the [neon-volume] GOLANG source files and
+// project post-build events are used to actually build the golang SOURCE using
+// the [neoncluster/golang] container.
+//
+// The compiled [neon-volume] binary will be written to the [bin] project subfolder.
+//
+// NOTE:
+// -----
+// You should add all of the GOLANG source files to the project and set the
+// build action to CONTENT.  This ensures that the GO project is rebuilt
+// whenever any of the source files are changed.
+
+using System;
+
+namespace neon_volume
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This tool is actually just used to build the [neon-volume] GOLANG Docker plugin.");
+        }
+    }
+}
---
+++ Tools/neon-volume/main.go
@@ -0,0 +1,7 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
