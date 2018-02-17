9c8d87a2a141a80f8070fb0531bbdcdbe7af5b26
Jeff Lill <jeff@lilltek.com>
2018-02-16 22:56:23 -0800

    [neon-volume-plugin]: Mostly working now.  Needs more testing.

--- Tools/neon-volume-plugin/main.go
+++ Tools/neon-volume-plugin/main.go
@@ -16,15 +16,81 @@ type neonDriver struct {
 	// here in the future.
 }
 
+type errorString struct {
+
+    s string
+}
+
+func (e *errorString) Error() string {
+
+    return e.s
+}
+
+func toError(text string) error {
+
+    return &errorString{text}
+}
+
 func mountPath(volumeName string) string {
+
 	return "/cfs/docker/" + volumeName
 }
 
+func cfsReady() (bool, error) {
+
+	stat, err := os.Stat("/cfs/READY")
+
+	if (err == nil && !stat.IsDir()) {
+		return true, nil
+	} else {
+		log.Println("[/cfs] is not ready.");
+		return false, cfsNotReady()
+	}
+}
+
+func volumeExists(volumeName string) bool {
+
+	path      := mountPath(volumeName)
+	stat, err := os.Stat(path)
+
+	if (err == nil && stat.IsDir()) {
+		return true
+	} else {	
+		return false
+	}
+}
+
+func cfsNotReady() error {
+
+	return toError("Cluster distributed filesystem [/cfs] is not ready.")
+}
+
+func volumeDoesNotExist(volumeName string) error {
+
+	return toError("volume [" + volumeName + "] does not exist.")
+}
+
 func (driver *neonDriver) Create(request *volume.CreateRequest) error {
-	
+
 	log.Println("create:", request.Name);
 
-	error := os.MkdirAll(mountPath(request.Name), 770)
+	ready, error := cfsReady()
+
+	if (!ready) {
+		return error
+	}
+	
+	if (volumeExists(request.Name)) {
+
+		// I'm not going to treat this as an error since CFS is
+		// distributed and its likely that folks may have 
+		// already created the volume on another host.
+
+		log.Println("volume [" + request.Name + "] already exists.")
+		return nil
+	}
+
+	error = os.MkdirAll(mountPath(request.Name), 770)
 	if (error != nil) {
 		log.Println("create error:", error)
 	}
@@ -36,46 +102,104 @@ func (driver *neonDriver) Remove(request *volume.RemoveRequest) error {
 
 	log.Println("remove:", request.Name);
 
-	error := os.RemoveAll(mountPath(request.Name))
-	if (error != nil) {
-		log.Println("create error:", error)
-	}
+	ready, error := cfsReady()
 
-	return error;
+	if (!ready) {
+		return error
+	}
+	
+	if (volumeExists(request.Name)) {
+
+		error = os.RemoveAll(mountPath(request.Name))
+		if (error != nil) {
+			log.Println("remove error:", error)
+		}
+
+		return error
+
+	} else {
+		return toError("volume [" + request.Name + "] does not exist.")
+	}
 }
 
 func (driver *neonDriver) Path(request *volume.PathRequest) (*volume.PathResponse, error) {
 
 	log.Println("path:", request.Name);
 
-	return &volume.PathResponse{Mountpoint: mountPath(request.Name)}, nil
+	ready, error := cfsReady()
+
+	if (!ready) {
+		return nil, error
+	}
+	
+	if (volumeExists(request.Name)) {
+		return &volume.PathResponse{Mountpoint: mountPath(request.Name)}, nil
+	} else {
+		return nil, volumeDoesNotExist(request.Name)
+	}
 }
 
 func (driver *neonDriver) Mount(request *volume.MountRequest) (*volume.MountResponse, error) {
 
 	log.Println("mount:", request.Name);
 
-	return &volume.MountResponse{Mountpoint: mountPath(request.Name)}, nil
+	ready, error := cfsReady()
+
+	if (!ready) {
+		return nil, error
+	}
+	
+	if (volumeExists(request.Name)) {
+		return &volume.MountResponse{Mountpoint: mountPath(request.Name)}, nil
+	} else {
+		return nil, volumeDoesNotExist(request.Name)
+	}
 }
 
 func (driver *neonDriver) Unmount(request *volume.UnmountRequest) error {
 
 	log.Println("unmount:", request.Name);
 
-	return nil
+	ready, error := cfsReady()
+
+	if (!ready) {
+		return error
+	}
+	
+	if (volumeExists(request.Name)) {
+		return nil
+	} else {
+		return volumeDoesNotExist(request.Name)
+	}
 }
 
 func (driver *neonDriver) Get(request *volume.GetRequest) (*volume.GetResponse, error) {
 
 	log.Println("get:", request.Name);
 
-	return &volume.GetResponse{Volume: &volume.Volume{Name: request.Name, Mountpoint: mountPath(request.Name)}}, nil
+	ready, error := cfsReady()
+
+	if (!ready) {
+		return nil, error
+	}
+	
+	if (volumeExists(request.Name)) {
+		return &volume.GetResponse{Volume: &volume.Volume{Name: request.Name, Mountpoint: mountPath(request.Name)}}, nil
+	} else {
+		return nil, volumeDoesNotExist(request.Name)
+	}
 }
 
 func (driver *neonDriver) List() (*volume.ListResponse, error) {
 
 	log.Println("list");
 
+	ready, error := cfsReady()
+
+	if (!ready) {
+		return nil, error;
+	}
+	
 	var volumes []*volume.Volume
 
 	files, err := ioutil.ReadDir("/cfs/docker")
