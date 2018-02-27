fd763b931242d239b3ebf40aeb4dcde6dbaa407c
Jeff Lill <jeff@lilltek.com>
2018-02-26 18:24:46 -0800

    [neon-cli]: Bumber version to: 1.2.45

--- Tools/neon-cli/Program.cs
+++ Tools/neon-cli/Program.cs
@@ -35,7 +35,7 @@ namespace NeonCli
         /// <summary>
         /// The <b>neon-cli</b> version.
         /// </summary>
-        public const string Version = "1.2.44";
+        public const string Version = "1.2.45";
 
         /// <summary>
         /// Program entry point.
