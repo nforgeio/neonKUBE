9d05df606f7557690bfcff430c78f50689452621
Jeff Lill <jeff@lilltek.com>
2018-03-03 22:03:56 -0800

    [neon-cli]: Verify admin privileges before shimming to Docker.

--- Tools/neon-cli/Program.cs
+++ Tools/neon-cli/Program.cs
@@ -360,6 +360,11 @@ Note that the tool requires admin priviledges for direct mode.
                             goto notShimmed;
                         }
 
+                        // We need administrator privileges to map the local drive
+                        // into a Docker container.
+
+                        Program.VerifyAdminPrivileges();
+
                         // Map the container's [/log] directory as required.
 
                         var logMount = string.Empty;
