a4ea2ec5991e45c93edd350447009feefa56fedf
Jeff Lill <jeff@lilltek.com>
2018-03-03 19:38:34 -0800

    [tools]: Added the [uuid-gen] tool.

---
+++ Tools/uuid-gen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uuid_gen
+{
+    /// <summary>
+    /// This tool simply generates a UUID and writes it to standard output.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main entry point.
+        /// </summary>
+        /// <param name="args">Command line arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.Write(Guid.NewGuid().ToString("D").ToLowerInvariant());
+        }
+    }
+}
