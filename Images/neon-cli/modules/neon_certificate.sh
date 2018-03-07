0f14392df016bb60423610693a30e3e76bbe511c
Jeff Lill <jeff@lilltek.com>
2018-03-06 19:14:26 -0800

    [neon-cli]: Build-in modules return better error message when executed outside of the Ansible master.

--- Images/neon-cli/modules/neon_certificate.sh
+++ Images/neon-cli/modules/neon_certificate.sh
@@ -4,7 +4,18 @@
 # this Ansible module.
 
 # This marker tells Ansible that we want JSON formatted arguments.
+
 WANT_JSON=yes
 
+# This command must run be run on the Ansible master within 
+# the [neon-cli] container.
+
+if [ "${IN_NEON_ANSIBLE_COMMAND}" == "" ] ; then
+
+    echo "*** ERROR: The [neon_certificate] module runs only on the Ansible master.  Consider using: delegate_to: localhost" 1>&2
+    exit 1
+fi
+
 # Invoke the module as a [neon-cli] command.
+
 neon ansible module neon_certificate $@
