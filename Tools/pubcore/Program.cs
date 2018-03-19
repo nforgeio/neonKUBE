8e056e0875116967735b0ba3b9a2043aa34d7f02
Jeff Lill <jeff@lilltek.com>
2018-03-19 16:23:10 -0700

    [tools]: Modified [pubcore] and the .NET Coretool projects to publish self-contained apps.
    This breaks the [neon-cli] Docker container.  I'll follow up with that in another commit.

--- Tools/pubcore/Program.cs
+++ Tools/pubcore/Program.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -26,7 +27,7 @@ namespace pubcore
         /// <summary>
         /// Tool version number.
         /// </summary>
-        public const string Version = "1.0";
+        public const string Version = "1.1";
 
         /// <summary>
         /// Program entrypoint.
@@ -34,33 +35,109 @@ namespace pubcore
         /// <param name="args">Command line arguments.</param>
         public static void Main(string[] args)
         {
-            if (args.Length != 2)
+            if (args.Length != 6)
             {
                 Console.WriteLine(
 $@"
 .NET Core Publishing Utility: pubcore [v{Version}]
 
-usage: pubcore CSPROJ-PATH BIN-PATH PUBLISH-PATH
+usage: pubcore PROJECT-PATH CONFIG TARGET-NAME PUBLISH-PATH [RUNTIME]
 
-    CSPROJ-PATH     - Path to the project (*.csproj)
-    TARGET-PATH     - Path to the build target.
-    PUBLISH-PATH    - Path to the output folder. 
+    PROJECT-PATH    - Path to the [.csproj] file
+    TARGET-NAME     - Build target name
+    CONFIG          - Build configuration (like: Debug or Release)
+    TARGET-PATH     - Build target path
+    PUBLISH-PATH    - Path to the publication folder
+
+REMARKS:
+
+This utility is designed to be called from within a .NET Core project's
+POST-BUILD event using Visual Studio post-build event macros.  Here's
+an example that publishes a standalone [win10-x64] app to OUTPUT-PATH.
+
+    pubcore ""$(ProjectPath)"" ""$(TargetName)"" ""$(ConfigurationName)"" ""OUTPUT-PATH"" win10-x64
+
+Note that you MUST ADD the following to the <PropertyGroup>...</PropertyGroup>
+section on your project CSPROJ file for this to work:
+
+    <RuntimeIdentifier>win10-x64</RuntimeIdentifier>
 ");
                 Environment.Exit(1);
             }
 
-            if (!File.Exists(args[0]))
+            // We need to examine/set an environment variable to prevent the [dotnet publish...]
+            // command below from recursively invoking the project build event that will invoke
+            // the program again.
+
+            const string recursionVar = "unix-text-68A780E5-00E7-4158-B5DE-E95C1D284911";
+
+            if (Environment.GetEnvironmentVariable(recursionVar) == "true")
             {
-                Console.WriteLine($"File [{args[0]}] does not exist.");
+                // Looks like we've recursed, so just bail right now.
+
+                return;
+            }
+
+            Environment.SetEnvironmentVariable(recursionVar, "true");
+
+            // Parse the arguments.
+
+            var projectPath = args[0];
+
+            if (!File.Exists(projectPath))
+            {
+                Console.WriteLine($"Project file [{projectPath}] does not exist.");
                 Environment.Exit(1);
             }
 
-            var programName = Path.GetFileNameWithoutExtension(args[0]);
-            var targetPath  = Path.GetDirectoryName(args[0]);
-            var publishPath = args[1];
-            var binFolder   = Path.Combine(publishPath, programName);
+            var targetName  = args[1];
+            var config      = args[2];
+            var targetPath  = Path.Combine(Path.GetDirectoryName(args[3]), "publish");
+            var publishPath = args[4];
+            var runtime     = args.ElementAtOrDefault(5);
+            var binFolder   = Path.Combine(publishPath, targetName);
 
             Directory.CreateDirectory(publishPath);
+
+            // Ensure that the runtime identifier is present in the project file.
+
+            var projectText     = File.ReadAllText(projectPath);
+            var runtimeProperty = $"<RuntimeIdentifier>{runtime}</RuntimeIdentifier>";
+
+            if (!projectText.Contains(runtimeProperty))
+            {
+                Console.Error.WriteLine();
+                Console.Error.WriteLine("ERROR: Make sure the runtime identifier below is present in your");
+                Console.Error.WriteLine("       project's <PropertyGroup/> section:");
+                Console.Error.WriteLine();
+                Console.Error.WriteLine($"    <PropertyGroup>");
+                Console.Error.WriteLine($"        {runtimeProperty}");
+                Console.Error.WriteLine($"    </PropertyGroup>");
+                Console.Error.WriteLine();
+                Console.Error.WriteLine($"PROJECT: {Path.GetFullPath(projectPath)}");
+                Console.Error.WriteLine();
+
+                Environment.Exit(1);
+            }
+
+            // Publish the project.
+
+            var startInfo = new ProcessStartInfo("dotnet.exe")
+            {
+                CreateNoWindow  = true,
+                UseShellExecute = false
+            };
+
+            startInfo.Arguments = $"publish \"{projectPath}\" -c \"{config}\" -r {runtime}";
+
+            var process = Process.Start(startInfo);
+
+            process.WaitForExit();
+
+            if (process.ExitCode != 0)
+            {
+                Environment.Exit(process.ExitCode);
+            }
 
             // It appears that references to published files can remain open 
             // after previous tool executions, even after the program has
@@ -73,10 +150,11 @@ usage: pubcore CSPROJ-PATH BIN-PATH PUBLISH-PATH
             {
                 try
                 {
-                    File.WriteAllText(Path.Combine(publishPath, $"{programName}.cmd"),
+                    File.WriteAllText(Path.Combine(publishPath, $"{targetName}.cmd"), 
 $@"@echo off
-dotnet %~dp0\{programName}\{Path.GetFileName(args[0])} %*
+%~dp0\{targetName}\{targetName}.exe %*
 ");
+
                     // Remove the output folder and then recreate it to ensure
                     // that all old files will be removed.
 
