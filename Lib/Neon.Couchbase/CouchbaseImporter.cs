e99fd9ff78cdc2f64c50cd7d82df78263283196f
Jeff Lill <jeff@lilltek.com>
2018-04-13 13:04:03 -0700

    [couchbase] Relocated [CouchbaseImporter] to [Neon.Couchbase.dll].

---
+++ Lib/Neon.Couchbase/CouchbaseImporter.cs
@@ -0,0 +1,279 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    CouchbaseImporter.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2018 by neonFORGE, LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Text;
+
+using Couchbase.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Neon.Common;
+
+namespace Neon.Data
+{
+    /// <summary>
+    /// Handles persisting JSON documents to Couchbase.
+    /// </summary>
+    public class CouchbaseImporter
+    {
+        private Action<string>  errorSink;
+        private bool            errorsDetected;
+        private IBucket         bucket;
+        private string          keyPattern;
+        private bool            checkMode;
+        private long            docNumber;
+        private StringBuilder   sbKey;      // Using a common instance for performance
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="errorSink">Action invoked when an error is encountered.</param>
+        /// <param name="bucket">The target Couchbase bucket.</param>
+        /// <param name="keyPattern">The key pattern (or <c>null</c>).</param>
+        /// <param name="checkMode">Pass <c>true</c> to process the file but not actually persist anything.</param>
+        public CouchbaseImporter(Action<string> errorSink, IBucket bucket, string keyPattern, bool checkMode)
+        {
+            this.errorSink  = errorSink;
+            this.bucket     = bucket;
+            this.keyPattern = keyPattern;
+            this.checkMode  = checkMode;
+            this.docNumber  = 1;
+            this.sbKey      = new StringBuilder();
+        }
+
+        /// <summary>
+        /// Reports an error.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        private void ReportError(string message)
+        {
+            errorSink?.Invoke(message);
+            errorsDetected = true;
+        }
+
+        /// <summary>
+        /// Persists a document to Couchbase.
+        /// </summary>
+        /// <param name="document">The document.</param>
+        /// <remarks>
+        /// <note>
+        /// This method will remove any <b>@@key</b> property from 
+        /// the document if present.
+        /// </note>
+        /// </remarks>
+        public void WriteDocument(JObject document)
+        {
+            string key;
+
+            if (errorsDetected)
+            {
+                // Stop writing documents after we've reported errors.
+
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(keyPattern))
+            {
+                // We need to parse the key pattern.
+
+                switch (keyPattern)
+                {
+                    // Optimize some common cases.
+
+                    case "#UUID#":
+
+                        key = EntityHelper.CreateUuid();
+                        break;
+
+                    case "#MONO_INCR#":
+
+                        key = docNumber.ToString();
+                        break;
+
+                    // Actually parse the thing. 
+
+                    default:
+
+                        sbKey.Clear();
+
+                        var pos = 0;
+
+                        while (pos < keyPattern.Length)
+                        {
+                            var ch = keyPattern[pos];
+
+                            switch (ch)
+                            {
+                                case '#':
+
+                                    if (pos < keyPattern.Length - 1 && keyPattern[pos + 1] == '#')
+                                    {
+                                        // Escaped '#'
+
+                                        sbKey.Append(ch);
+                                        pos++;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        // Scan forward for the closing '#'.
+
+                                        var posEnd = keyPattern.IndexOf('#', pos + 1);
+
+                                        if (posEnd == -1)
+                                        {
+                                            ReportError($"Key pattern [{keyPattern}] is missing the closing [#].");
+                                            return;
+                                        }
+
+                                        var identifier = keyPattern.Substring(pos + 1, posEnd - (pos + 1));
+
+                                        switch (identifier)
+                                        {
+                                            case "UUID":
+
+                                                sbKey.Append(EntityHelper.CreateUuid());
+                                                break;
+
+                                            case "MONO_INCR":
+
+                                                sbKey.Append(docNumber);
+                                                break;
+
+                                            default:
+
+                                                ReportError($"Key pattern [{keyPattern}] includes unknown key generator [#{identifier}#].");
+                                                return;
+                                        }
+
+                                        pos = posEnd;
+                                    }
+                                    break;
+
+                                case '%':
+
+                                    if (pos < keyPattern.Length - 1 && keyPattern[pos + 1] == '%')
+                                    {
+                                        // Escaped '%'
+
+                                        sbKey.Append(ch);
+                                        pos++;
+                                        break;
+                                    }
+                                    else
+                                    {
+                                        // Scan forward for the closing '%'.
+
+                                        var posEnd = keyPattern.IndexOf('%', pos + 1);
+
+                                        if (posEnd == -1)
+                                        {
+                                            ReportError($"Key pattern [{keyPattern}] is missing the closing [%].");
+                                            return;
+                                        }
+
+                                        var propertyName  = keyPattern.Substring(pos + 1, posEnd - (pos + 1));
+                                        var propertyValue = document.GetValue(propertyName);
+
+                                        if (propertyValue != null)
+                                        {
+                                            switch (propertyValue.Type)
+                                            {
+                                                case JTokenType.String:
+                                                case JTokenType.Integer:
+                                                case JTokenType.Float:
+                                                case JTokenType.Guid:
+                                                case JTokenType.Date:
+                                                case JTokenType.Uri:
+
+                                                    sbKey.Append(propertyValue);
+                                                    break;
+
+                                                default:
+
+                                                    ReportError($"Document number [{docNumber}] has an invalid [{propertyName}] property referenced by the key pattern.");
+                                                    return;
+                                            }
+
+                                            pos = posEnd;
+                                        }
+                                        else
+                                        {
+                                            ReportError($"Document number [{docNumber}] is missing the [{propertyName}] property referenced by the key pattern.");
+                                            return;
+                                        }
+                                    }
+                                    break;
+
+                                default:
+
+                                    sbKey.Append(ch);
+                                    break;
+                            }
+
+                            pos++;
+                        }
+
+                        key = sbKey.ToString();
+                        break;
+                }
+            }
+            else
+            {
+                // We don't have a key pattern so we'll look for a
+                // [@@key] property and use that value if present.
+                // Otherwise, we'll generate a UUID.
+
+                var keyProperty = document.GetValue("@@key");
+
+                if (keyProperty != null)
+                {
+                    switch (keyProperty.Type)
+                    {
+                        case JTokenType.String:
+                        case JTokenType.Integer:
+                        case JTokenType.Float:
+                        case JTokenType.Guid:
+                        case JTokenType.Date:
+                        case JTokenType.Uri:
+
+                            key = keyProperty.ToString();
+                            break;
+
+                        default:
+
+                            key = EntityHelper.CreateUuid();
+                            break;
+                    }
+                }
+                else
+                {
+                    key = EntityHelper.CreateUuid();
+                }
+
+                document.Remove("@@key"); // We don't persist the special key property.
+            }
+
+            if (!checkMode)
+            {
+                bucket.Upsert(key, document);
+            }
+
+            docNumber++;
+        }
+
+        /// <summary>
+        /// Returns the number of persisted documents.
+        /// </summary>
+        public long DocumentCount
+        {
+            get { return docNumber - 1; }
+        }
+    }
+}
