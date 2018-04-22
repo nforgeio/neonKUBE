4a91bab5460bacdc997a480136131513032c056a
Jeff Lill <jeff@lilltek.com>
2018-04-21 18:15:26 -0700

    [test]: Added [Test.Neon.Cluster] project.

---
+++ Test/Test.Neon.Cluster/neon-cli/ansible/Test_NeonCliAnsibleDockerService.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Test_NeonCliAnsibleDockerService.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2018 by neonFORGE, LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Neon.Common;
+
+using Xunit;
+
+namespace TestNeonCluster
+{
+    public class Test_NeonCliAnsibleDockerService : IClassFixture<DockerFixture>
+    {
+        private DockerFixture docker;
+
+        public Test_NeonCliAnsibleDockerService(DockerFixture docker)
+        {
+            this.docker = docker;
+        }
+
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
