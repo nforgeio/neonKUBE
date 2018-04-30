556204d048cd448430ca3bcc56f2bc4b03fd35a4
Jeff Lill <jeff@lilltek.com>
2018-04-29 17:03:20 -0700

    [neon-cli]: Bump version to 1.2.81

--- Tools/neon-cli/Program.cs
+++ Tools/neon-cli/Program.cs
@@ -35,7 +35,7 @@ namespace NeonCli
         /// <summary>
         /// The <b>neon-cli</b> version.
         /// </summary>
-        public const string Version = "1.2.80";
+        public const string Version = "1.2.81";
 
         /// <summary>
         /// Program entry point.
