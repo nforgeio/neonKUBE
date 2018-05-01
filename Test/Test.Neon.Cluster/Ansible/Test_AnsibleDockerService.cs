76bde84223b5b26adc472e5cb41317d13d008cb2
Jeff Lill <jeff@lilltek.com>
2018-04-30 18:37:16 -0700

    [ansible]: [neon_docker_service] first test passes.

--- Test/Test.Neon.Cluster/Ansible/Test_AnsibleDockerService.cs
+++ Test/Test.Neon.Cluster/Ansible/Test_AnsibleDockerService.cs
@@ -19,17 +19,89 @@ namespace TestNeonCluster
 {
     public class Test_AnsibleDockerService : IClassFixture<ClusterFixture>
     {
-        private ClusterFixture docker;
+        private ClusterFixture cluster;
 
-        public Test_AnsibleDockerService(ClusterFixture docker)
+        public Test_AnsibleDockerService(ClusterFixture cluster)
         {
-            this.docker = docker;
+            this.cluster = cluster;
+
+            // The test methods in this class depend on some fixed
+            // assets like networks, secrets, configs and such and
+            // then start, modify, and remove services.
+            //
+            // We're going to initialize these assets once and then
+            // reset only the cluster services between test methods
+            // for (hopefully) a bit better test execution performance.
+
+            if (cluster.LoginAndInitialize(login: null))
+            {
+                cluster.CreateNetwork("test-1");
+                cluster.CreateNetwork("test-2");
+                cluster.CreateSecret("test-1", "test-1");
+                cluster.CreateSecret("test-2", "test-2");
+                cluster.CreateConfig("test-1", "test-1");
+                cluster.CreateConfig("test-2", "test-2");
+            }
+            else
+            {
+                cluster.ClearServices();
+            }
         }
 
         [Fact]
         [Trait(TestCategory.CategoryTrait, TestCategory.NeonCli)]
-        public void Test()
+        public void SimpleDeployAndRemove()
         {
+            // Verify that we can deploy a basic service.
+
+            var name     = "test";
+            var playbook =
+$@"
+- name: test
+  hosts: localhost
+  tasks:
+    - name: manage service
+      neon_docker_service:
+        name: {name}
+        state: present
+        image: neoncluster/test        
+";
+            var results    = AnsiblePlayer.NeonPlay(playbook);
+            var taskResult = results.GetTaskResult("manage service");
+
+            Assert.True(taskResult.Success);
+            Assert.True(taskResult.Changed);
+            Assert.Single(cluster.ListServices().Where(s => s.Name == name));
+
+            // Verify that [state=absent] will remove the service.
+
+            playbook =
+$@"
+- name: test
+  hosts: localhost
+  tasks:
+    - name: manage service
+      neon_docker_service:
+        name: {name}
+        state: absent
+        image: neoncluster/test
+";
+            results    = AnsiblePlayer.NeonPlay(playbook);
+            taskResult = results.GetTaskResult("manage service");
+
+            Assert.True(taskResult.Success);
+            Assert.True(taskResult.Changed);
+            Assert.Empty(cluster.ListServices().Where(s => s.Name == name));
+
+            // Verify that removing the service again doesn't change anything
+            // because it's already been removed.
+
+            results    = AnsiblePlayer.NeonPlay(playbook);
+            taskResult = results.GetTaskResult("manage service");
+
+            Assert.True(taskResult.Success);
+            Assert.False(taskResult.Changed);
+            Assert.Empty(cluster.ListServices().Where(s => s.Name == name));
         }
     }
 }
