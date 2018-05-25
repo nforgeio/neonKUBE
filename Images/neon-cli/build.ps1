2e6125550ec5efe4583f7336ff8efdcfd3f67b33
Jeff Lill <jeff@lilltek.com>
2018-05-24 20:16:21 -0700

    [comment]

--- Images/neon-cli/build.ps1
+++ Images/neon-cli/build.ps1
@@ -33,7 +33,7 @@ if (Test-Path bin)
 Exec { mkdir bin }
 Exec { dotnet publish "$src_tools_path\\neon-cli\\neon-cli.csproj" -c Release -o "$pwd\bin" }
 
-# Split the build binaries into [__app] (application) and [__dep] dependency subfolders
+# Split the build binaries into [__app] application and [__dep] dependency subfolders
 # so we can tune the image layers.
 
 Exec { core-layers $appname "$pwd\bin" }
