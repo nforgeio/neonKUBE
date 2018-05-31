e015fad15387b6190259454d6f2b98042fd88978
Jeff Lill <jeff@lilltek.com>
2018-05-31 15:11:57 -0700

    [cluster]: Upgrade to Vault v0.10.1

--- Lib/Neon.Cluster/Model/ClusterDef/VaultOptions.cs
+++ Lib/Neon.Cluster/Model/ClusterDef/VaultOptions.cs
@@ -29,11 +29,11 @@ namespace Neon.Cluster
     /// </summary>
     public class VaultOptions
     {
-        private const string        defaultVersion      = "0.9.6";
+        private const string        defaultVersion      = "0.10.1";
         private const int           defaultKeyCount     = 1;
         private const int           defaultKeyThreshold = 1;
         private const string        defaultLease        = "0";
-        private readonly Version    minVersion          = new System.Version("0.9.6");
+        private readonly Version    minVersion          = new System.Version("0.10.1");
 
         /// <summary>
         /// Default constructor.
