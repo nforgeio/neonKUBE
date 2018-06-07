2281a7a5ddad6dfd42484e759903759cf6a7e647
Jeff Lill <jeff@lilltek.com>
2018-06-06 17:53:30 -0700

    [neon-cluster-manager]: Completed log index retention limits.

--- Services/neon-cluster-manager/Program.cs
+++ Services/neon-cluster-manager/Program.cs
@@ -588,11 +588,6 @@ namespace NeonClusterManager
         {
             while (true)
             {
-                // We don't need to poll that often because cluster managers
-                // will rarely change.
-
-                await Task.Delay(managerPollInterval);
-
                 try
                 {
                     if (terminator.CancellationToken.IsCancellationRequested)
@@ -639,7 +634,10 @@ namespace NeonClusterManager
                     log.LogError($"MANAGER-POLLER", e);
                 }
 
-                await Task.Delay(vaultPollInterval, terminator.CancellationToken);
+                // We don't need to poll that often because cluster managers
+                // will rarely change.
+
+                await Task.Delay(managerPollInterval, terminator.CancellationToken);
             }
         }
 
@@ -653,8 +651,6 @@ namespace NeonClusterManager
             {
                 while (true)
                 {
-                    await Task.Delay(logPollInterval);
-
                     var manager = cluster.GetHealthyManager();
 
                     try
@@ -674,12 +670,14 @@ namespace NeonClusterManager
                         //      metricbeat-6.1.1-2018.06.06
                         //
                         // The date is simply encodes the day covered by the index.
-                        // We're going to remove any indexes that are at least one
-                        // day older than the current date so that we won't delete
-                        // partial days logs.
+
+                        if (!cluster.TryGetGlobalInt(NeonClusterGlobals.LogRetentionDays, out var retentionDays))
+                        {
+                            retentionDays = 14;
+                        }
 
                         var utcNow           = DateTime.UtcNow;
-                        var deleteBeforeDate = new DateTime(utcNow.Year, utcNow.Month, utcNow.Day) - TimeSpan.FromDays(1);
+                        var deleteBeforeDate = new DateTime(utcNow.Year, utcNow.Month, utcNow.Day) - TimeSpan.FromDays(retentionDays);
 
                         var indexList = await jsonClient.GetAsync<JObject>($"http://{manager.PrivateAddress}:{NeonHostPorts.ProxyPrivateHttpLogEsData}/_aliases");
 
@@ -706,7 +704,7 @@ namespace NeonClusterManager
 
                             var date      = indexName.Substring(pos + 1);
                             var fields    = date.Split('.');
-                            var indexDate = default(DateTime); ;
+                            var indexDate = default(DateTime);
 
                             try
                             {
@@ -736,7 +734,7 @@ namespace NeonClusterManager
                         log.LogError($"LOG-PURGER", e);
                     }
 
-                    await Task.Delay(vaultPollInterval, terminator.CancellationToken);
+                    await Task.Delay(logPollInterval);
                 }
             }
         }
