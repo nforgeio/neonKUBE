4626842b0f179937701fff2d48bdbe092b52c225
Jeff Lill <jeff@lilltek.com>
2018-06-25 09:45:36 -0700

    [test]: Unit testing in progress.

--- Test/Test.Neon.Cluster/Cluster/Test_ClusterLoadBalancer.cs
+++ Test/Test.Neon.Cluster/Cluster/Test_ClusterLoadBalancer.cs
@@ -1,5 +1,5 @@
 ﻿//-----------------------------------------------------------------------------
-// FILE:	    Test_Cluster.cs
+// FILE:	    Test_ClusterLoadBalancer.cs
 // CONTRIBUTOR: Jeff Lill
 // COPYRIGHT:	Copyright (c) 2016-2018 by neonFORGE, LLC.  All rights reserved.
 
@@ -18,49 +18,18 @@ using Neon.Xunit.Cluster;
 
 using Xunit;
 
-namespace TestSamples
+namespace TestNeonCluster
 {
-    // This sample demonstrates how to use the [ClusterFixture] to execute
-    // unit tests against a real neonCLUSTER.
-    //
-    // Prerequisites:
-    //
-    //  * Deployed test cluster
-    //
-    //  * Login for the cluster imported for the current user
-    //
-    //  * NEON_TEST_CLUSTER environment variable set to the
-    //    login for the test cluster, like:
-    //
-    //      NEON_TEST_CLUSTER=root@mycluster
-
-    public class Test_Cluster : IClassFixture<ClusterFixture>
+    public class Test_ClusterLoadBalancer : IClassFixture<ClusterFixture>
     {
         private ClusterFixture  fixture;
         private ClusterProxy    cluster;
-        private HostsFixture    hosts;
 
-        public Test_Cluster(ClusterFixture fixture)
+        public Test_ClusterLoadBalancer(ClusterFixture fixture)
         {
-            if (!fixture.LoginAndInitialize(action:
-                () =>
-                {
-                    // This adds a [HostsFixture] to the [ClusterFixture] (which inherits
-                    // from [TestFixtureSet]).  We'll name the HostFixture so we can use
-                    // it to setup local DNS entries for the tests.
-
-                    fixture.AddFixture("hosts", hosts = new HostsFixture());
-                }))
+            if (!fixture.LoginAndInitialize())
             {
-                // This call ensures that the cluster is reset to a
-                // pristine state before each test is invoked.
-
                 fixture.Reset();
-
-                // Retrieve the hosts fixture and reset it.
-
-                hosts = (HostsFixture)fixture["hosts"];
-                hosts.Reset();
             }
 
             this.fixture = fixture;
@@ -80,14 +49,6 @@ namespace TestSamples
 
             Assert.Empty(fixture.ListStacks());
             Assert.Empty(fixture.ListServices());
-
-            // Add the local DNS entries for the services we'll deploy.  We're
-            // publishing these on host ports, so we'll use map the DNS entries
-            // to the local loopback address.
-
-            hosts.AddHostAddress("foo.com", "127.0.0.1", deferCommit: true);
-            hosts.AddHostAddress("bar.com", "127.0.0.1", deferCommit: true);
-            hosts.Commit();
 
             // Spin up a couple of NodeJS as stacks configuring them to return
             // different text using the OUTPUT environment variable.
