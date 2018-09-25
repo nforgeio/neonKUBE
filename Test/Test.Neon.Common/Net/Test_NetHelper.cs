d2190c8a9db6815036828e0892c3936ffdf9dbe4
Jeff Lill <jeff@lilltek.com>
2018-09-25 13:30:51 -0700

    [test]: Tested NetHelper.GetReachableHosts()

--- Test/Test.Neon.Common/Net/Test_NetHelper.cs
+++ Test/Test.Neon.Common/Net/Test_NetHelper.cs
@@ -490,11 +490,8 @@ namespace TestCommon
             //-----------------------------------------------------------------
             // IP address based hosts.
 
-            
-
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { "127.0.0.1", "127.0.0.2", "127.0.0.3" }).Host);
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { "127.0.0.1", "127.0.0.2", "127.0.0.3" }).Address.ToString());
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { "127.0.0.1", "127.0.0.2", "127.0.0.3" }, ReachableHostMode.ReturnFirst).Host);
+            TestHelper.AssertEquivalent(new string[] { "127.0.0.1", "127.0.0.2", "127.0.0.3" }, NetHelper.GetReachableHosts(new string[] { "127.0.0.1", "127.0.0.2", "127.0.0.3" }).Select(rh => rh.Host));
+            TestHelper.AssertEquivalent(new string[] { "127.0.0.1", "127.0.0.2", "127.0.0.3" }, NetHelper.GetReachableHosts(new string[] { "127.0.0.1", "127.0.0.2", "127.0.0.3" }).Select(rh => rh.Address.ToString()));
 
             // The [192.0.2.0/24] subnet is never supposed to be routable so we'll use 
             // some addresses in there to simulate offline hosts.
@@ -503,17 +500,13 @@ namespace TestCommon
             const string badIP1 = "192.0.2.2";
             const string badIP2 = "192.0.2.3";
 
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { "127.0.0.1", badIP0, badIP1 }).Host);
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { badIP0, "127.0.0.1", badIP1 }).Host);
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { badIP0, badIP1, "127.0.0.1" }).Host);
+            TestHelper.AssertEquivalent(new string[] { "127.0.0.1" }, NetHelper.GetReachableHosts(new string[] { "127.0.0.1", badIP0, badIP1 }).Select(rh => rh.Host));
+            TestHelper.AssertEquivalent(new string[] { "127.0.0.1" }, NetHelper.GetReachableHosts(new string[] { badIP0, "127.0.0.1", badIP1 }).Select(rh => rh.Host));
+            TestHelper.AssertEquivalent(new string[] { "127.0.0.1" }, NetHelper.GetReachableHosts(new string[] { badIP0, badIP1, "127.0.0.1" }).Select(rh => rh.Host));
 
-            // Verify the failure modes.
+            // Verify when no hosts are reachable.
 
-            Assert.Equal(badIP0, NetHelper.GetReachableHost(new string[] { badIP0, badIP1, badIP2 }).Host);
-            Assert.Equal(badIP0, NetHelper.GetReachableHost(new string[] { badIP0, badIP1, badIP2 }, ReachableHostMode.ReturnFirst).Host);
-            Assert.True( NetHelper.GetReachableHost(new string[] { badIP0, badIP1, badIP2 }, ReachableHostMode.ReturnFirst).Unreachable);
-            Assert.Null(NetHelper.GetReachableHost(new string[] { badIP0, badIP1, badIP2 }, ReachableHostMode.ReturnNull));
-            Assert.Throws<NetworkException>(() => NetHelper.GetReachableHost(new string[] { badIP0, badIP1, badIP2 }, ReachableHostMode.Throw).Host);
+            Assert.Empty( NetHelper.GetReachableHosts(new string[] { badIP0, badIP1, badIP2 }));
 
             //-----------------------------------------------------------------
             // Hostname based hosts.
@@ -521,25 +514,19 @@ namespace TestCommon
             // Verify that we always return the first host if it's healthy
             // when we're using [ReachableHostMode.ReturnFirst].
 
-            Assert.Equal("www.google.com", NetHelper.GetReachableHost(new string[] { "www.google.com", "www.microsoft.com", "www.facebook.com" }).Host);
-            Assert.Equal("www.google.com", NetHelper.GetReachableHost(new string[] { "www.google.com", "www.microsoft.com", "www.facebook.com" }, ReachableHostMode.ReturnFirst).Host);
-            Assert.False(NetHelper.GetReachableHost(new string[] { "www.google.com", "www.microsoft.com", "www.facebook.com" }, ReachableHostMode.ReturnFirst).Unreachable);
+            TestHelper.AssertEquivalent(new string[] { "www.google.com", "www.microsoft.com", "www.facebook.com" }, NetHelper.GetReachableHosts(new string[] { "www.google.com", "www.microsoft.com", "www.facebook.com" }).Select(rh => rh.Host));
 
             const string badHost0 = "bad0.host.baddomain";
             const string badHost1 = "bad1.host.baddomain";
             const string badHost2 = "bad2.host.baddomain";
 
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { "127.0.0.1", badHost0, badHost1 }).Host);
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { badHost0, "127.0.0.1", badHost1 }).Host);
-            Assert.Equal("127.0.0.1", NetHelper.GetReachableHost(new string[] { badHost0, badHost1, "127.0.0.1" }).Host);
+            TestHelper.AssertEquivalent(new string[] { "127.0.0.1" }, NetHelper.GetReachableHosts(new string[] { "127.0.0.1", badHost0, badHost1 }).Select(rh => rh.Host));
+            TestHelper.AssertEquivalent(new string[] { "127.0.0.1" }, NetHelper.GetReachableHosts(new string[] { badHost0, "127.0.0.1", badHost1 }).Select(rh => rh.Host));
+            TestHelper.AssertEquivalent(new string[] { "127.0.0.1" }, NetHelper.GetReachableHosts(new string[] { badHost0, badHost1, "127.0.0.1" }).Select(rh => rh.Host));
 
-            // Verify the failure modes.
+            // Verify when no hosts are reachable.
 
-            Assert.Equal(badHost0, NetHelper.GetReachableHost(new string[] { badHost0, badHost1, badHost2 }).Host);
-            Assert.Equal(badHost0, NetHelper.GetReachableHost(new string[] { badHost0, badHost1, badHost2 }, ReachableHostMode.ReturnFirst).Host);
-            Assert.True(NetHelper.GetReachableHost(new string[] { badHost0, badHost1, badHost2 }, ReachableHostMode.ReturnFirst).Unreachable);
-            Assert.Null(NetHelper.GetReachableHost(new string[] { badHost0, badHost1, badHost2 }, ReachableHostMode.ReturnNull));
-            Assert.Throws<NetworkException>(() => NetHelper.GetReachableHost(new string[] { badHost0, badHost1, badHost2 }, ReachableHostMode.Throw));
+            Assert.Empty(NetHelper.GetReachableHosts(new string[] { badHost0, badHost1, badHost2 }));
         }
     }
 }
--- Test/Test.Neon.Common/Xunit/Test_TestHelper.cs
+++ Test/Test.Neon.Common/Xunit/Test_TestHelper.cs
@@ -73,30 +73,30 @@ namespace TestCommon
         [Trait(TestCategory.CategoryTrait, TestCategory.NeonCommon)]
         public void Enumerable_Equivalent()
         {
-            TestHelper.Equivalent(new List<string>(), new List<string>());
-            TestHelper.Equivalent(new List<string>() { "0" }, new List<string>() { "0" });
-            TestHelper.Equivalent(new List<string>() { "0", "1" }, new List<string>() { "0", "1" });
-            TestHelper.Equivalent(new List<string>() { "0", "1" }, new List<string>() { "1", "0" });
+            TestHelper.AssertEquivalent(new List<string>(), new List<string>());
+            TestHelper.AssertEquivalent(new List<string>() { "0" }, new List<string>() { "0" });
+            TestHelper.AssertEquivalent(new List<string>() { "0", "1" }, new List<string>() { "0", "1" });
+            TestHelper.AssertEquivalent(new List<string>() { "0", "1" }, new List<string>() { "1", "0" });
 
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new List<string>(), new List<string>() { "0" }));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new List<string>() { "0" }, new List<string>()));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new List<string>() { "0" }, new List<string>() { "1" }));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new List<string>() { "0" }, new List<string>() { "0", "1" }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new List<string>(), new List<string>() { "0" }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new List<string>() { "0" }, new List<string>()));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new List<string>() { "0" }, new List<string>() { "1" }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new List<string>() { "0" }, new List<string>() { "0", "1" }));
         }
 
         [Fact]
         [Trait(TestCategory.CategoryTrait, TestCategory.NeonCommon)]
         public void Enumerable_NotEquivalent()
         {
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new List<string>(), new List<string>()));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new List<string>() { "0" }, new List<string>() { "0" }));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new List<string>() { "0", "1" }, new List<string>() { "0", "1" }));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new List<string>() { "0", "1" }, new List<string>() { "1", "0" }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new List<string>(), new List<string>()));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new List<string>() { "0" }, new List<string>() { "0" }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new List<string>() { "0", "1" }, new List<string>() { "0", "1" }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new List<string>() { "0", "1" }, new List<string>() { "1", "0" }));
 
-            TestHelper.NotEquivalent(new List<string>(), new List<string>() { "0" });
-            TestHelper.NotEquivalent(new List<string>() { "0" }, new List<string>());
-            TestHelper.NotEquivalent(new List<string>() { "0" }, new List<string>() { "1" });
-            TestHelper.NotEquivalent(new List<string>() { "0" }, new List<string>() { "0", "1" });
+            TestHelper.AssertNotEquivalent(new List<string>(), new List<string>() { "0" });
+            TestHelper.AssertNotEquivalent(new List<string>() { "0" }, new List<string>());
+            TestHelper.AssertNotEquivalent(new List<string>() { "0" }, new List<string>() { "1" });
+            TestHelper.AssertNotEquivalent(new List<string>() { "0" }, new List<string>() { "0", "1" });
         }
 
         [Fact]
@@ -105,15 +105,15 @@ namespace TestCommon
         {
             var comparer = new ItemComparer();
 
-            TestHelper.Equivalent(new List<Item>(), new List<Item>(), comparer);
-            TestHelper.Equivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("0") }, comparer);
-            TestHelper.Equivalent(new List<Item>() { new Item("0"), new Item("1") }, new List<Item>() { new Item("0"), new Item("1") }, comparer);
-            TestHelper.Equivalent(new List<Item>() { new Item("0"), new Item("1") }, new List<Item>() { new Item("1"), new Item("0") }, comparer);
+            TestHelper.AssertEquivalent(new List<Item>(), new List<Item>(), comparer);
+            TestHelper.AssertEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("0") }, comparer);
+            TestHelper.AssertEquivalent(new List<Item>() { new Item("0"), new Item("1") }, new List<Item>() { new Item("0"), new Item("1") }, comparer);
+            TestHelper.AssertEquivalent(new List<Item>() { new Item("0"), new Item("1") }, new List<Item>() { new Item("1"), new Item("0") }, comparer);
 
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new List<Item>(), new List<Item>() { new Item("0") }, comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new List<Item>() { new Item("0") }, new List<Item>(), comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("1") }, comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("0"), new Item("1") }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new List<Item>(), new List<Item>() { new Item("0") }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new List<Item>() { new Item("0") }, new List<Item>(), comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("1") }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("0"), new Item("1") }, comparer));
         }
 
         [Fact]
@@ -122,43 +122,43 @@ namespace TestCommon
         {
             var comparer = new ItemComparer();
 
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new List<Item>(), new List<Item>(), comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("0") }, comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new List<Item>() { new Item("0"), new Item("1") }, new List<Item>() { new Item("0"), new Item("1") }, comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new List<Item>() { new Item("0"), new Item("1") }, new List<Item>() { new Item("1"), new Item("0") }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new List<Item>(), new List<Item>(), comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("0") }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new List<Item>() { new Item("0"), new Item("1") }, new List<Item>() { new Item("0"), new Item("1") }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new List<Item>() { new Item("0"), new Item("1") }, new List<Item>() { new Item("1"), new Item("0") }, comparer));
 
-            TestHelper.NotEquivalent(new List<Item>(), new List<Item>() { new Item("0") }, comparer);
-            TestHelper.NotEquivalent(new List<Item>() { new Item("0") }, new List<Item>(), comparer);
-            TestHelper.NotEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("1") }, comparer);
-            TestHelper.NotEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("0"), new Item("1") }, comparer);
+            TestHelper.AssertNotEquivalent(new List<Item>(), new List<Item>() { new Item("0") }, comparer);
+            TestHelper.AssertNotEquivalent(new List<Item>() { new Item("0") }, new List<Item>(), comparer);
+            TestHelper.AssertNotEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("1") }, comparer);
+            TestHelper.AssertNotEquivalent(new List<Item>() { new Item("0") }, new List<Item>() { new Item("0"), new Item("1") }, comparer);
         }
 
         [Fact]
         [Trait(TestCategory.CategoryTrait, TestCategory.NeonCommon)]
         public void Collection_Equivalent()
         {
-            TestHelper.Equivalent(new Dictionary<string, string>(), new Dictionary<string, string>());
-            TestHelper.Equivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "0", "0" } });
-            TestHelper.Equivalent(new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } }, new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } });
+            TestHelper.AssertEquivalent(new Dictionary<string, string>(), new Dictionary<string, string>());
+            TestHelper.AssertEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "0", "0" } });
+            TestHelper.AssertEquivalent(new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } }, new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } });
 
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new Dictionary<string, string>(), new Dictionary<string, string>() { { "0", "0" } }));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>()));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "1", "1" } }));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new Dictionary<string, string>(), new Dictionary<string, string>() { { "0", "0" } }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>()));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "1", "1" } }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } }));
         }
 
         [Fact]
         [Trait(TestCategory.CategoryTrait, TestCategory.NeonCommon)]
         public void Collection_NotEquivalent()
         {
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new Dictionary<string, string>(), new Dictionary<string, string>()));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "0", "0" } }));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } }, new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new Dictionary<string, string>(), new Dictionary<string, string>()));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "0", "0" } }));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } }, new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } }));
 
-            TestHelper.NotEquivalent(new Dictionary<string, string>(), new Dictionary<string, string>() { { "0", "0" } });
-            TestHelper.NotEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>());
-            TestHelper.NotEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "1", "1" } });
-            TestHelper.NotEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } });
+            TestHelper.AssertNotEquivalent(new Dictionary<string, string>(), new Dictionary<string, string>() { { "0", "0" } });
+            TestHelper.AssertNotEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>());
+            TestHelper.AssertNotEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "1", "1" } });
+            TestHelper.AssertNotEquivalent(new Dictionary<string, string>() { { "0", "0" } }, new Dictionary<string, string>() { { "0", "0" }, { "1", "1" } });
         }
 
         [Fact]
@@ -167,14 +167,14 @@ namespace TestCommon
         {
             var comparer = new ItemComparer();
 
-            TestHelper.Equivalent(new Dictionary<string, Item>(), new Dictionary<string, Item>(), comparer);
-            TestHelper.Equivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "0", new Item("0") } }, comparer);
-            TestHelper.Equivalent(new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, comparer);
+            TestHelper.AssertEquivalent(new Dictionary<string, Item>(), new Dictionary<string, Item>(), comparer);
+            TestHelper.AssertEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "0", new Item("0") } }, comparer);
+            TestHelper.AssertEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, comparer);
 
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new Dictionary<string, Item>(), new Dictionary<string, Item>() { { "0", new Item("0") } }, comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>(), comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "1", new Item("1") } }, comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.Equivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new Dictionary<string, Item>(), new Dictionary<string, Item>() { { "0", new Item("0") } }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>(), comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "1", new Item("1") } }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, comparer));
         }
 
         [Fact]
@@ -183,14 +183,14 @@ namespace TestCommon
         {
             var comparer = new ItemComparer();
 
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new Dictionary<string, Item>(), new Dictionary<string, Item>(), comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "0", new Item("0") } }, comparer));
-            Assert.ThrowsAny<Exception>(() => TestHelper.NotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new Dictionary<string, Item>(), new Dictionary<string, Item>(), comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "0", new Item("0") } }, comparer));
+            Assert.ThrowsAny<Exception>(() => TestHelper.AssertNotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, comparer));
 
-            TestHelper.NotEquivalent(new Dictionary<string, Item>(), new Dictionary<string, Item>() { { "0", new Item("0") } }, comparer);
-            TestHelper.NotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>(), comparer);
-            TestHelper.NotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "1", new Item("1") } }, comparer);
-            TestHelper.NotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, comparer);
+            TestHelper.AssertNotEquivalent(new Dictionary<string, Item>(), new Dictionary<string, Item>() { { "0", new Item("0") } }, comparer);
+            TestHelper.AssertNotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>(), comparer);
+            TestHelper.AssertNotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "1", new Item("1") } }, comparer);
+            TestHelper.AssertNotEquivalent(new Dictionary<string, Item>() { { "0", new Item("0") } }, new Dictionary<string, Item>() { { "0", new Item("0") }, { "1", new Item("1") } }, comparer);
         }
     }
 }
