5119de711d171ed28c21be85ed48813c703b9e97
Jeff Lill <jeff@lilltek.com>
2018-10-27 13:24:28 -0700

    [images]: Include varnish scripts and VMODs in the release ZIP.

--- Images/varnish-builder/docker-entrypoint.sh
+++ Images/varnish-builder/docker-entrypoint.sh
@@ -4,7 +4,11 @@
 # CONTRIBUTOR:  Jeff Lill
 # COPYRIGHT:    Copyright (c) 2016-2018 by neonFORGE, LLC.  All rights reserved.
 #
-# Builds Varnish and writes the [varnishd] binary to [/mnt/output].
+# Builds Varnish and writes a ZIP file including the [varnish*] binaries and modules to [/mnt/output].
+
+if [ "$1" == "bash "] ; then
+    exec bash
+fi
 
 # Add the root directory to the PATH.
 
@@ -22,10 +26,21 @@ if [ "${GIT_BRANCH}" == "" ] ; then
     exit 1
 fi
 
+# Create a local [/output] folder where we'll write output 
+# files before we ZIP them.
+
+export OUTPUT_DIR=/varnish-install
+mkdir -p $OUTPUT_DIR
+
 # Ensure that the [/mnt/output] folder exists as a fail-safe.  Normally, the
 # caller would mount a host directory here.
 
 mkdir -p /mnt/output
+
+#------------------------------------------------------------------------------
+# Build varnish.  Here's the documentation:
+#
+#       https://varnish-cache.org/docs/trunk/installation/install.html
 
 # Clone the source repo and switch to the requested branch.
 
@@ -40,14 +55,37 @@ sh autogen.sh
 sh configure
 make
 
+# Install Varnish so we can gather the files.
+
+make install
+
+# Gather the relevant files to $OUTPUT_DIR.
+
+mkdir -p $OUTPUT_DIR/bin
+cp /varnish-cache/bin/varnishadm/varnishadm     $OUTPUT_DIR/bin
+cp /varnish-cache/bin/varnishd/varnishd         $OUTPUT_DIR/bin
+cp /varnish-cache/bin/varnishhist/varnishhist   $OUTPUT_DIR/bin
+cp /varnish-cache/bin/varnishlog/varnishlog     $OUTPUT_DIR/bin
+cp /varnish-cache/bin/varnishncsa/varnishncsa   $OUTPUT_DIR/bin
+cp /varnish-cache/bin/varnishstat/varnishstat   $OUTPUT_DIR/bin
+cp /varnish-cache/bin/varnishtest/varnishtest   $OUTPUT_DIR/bin
+cp /varnish-cache/bin/varnishtop/varnishtop     $OUTPUT_DIR/bin
+
+mkdir -p $OUTPUT_DIR/vmods
+cp -r /usr/local/lib/varnish/vmods/*            $OUTPUT_DIR/vmods
+
+#------------------------------------------------------------------------------
+# ZIP and publish the output files.
+
+export OUTPUT_ZIP=/tmp/varnish.${GIT_BRANCH}.0.zip
+
+zip $OUTPUT_ZIP $OUTPUT_DIR/bin/*
+zip $OUTPUT_ZIP $OUTPUT_DIR/vmods/*
+cp $OUTPUT_ZIP /mnt/output
+
+#------------------------------------------------------------------------------
 # Validate the build if CHECK=1.  This takes 30+ minutes.
 
 if [ "$CHECK" == "1" ] ; then
     make check
 fi
-
-# Copy the output binary.
-
-cp /varnish-cache/bin/varnishd/varnishd /tmp
-zip -j /tmp/varnish.${GIT_BRANCH}.0.zip /tmp/varnishd
-cp /tmp/varnish.${GIT_BRANCH}.0.zip /mnt/output
--- Images/varnish/BUILD.md
+++ Images/varnish/BUILD.md
@@ -12,11 +12,11 @@ You'll need to manually perform the following steps to build and publish the `va
 
 1. Build `varnishd` via the command below, specifying the Varnish branch/version:
 
-   `docker run --rm --env "GIT_REPO=https://github.com/jefflill/varnish-cache.git" --env "GIT_BRANCH=6.1" --mount type=bind,src=%NF_BUILD%,dst=/mnt/output nhive/varnish-builder`
+   `docker run -it --rm --env "GIT_REPO=https://github.com/jefflill/varnish-cache.git" --env "GIT_BRANCH=6.1" --mount type=bind,src=%NF_BUILD%,dst=/mnt/output nhive/varnish-builder`
 
-   It's a good idea to verify the build by adding the `--env CHECK=1` environment variable.  This will take 30+ minutes:
+   It's a good idea to verify the build for each Varnish version at least once by adding the `--env CHECK=1` environment variable.  This will take 30+ minutes:
 
-   `docker run --rm --env "GIT_REPO=https://github.com/jefflill/varnish-cache.git" --env "GIT_BRANCH=6.1" --env "CHECK=1" --mount type=bind,src=%NF_BUILD%,dst=/mnt/output nhive/varnish-builder`
+   `docker run -it --rm --env "GIT_REPO=https://github.com/jefflill/varnish-cache.git" --env "GIT_BRANCH=6.1" --env "CHECK=1" --mount type=bind,src=%NF_BUILD%,dst=/mnt/output nhive/varnish-builder`
 
 2. Upload the ZIP file to S3 at:  https://s3.console.aws.amazon.com/s3/buckets/neoncluster
 
