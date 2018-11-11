bacd121ad21ab58cecb65d15e6b16d31f350de9d
Jeff Lill <jeff@lilltek.com>
2018-11-10 18:21:59 -0800

    [neon-cli]: Bump version to 18.11.0-alpha.5

--- Tools/neon-cli/Program.cs
+++ Tools/neon-cli/Program.cs
@@ -37,7 +37,7 @@ namespace NeonCli
         /// if the <b>--version=VERSION</b> option was specified to force a specific
         /// version.
         /// </summary>
-        public const string ActualVersion  = "18.10.1-alpha.4";
+        public const string ActualVersion  = "18.11.0-alpha.5";
 
         /// <summary>
         /// Returns the <b>neon-cli</b> version.  This may be different from <see cref="ActualVersion"/>
