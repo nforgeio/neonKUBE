1335aab5d2afebe555e9cbe92d6170705058ab7f
Jeff Lill <jeff@lilltek.com>
2019-01-31 08:46:18 -0800

    [cluster setup]: Configure tiller service role binding.

--- Tools/neon-cli/Commands/Cluster/ClusterSetupCommand.cs
+++ Tools/neon-cli/Commands/Cluster/ClusterSetupCommand.cs
@@ -1096,6 +1096,28 @@ networking:
                         () =>
                         {
                             firstMaster.Status = "deploy: Helm/Tiller";
+
+                            firstMaster.KubeCtlApply(
+@"
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: tiller
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: tiller
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: tiller
+  namespace: kube-system
+");
                             firstMaster.SudoCommand("helm init --service-account tiller");
                         });
 
