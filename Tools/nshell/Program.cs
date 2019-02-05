1cbc85a184460838a43313724aa74b21f61fa607
Jeff Lill <jeff@lilltek.com>
2019-02-04 17:37:06 -0800

    [nshell]: Command classes and [version] command.

--- Tools/nshell/Program.cs
+++ Tools/nshell/Program.cs
@@ -17,12 +17,23 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
 using Neon;
 using Neon.Common;
+using Neon.Diagnostics;
 
 namespace NShell
 {
@@ -42,7 +53,209 @@ namespace NShell
         /// <param name="args">Command line arguments.</param>
         public static void Main(string[] args)
         {
-            var commandLine = new CommandLine(args);
+            string usage = $@"
+neonKUBE Shell Utilities: nshell [v{Program.Version}]
+{Build.Copyright}
+
+USAGE:
+
+    nshell [OPTIONS] COMMAND [ARG...]
+
+COMMAND SUMMARY:
+
+    nshell help     COMMAND
+
+ARGUMENTS:
+
+OPTIONS:
+
+";
+            // Disable any logging that might be performed by library classes.
+
+            LogManager.Default.LogLevel = LogLevel.None;
+
+            // Process the command line.
+
+            try
+            {
+                ICommand command;
+
+                CommandLine     = new CommandLine(args);
+                LeftCommandLine = CommandLine.Split("--").Left;
+
+                if (CommandLine.Arguments.Length == 0)
+                {
+                    Console.WriteLine(usage);
+                    Program.Exit(0);
+                }
+
+                var commands = new List<ICommand>()
+                {
+                    new VersionCommand()
+                };
+
+                // Short-circuit the help command.
+
+                if (CommandLine.Arguments[0] == "help")
+                {
+                    if (CommandLine.Arguments.Length == 1)
+                    {
+                        Console.WriteLine(usage);
+                        Program.Exit(0);
+                    }
+
+                    CommandLine = CommandLine.Shift(1);
+
+                    command = GetCommand(CommandLine, commands);
+
+                    if (command == null)
+                    {
+                        Console.Error.WriteLine($"*** ERROR: Unexpected [{CommandLine.Arguments[0]}] command.");
+                        Program.Exit(1);
+                    }
+
+                    command.Help();
+                    Program.Exit(0);
+                }
+
+                // Lookup the command.
+
+                command = GetCommand(CommandLine, commands);
+
+                if (command == null)
+                {
+                    Console.Error.WriteLine($"*** ERROR: Unexpected [{CommandLine.Arguments[0]}] command.");
+                    Program.Exit(1);
+                }
+
+                // Run the command.
+
+                if (command.SplitItem != null)
+                {
+                    // We don't shift the command line for pass-thru commands 
+                    // because we don't want to change the order of any options.
+
+                    command.Run(CommandLine);
+                }
+                else
+                {
+                    command.Run(CommandLine.Shift(command.Words.Length));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"*** ERROR: {NeonHelper.ExceptionError(e)}");
+                Console.Error.WriteLine(string.Empty);
+                Program.Exit(1);
+            }
+
+            Program.Exit(0);
+        }
+
+        /// <summary>
+        /// Returns the orignal program <see cref="CommandLine"/>.
+        /// </summary>
+        public static CommandLine CommandLine { get; private set; }
+
+        /// <summary>
+        /// Returns the part of the command line to the left of the [--] splitter
+        /// or the entire command line if there is no splitter.
+        /// </summary>
+        public static CommandLine LeftCommandLine { get; private set; }
+
+        /// <summary>
+        /// Returns <c>true</c> if the program was built from the production <b>PROD</b> 
+        /// source code branch.
+        /// </summary>
+        public static bool IsProd => ThisAssembly.Git.Branch.Equals("prod", StringComparison.InvariantCultureIgnoreCase);
+
+        /// <summary>
+        /// Returns the program version as the Git branch and commit and an optional
+        /// indication of whether the program was build from a dirty branch.
+        /// </summary>
+        public static string GitVersion
+        {
+            get
+            {
+                var version = $"{ThisAssembly.Git.Branch}-{ThisAssembly.Git.Commit}";
+
+#pragma warning disable 162 // Unreachable code
+
+                //if (ThisAssembly.Git.IsDirty)
+                //{
+                //    version += "-DIRTY";
+                //}
+
+#pragma warning restore 162 // Unreachable code
+
+                return version;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to match the command line to the <see cref="ICommand"/> to be used
+        /// to implement the command.
+        /// </summary>
+        /// <param name="commandLine">The command line.</param>
+        /// <param name="commands">The commands.</param>
+        /// <returns>The command instance or <c>null</c>.</returns>
+        private static ICommand GetCommand(CommandLine commandLine, List<ICommand> commands)
+        {
+            // Sort the commands in decending order by number of words in the
+            // command (we want to match the longest sequence).
+
+            foreach (var command in commands.OrderByDescending(c => c.Words.Length))
+            {
+                if (command.Words.Length > commandLine.Arguments.Length)
+                {
+                    // Not enough arguments to match the command.
+
+                    continue;
+                }
+
+                var matches = true;
+
+                for (int i = 0; i < command.Words.Length; i++)
+                {
+                    if (!string.Equals(command.Words[i], commandLine.Arguments[i]))
+                    {
+                        matches = false;
+                        break;
+                    }
+                }
+
+                if (!matches && command.AltWords != null)
+                {
+                    matches = true;
+
+                    for (int i = 0; i < command.AltWords.Length; i++)
+                    {
+                        if (!string.Equals(command.AltWords[i], commandLine.Arguments[i]))
+                        {
+                            matches = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (matches)
+                {
+                    return command;
+                }
+            }
+
+            // No match.
+
+            return null;
+        }
+
+        /// <summary>
+        /// Exits the program returning the specified process exit code.
+        /// </summary>
+        /// <param name="exitCode">The exit code.</param>
+        public static void Exit(int exitCode)
+        {
+            Environment.Exit(exitCode);
         }
     }
 }
