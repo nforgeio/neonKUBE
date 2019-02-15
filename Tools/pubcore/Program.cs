f9e09d9172674f0ae78cd93f4d19b5123724f44d
Jeff Lill <jeff@lilltek.com>
2019-02-15 09:03:16 -0800

    [pubcore]: Use [--no-build] instead of [--no-restore].
    I've been waiting for the [--no-build] option and .NET Core 3.0 finally has it.  This implies [--no-restore] and should result in faster solution builds because we won't build these projects twice any more.

--- Tools/pubcore/Program.cs
+++ Tools/pubcore/Program.cs
@@ -174,7 +174,7 @@ or:
                     var sbOutput = new StringBuilder();
 
                     process.StartInfo.FileName               = "dotnet.exe";
-                    process.StartInfo.Arguments              = $"publish \"{projectPath}\" -c \"{config}\" -r {runtime} --no-restore --no-dependencies";
+                    process.StartInfo.Arguments              = $"publish \"{projectPath}\" -c \"{config}\" -r {runtime} --no-build --no-dependencies";
                     process.StartInfo.CreateNoWindow         = true;
                     process.StartInfo.UseShellExecute        = false;
                     process.StartInfo.RedirectStandardError  = true;
