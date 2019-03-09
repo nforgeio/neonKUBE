acad36df5fbce9fe646bfff047e5b5ada39ec6f5
Jeff Lill <jeff@lilltek.com>
2019-03-08 17:45:30 -0800

    #433: Toast now displays neonKUBE Desktop.

--- Desktop/WinDesktop/Properties/AssemblyInfo.cs
+++ Desktop/WinDesktop/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WinDesktop")]
+[assembly: AssemblyTitle("neonKUBE Desktop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WinDesktop")]
+[assembly: AssemblyProduct("neonKUBE Desktop")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
