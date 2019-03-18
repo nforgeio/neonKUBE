6e33ee95034d809fcf805a6747b94828083e1da5
Jeff Lill <jeff@lilltek.com>
2019-03-18 09:08:01 -0700

    [Neon.CodeGen]: Service client generation in progress.

---
+++ Lib/Neon.CodeGen/PassParameter.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    PassParameter.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2016-2019 by neonFORGE, LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime;
+using System.Runtime.Serialization;
+using System.Text;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Text;
+
+using Newtonsoft.Json;
+
+using Neon.Serialization;
+
+namespace Neon.CodeGen
+{
+    /// <summary>
+    /// Identifies how a service method parameter is passed within a REST
+    /// service request.
+    /// </summary>
+    internal enum PassParameter
+    {
+        /// <summary>
+        /// Passes the parameter as the HTTP request body. 
+        /// </summary>
+        AsBody,
+
+        /// <summary>
+        /// Passes the parameter as a URI query parameter.
+        /// </summary>
+        InQuery,
+
+        /// <summary>
+        /// Passes the request within the URI route template. 
+        /// </summary>
+        InRoute
+    }
+}
