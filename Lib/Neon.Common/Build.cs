c06cd39ec9c603744c49a4aaf79eb6c40f85cbc1
Jeff Lill <jeff@lilltek.com>
2019-04-08 08:30:08 -0700

    [build]: Bump version to: 0.3.6-alpha

--- Lib/Neon.Common/Build.cs
+++ Lib/Neon.Common/Build.cs
@@ -46,7 +46,7 @@ namespace Neon
         /// <summary>
         /// The released product version.
         /// </summary>
-        public const string ProductVersion = "0.3.5-alpha";
+        public const string ProductVersion = "0.3.6-alpha";
 
         /// <summary>
         /// The product license.
