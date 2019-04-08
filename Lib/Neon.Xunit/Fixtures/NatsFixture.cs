a66f92b7ed9a0e368e132aa641401a63e3cfa322
Jeff Lill <jeff@lilltek.com>
2019-04-08 08:09:12 -0700

    [Neon.Xunit]: NatsFixture in progress.

--- Lib/Neon.Xunit/Fixtures/NatsFixture.cs
+++ Lib/Neon.Xunit/Fixtures/NatsFixture.cs
@@ -28,8 +28,12 @@ using Neon.Net;
 
 namespace Neon.Xunit.Couchbase
 {
+    public class NatsSettings
+    {
+    }
+
     /// <summary>
-    /// Used to run the Docker <b>nkubeio.nats-test</b> container on 
+    /// Used to run the Docker <b>nkubeio/nats-test</b> container on 
     /// the current machine as a test fixture while tests are being performed 
     /// and then deletes the container when the fixture is disposed.
     /// </summary>
@@ -58,23 +62,101 @@ namespace Neon.Xunit.Couchbase
         {
         }
 
-        public TestFixtureStatus Start()
+        /// <summary>
+        /// Starts a Couchbase container if it's not already running.  You'll generally want
+        /// to call this in your test class constructor instead of <see cref="ITestFixture.Start(Action)"/>.
+        /// </summary>
+        /// <param name="settings">Optional NATS settings.</param>
+        /// <param name="image">Optionally specifies the NATS container image (defaults to <b>nkubeio/nats-test:latest</b>).</param>
+        /// <param name="name">Optionally specifies the NATS container name (defaults to <c>nats-test</c>).</param>
+        /// <param name="env">Optional environment variables to be passed to the Couchbase container, formatted as <b>NAME=VALUE</b> or just <b>NAME</b>.</param>
+        /// <param name="username">Optional NATS username (defaults to <b>Administrator</b>).</param>
+        /// <param name="password">Optional NATS password (defaults to <b>password</b>).</param>
+        /// <returns>
+        /// <see cref="TestFixtureStatus.Started"/> if the fixture wasn't previously started and
+        /// this method call started it or <see cref="TestFixtureStatus.AlreadyRunning"/> if the 
+        /// fixture was already running.
+        /// </returns>
+        /// <remarks>
+        /// <note>
+        /// Some of the <paramref name="settings"/> properties will be ignored including 
+        /// <see cref="CouchbaseSettings.Servers"/>.  This will be replaced by the local
+        /// endpoint for the Couchbase container.  Also, the fixture will connect to the 
+        /// <b>test</b> bucket by default (unless another is specified).
+        /// </note>
+        /// <para>
+        /// There are three basic patterns for using this fixture.
+        /// </para>
+        /// <list type="table">
+        /// <item>
+        /// <term><b>initialize once</b></term>
+        /// <description>
+        /// <para>
+        /// The basic idea here is to have your test class initialize NATS
+        /// once within the test class constructor inside of the initialize action
+        /// with common state that all of the tests can access.
+        /// </para>
+        /// <para>
+        /// This will be quite a bit faster than reconfiguring Couchbase at the
+        /// beginning of every test and can work well for many situations.
+        /// </para>
+        /// </description>
+        /// </item>
+        /// <item>
+        /// <term><b>initialize every test</b></term>
+        /// <description>
+        /// For scenarios where NATS must be cleared before every test,
+        /// you can use the <see cref="Clear()"/> method to reset its
+        /// state within each test method, populate the database as necessary,
+        /// and then perform your tests.
+        /// </description>
+        /// </item>
+        /// <item>
+        /// <term><b>docker integrated</b></term>
+        /// <description>
+        /// The <see cref="NatsFixture"/> can also be added to the <see cref="DockerFixture"/>
+        /// and used within a swarm.  This is useful for testing multiple services and
+        /// also has the advantage of ensure that swarm/node state is fully reset
+        /// before the database container is started.
+        /// </description>
+        /// </item>
+        /// </list>
+        /// </remarks>
+        public TestFixtureStatus Start(
+            NatsSettings        settings  = null,
+            string              image     = "nkubeio/nats-test:latest",
+            string              name      = "nats-test",
+            string[]            env       = null,
+            string              username  = "Administrator",
+            string              password  = "password")
         {
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(image));
 
             return base.Start(
                 () =>
                 {
-                    StartInAction(settings, image, name, env, username, password, noPrimary);
+                    StartInAction(settings, image, name, env, username, password);
                 });
         }
 
         /// <summary>
         /// Actually starts NATS within the initialization <see cref="Action"/>.  You'll
-        /// generally want to use <see cref="Start()"/>
+        /// generally want to use <see cref="Start(NatsSettings, string, string, string[], string, string)"/>
         /// but this method is used internally or for special situations.
         /// </summary>
-        public void StartInAction()
+        /// <param name="settings">Optional Couchbase settings.</param>
+        /// <param name="image">Optionally specifies the Couchbase container image (defaults to <b>nkubeio/couchbase-test:latest</b>).</param>
+        /// <param name="name">Optionally specifies the Couchbase container name (defaults to <c>cb-test</c>).</param>
+        /// <param name="env">Optional environment variables to be passed to the Couchbase container, formatted as <b>NAME=VALUE</b> or just <b>NAME</b>.</param>
+        /// <param name="username">Optional Couchbase username (defaults to <b>Administrator</b>).</param>
+        /// <param name="password">Optional Couchbase password (defaults to <b>password</b>).</param>
+        public void StartInAction(
+            NatsSettings        settings  = null,
+            string              image     = "nkubeio/nats-test:latest",
+            string              name      = "nats-test",
+            string[]            env       = null,
+            string              username  = "Administrator",
+            string              password  = "password")
         {
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(image));
 
