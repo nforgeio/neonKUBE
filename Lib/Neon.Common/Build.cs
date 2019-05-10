f1c764d544d31221b1017a18543838c9a8ec349d
Jeff Lill <jeff@lilltek.com>
2019-05-09 18:20:01 -0700

    RELEASE: 0.4.5-alpha

--- Lib/Neon.Common/Build.cs
+++ Lib/Neon.Common/Build.cs
@@ -46,7 +46,7 @@ namespace Neon
         /// <summary>
         /// The released product version.
         /// </summary>
-        public const string ProductVersion = "0.4.4-alpha";
+        public const string ProductVersion = "0.4.5-alpha";
 
         /// <summary>
         /// The product license.
