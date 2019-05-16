0f016f797aac03b07621aa81027aabac4a93784f
Jeff Lill <jeff@lilltek.com>
2019-05-15 19:03:34 -0700

    [formatting]

--- Test/Test.Neon.Cadence/Test_Messages.cs
+++ Test/Test.Neon.Cadence/Test_Messages.cs
@@ -57,7 +57,7 @@ namespace TestCadence
         //---------------------------------------------------------------------
         // Implementation
 
-        CadenceFixture fixture;
+        CadenceFixture      fixture;
         CadenceConnection   connection;
         HttpClient          proxyClient;
 
