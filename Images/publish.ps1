523686d61c41569df0fbac388ee4b973c5ed14cc
Jeff Lill <jeff@lilltek.com>
2019-07-10 09:59:51 -0700

    [images]: Include [golang] in full image builds.

--- Images/publish.ps1
+++ Images/publish.ps1
@@ -132,6 +132,7 @@ if ($dotnetBase)
 
 if ($other)
 {
+	Publish "$image_root\\golang"
     Publish "$image_root\\nats"
     Publish "$image_root\\nats-streaming"
 	Publish "$image_root\\cadence-test"
