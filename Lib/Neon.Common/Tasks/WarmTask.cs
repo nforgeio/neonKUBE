49c3717033d6e07169421ac4c09625d3752ad653
Jeff Lill <jeff@lilltek.com>
2019-08-20 15:07:55 -0700

    [Neon.Common]: Implemented WarmTask and WarmTask<T>

--- Lib/Neon.Common/Tasks/WarmTask.cs
+++ Lib/Neon.Common/Tasks/WarmTask.cs
@@ -18,8 +18,6 @@
 // Code based on a MSDN article by Stephen Toub (MSFT):
 // http://blogs.msdn.com/b/pfxteam/archive/2012/02/12/10267069.aspx
 
-#if TODO
-
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
@@ -34,7 +32,7 @@ using Neon.Common;
 namespace Neon.Tasks
 {
     /// <summary>
-    /// Implements a specialized form of <see cref="Task"/> that allows operations to
+    /// Implements a specialized awaitable that returns <c>void</c> and allows operations to
     /// divide their implementation into two stages: the part that executes immediately
     /// and the part that executes after the task is actually awaited by the caller.
     /// </summary>
@@ -45,7 +43,7 @@ namespace Neon.Tasks
     /// before it is awaited.  This is usually the desired behavior.
     /// </para>
     /// <para>
-    /// Occassionally though, you may need more control over exactly when the operation is
+    /// Occasionally, you may need more control over exactly when the operation is
     /// performed or perhaps even splitting the operation into two stages, the <b>hot</b>
     /// stage that starts immediately and the completion stage that starts after the caller
     /// awaits the operation.  An operation that defers its implementation until after
@@ -53,17 +51,70 @@ namespace Neon.Tasks
     /// with a <b>hot</b> and <b>cold</b> parts is known as a <b>warm</b> task.
     /// </para>
     /// <para>
-    /// The <see cref="WarmTask"/> and <see cref="WarmTask{T}"/> classes provide an
-    /// easy way to implement both warm and cold tasks using asynchronous unctions.
+    /// This first example demonstrates how to create a task that doesn't schedule a
+    /// (cold) action until after the task is awaited.
     /// </para>
+    /// <code langiage="c#">
+    /// var coldCompleted = false;
+    /// var warmTask = new WarmTask(null,
+    ///     async () =>
+    ///     {
+    ///         coldCompleted = true;
+    ///         await Task.CompletedTask;
+    ///     });
+    ///     
+    /// // The warm task's cold action won't actually start until after the warm task
+    /// // is awaited, so this assertion will always pass.
+    /// 
+    /// await Task.Delay(TimeSpan.FromSections(1));
+    /// Assert.False(coldCompleted);
+    /// 
+    /// // Await the warm task now and then verify that the cold action executed.
+    /// 
+    /// await warmTask;
+    /// Assert.True(coldCompleted);
+    /// </code>
+    /// <para>
+    /// Here's another example with both hot and cold actions:
+    /// </para>
+    /// <code language="c#">
+    /// var hotCompleted  = false;
+    /// var coldCompleted = false;
+    /// 
+    /// var warmTask = new WarmTask(
+    ///     hotAction: async () =>
+    ///     {
+    ///         hotCompleted = true;
+    ///         await Task.CompletedTask;
+    ///     },
+    ///     coldAction: async () =>
+    ///     {
+    ///         coldCompleted = true;
+    ///         await Task.CompletedTask;
+    ///     });
+    ///     
+    /// // Wait for a bit and then confirm that the hot action 
+    /// // executed and the cold action did not.
+    /// 
+    /// await Task.Delay(TimeSpan.FromSections(1));
+    /// Assert.True(hotCompleted);
+    /// Assert.False(coldCompleted);
+    /// 
+    /// // Await the warm task.  This will cause the cold action
+    /// // to be scheduled.  We'll wait for a bit and verify that
+    /// // both actions now report being executed.
+    /// 
+    /// await warmTask;
+    /// 
+    /// Assert.True(hotCompleted);
+    /// Assert.True(coldCompleted);
+    /// </code>
     /// </remarks>
-    public class WarmTask
+    public class WarmTask : INotifyCompletion
     {
-        private readonly TaskCompletionSource<object> awaiter;
-
-        public WarmTask()
-        {
-        }
+        private TaskCompletionSource<object>    tcs;
+        private Func<Task>                      coldAction;
+        private Task                            hotTask;
 
         /// <summary>
         /// Constructs a <see cref="WarmTask"/> that immediately schedules the optional
@@ -76,21 +127,76 @@ namespace Neon.Tasks
         public WarmTask(Func<Task> hotAction, Func<Task> coldAction)
         {
             Covenant.Requires<ArgumentNullException>(coldAction != null);
+
+            this.coldAction = coldAction;
+
+            if (hotAction != null)
+            {
+                this.hotTask = Task.Run(() => hotAction());
+            }
+            else
+            {
+                this.hotTask = null;
+            }
+
+            this.tcs = new TaskCompletionSource<object>();
         }
 
         /// <summary>
         /// The magic method the C# compiler uses to implement async/await.
         /// </summary>
         /// <returns>The operation's awaiter.</returns>
-        public Task GetAwaiter() => awaiter.Task;
+        public TaskAwaiter GetAwaiter()
+        {
+            if (hotTask != null)
+            {
+                // We have a hot action and we need to ensure that it
+                // has completed before we start the cold action.
+
+                hotTask.ContinueWith(
+                    async state =>
+                    {
+                        await coldAction();
+                        tcs.SetResult(null);
+                    });
+            }
+            else
+            {
+                // There is no hot action, so we can immediately start the
+                // cold action when the WarmTask is awaited.
+
+                Task.Run(() => coldAction())
+                    .ContinueWith(
+                        async state =>
+                        {
+                            tcs.SetResult(null);
+                            await Task.CompletedTask;
+                        });
+            }
+
+            return ((Task)tcs.Task).GetAwaiter();
+        }
+
+        /// <inheritdoc/>
+        public bool IsCompleted { get; private set; }
+
+        /// <inheritdoc/>
+        public void OnCompleted(Action continuation)
+        {
+            if (continuation != null)
+            {
+                Task.Run(continuation);
+            }
+        }
     }
 
     /// <summary>
-    /// Implements a specialized form of <see cref="Task{T}"/> that allows operations to
-    /// divide their implementation into two stages: the part that executes immediately
-    /// and the part that executes after the task is actually awaited by the caller.
+    /// Implements a specialized awaitable that returns <typeparamref name="TResult"/> and 
+    /// allows operations to divide their implementation into two stages: the part that 
+    /// executes immediately and the part that executes after the task is actually awaited 
+    /// by the caller.
     /// </summary>
-    /// <typeparam name="TResult">The operation result type.</typeparam>
+    /// <typeparam name="TResult">Specifies the result type.</typeparam>
     /// <remarks>
     /// <para>
     /// Async operation implementations start <b>hot</b>.  This means that the async operation
@@ -98,7 +204,7 @@ namespace Neon.Tasks
     /// before it is awaited.  This is usually the desired behavior.
     /// </para>
     /// <para>
-    /// Occassionally though, you may need more control over exactly when the operation is
+    /// Occasionally, you may need more control over exactly when the operation is
     /// performed or perhaps even splitting the operation into two stages, the <b>hot</b>
     /// stage that starts immediately and the completion stage that starts after the caller
     /// awaits the operation.  An operation that defers its implementation until after
@@ -106,13 +212,69 @@ namespace Neon.Tasks
     /// with a <b>hot</b> and <b>cold</b> parts is known as a <b>warm</b> task.
     /// </para>
     /// <para>
-    /// The <see cref="WarmTask"/> and <see cref="WarmTask{T}"/> classes provide an
-    /// easy way to implement both warm and cold tasks using asynchronous unctions.
+    /// This first example demonstrates how to create a task that doesn't schedule a
+    /// (cold) action until after the task is awaited.
     /// </para>
+    /// <code langiage="c#">
+    /// var coldCompleted = false;
+    /// var warmTask = new WarmTask&lt;string&gt;(null,
+    ///     async () =>
+    ///     {
+    ///         coldCompleted = true;
+    ///         await Task.FromResult("Hello World!");
+    ///     });
+    ///     
+    /// // The warm task's cold action won't actually start until after the warm task
+    /// // is awaited, so this assertion will always pass.
+    /// 
+    /// await Task.Delay(TimeSpan.FromSections(1));
+    /// Assert.False(coldCompleted);
+    /// 
+    /// // Await the warm task now and then verify that the cold action executed.
+    /// 
+    /// Assert.Equal("Hello World!", await warmTask);
+    /// Assert.True(coldCompleted);
+    /// </code>
+    /// <para>
+    /// Here's another example with both hot and cold actions:
+    /// </para>
+    /// <code language="c#">
+    /// var hotCompleted  = false;
+    /// var coldCompleted = false;
+    /// 
+    /// var warmTask = new WarmTask&lt;string&gt;(
+    ///     hotAction: async () =>
+    ///     {
+    ///         hotCompleted = true;
+    ///         await Task.CompletedTask;
+    ///     },
+    ///     coldAction: async () =>
+    ///     {
+    ///         coldCompleted = true;
+    ///         await Task.FromResult("Hello World!");
+    ///     });
+    ///     
+    /// // Wait for a bit and then confirm that the hot action 
+    /// // executed and the cold action did not.
+    /// 
+    /// await Task.Delay(TimeSpan.FromSections(1));
+    /// Assert.True(hotCompleted);
+    /// Assert.False(coldCompleted);
+    /// 
+    /// // Await the warm task.  This will cause the cold action
+    /// // to be scheduled.  We'll wait for a bit and verify that
+    /// // both actions now report being executed.
+    /// 
+    /// Assert.Equal("Hello World!", await warmTask);
+    /// Assert.True(hotCompleted);
+    /// Assert.True(coldCompleted);
+    /// </code>
     /// </remarks>
-    public class WarmTask<TResult>
+    public class WarmTask<TResult> : INotifyCompletion
     {
-        private readonly TaskCompletionSource<TResult> awaiter;
+        private TaskCompletionSource<TResult>   tcs;
+        private Func<Task<TResult>>             coldAction;
+        private Task                            hotTask;
 
         /// <summary>
         /// Constructs a <see cref="WarmTask"/> that immediately schedules the optional
@@ -124,22 +286,71 @@ namespace Neon.Tasks
         /// <param name="coldAction">The cold action (required).</param>
         /// <remarks>
         /// <note>
-        /// The <paramref name="hotAction"/> returns only void because it's intended
-        /// only to initiate the operation where as the required <paramref name="coldAction"/>
-        /// is actually a function that returns the result.
+        /// <paramref name="coldAction"/> is the action that returns the overall task result.
         /// </note>
         /// </remarks>
-        public WarmTask(Action hotAction, Func<TResult> coldAction)
+        public WarmTask(Func<Task> hotAction, Func<Task<TResult>> coldAction)
         {
             Covenant.Requires<ArgumentNullException>(coldAction != null);
+
+            this.coldAction = coldAction;
+
+            if (hotAction != null)
+            {
+                this.hotTask = Task.Run(() => hotAction());
+            }
+            else
+            {
+                this.hotTask = null;
+            }
+
+            this.tcs = new TaskCompletionSource<TResult>();
         }
 
         /// <summary>
         /// The magic method the C# compiler uses to implement async/await.
         /// </summary>
         /// <returns>The operation's awaiter.</returns>
-        public Task<TResult> GetAwaiter() => awaiter.Task;
+        public TaskAwaiter<TResult> GetAwaiter()
+        {
+            if (hotTask != null)
+            {
+                // We have a hot action and we need to ensure that it
+                // has completed before we start the cold action.
+
+                hotTask.ContinueWith(
+                    async state =>
+                    {
+                        tcs.SetResult(await coldAction());
+                    });
+            }
+            else
+            {
+                // There is no hot action, so we can immediately start the
+                // cold action when the WarmTask is awaited.
+
+                Task.Run(() => coldAction())
+                    .ContinueWith(
+                        async state =>
+                        {
+                            tcs.SetResult(state.Result);
+                            await Task.CompletedTask;
+                        });
+            }
+
+            return tcs.Task.GetAwaiter();
+        }
+
+        /// <inheritdoc/>
+        public bool IsCompleted { get; private set; }
+
+        /// <inheritdoc/>
+        public void OnCompleted(Action continuation)
+        {
+            if (continuation != null)
+            {
+                Task.Run(continuation);
+            }
+        }
     }
 }
-
-#endif
