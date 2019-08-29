bea0dfd2a8b44dfb7fe69f8482afcab8a8c83562
Jeff Lill <jeff@lilltek.com>
2019-08-28 17:14:12 -0700

    [cadence]: Formatting

--- Lib/Neon.Cadence/CadenceClient.cs
+++ Lib/Neon.Cadence/CadenceClient.cs
@@ -41,6 +41,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Net.Http.Server;
+
 using Neon.Cadence.Internal;
 using Neon.Common;
 using Neon.Diagnostics;
