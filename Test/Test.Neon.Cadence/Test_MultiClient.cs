7b487ff1a82999bbc2b24c53f3848ec4edc5d3c5
Jeff Lill <jeff@lilltek.com>
2019-10-01 19:17:14 -0700

    [cadence]: TChannel experiments

--- Test/Test.Neon.Cadence/Test_MultiClient.cs
+++ Test/Test.Neon.Cadence/Test_MultiClient.cs
@@ -179,5 +179,262 @@ namespace TestCadence
                 }
             }
         }
+
+        //---------------------------------------------------------------------
+
+        private static CadenceClient    workerClient1;
+        private static CadenceClient    workerClient2;
+        private static CadenceClient    workerClient3;
+
+        //---------------------------------------
+
+        public interface IActivityWorker1 : IActivity
+        {
+            [ActivityMethod]
+            Task<bool> RunAsync();
+        }
+
+        public class ActivityWorker1 : ActivityBase, IActivityWorker1
+        {
+            public async Task<bool> RunAsync()
+            {
+                // Verify that this is executing on the correct client/worker instance.
+
+                return await Task.FromResult(Activity.Client.ClientId == workerClient1.ClientId);
+            }
+        }
+
+        public interface IWorkflowWorker1 : IWorkflow
+        {
+            [WorkflowMethod]
+            Task<bool> RunAsync(bool testActivity);
+        }
+
+        public class WorkflowWorker1 : WorkflowBase, IWorkflowWorker1
+        {
+            public async Task<bool> RunAsync(bool testActivity)
+            {
+                // Verify that this is executing on the correct client/worker instance.
+
+                if (Workflow.Client.ClientId != workerClient1.ClientId)
+                {
+                    return false;
+                }
+
+                if (testActivity)
+                {
+                    var stub = Workflow.NewActivityStub<IActivityWorker1>();
+
+                    if (!await stub.RunAsync())
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        //---------------------------------------
+
+        public interface IActivityWorker2 : IActivity
+        {
+            [ActivityMethod]
+            Task<bool> RunAsync();
+        }
+
+        public class ActivityWorker2 : ActivityBase, IActivityWorker2
+        {
+            public async Task<bool> RunAsync()
+            {
+                // Verify that this is executing on the correct client/worker instance.
+
+                return await Task.FromResult(Activity.Client.ClientId == workerClient2.ClientId);
+            }
+        }
+
+        public interface IWorkflowWorker2 : IWorkflow
+        {
+            [WorkflowMethod]
+            Task<bool> RunAsync(bool testActivity);
+        }
+
+        public class WorkflowWorker2 : WorkflowBase, IWorkflowWorker2
+        {
+            public async Task<bool> RunAsync(bool testActivity)
+            {
+                // Verify that this is executing on the correct client/worker instance.
+
+                if (Workflow.Client.ClientId != workerClient2.ClientId)
+                {
+                    return false;
+                }
+
+                if (testActivity)
+                {
+                    var stub = Workflow.NewActivityStub<IActivityWorker2>();
+
+                    if (!await stub.RunAsync())
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        //---------------------------------------
+
+        public interface IActivityWorker3 : IActivity
+        {
+            [ActivityMethod]
+            Task<bool> RunAsync();
+        }
+
+        public class ActivityWorker3 : ActivityBase, IActivityWorker3
+        {
+            public async Task<bool> RunAsync()
+            {
+                // Verify that this is executing on the correct client/worker instance.
+
+                return await Task.FromResult(Activity.Client.ClientId == workerClient3.ClientId);
+            }
+        }
+
+        public interface IWorkflowWorker3 : IWorkflow
+        {
+            [WorkflowMethod]
+            Task<bool> RunAsync(bool testActivity);
+        }
+
+        public class WorkflowWorker3 : WorkflowBase, IWorkflowWorker3
+        {
+            public async Task<bool> RunAsync(bool testActivity)
+            {
+                // Verify that this is executing on the correct client/worker instance.
+
+                if (Workflow.Client.ClientId != workerClient3.ClientId)
+                {
+                    return false;
+                }
+
+                if (testActivity)
+                {
+                    var stub = Workflow.NewActivityStub<IActivityWorker3>();
+
+                    if (!await stub.RunAsync())
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        [Fact]
+        [Trait(TestCategory.CategoryTrait, TestCategory.NeonCadence)]
+        public async Task ClientAndWorkers()
+        {
+            // Test the scenario where there multiple clients without
+            // workers that will be used to simulate apps that make calls
+            // on workflows and then create multiple clients that register
+            // different workflows and activities and then verify that
+            // each of the workerless clients are able to execute workflows
+            // and activities and that these end up being executed on the
+            // correct clients.
+
+            var clients = new List<CadenceClient>();
+
+            try
+            {
+                // Initialize the non-worker clients.
+
+                CadenceClient   client1;
+                CadenceClient   client2;
+                CadenceClient   client3;
+
+                clients.Add(client1 = await CadenceClient.ConnectAsync(fixture.Settings));
+                clients.Add(client2 = await CadenceClient.ConnectAsync(fixture.Settings));
+                clients.Add(client3 = await CadenceClient.ConnectAsync(fixture.Settings));
+
+                // Initialize the worker clients.
+
+                clients.Add(workerClient1 = await CadenceClient.ConnectAsync(fixture.Settings));
+                clients.Add(workerClient2 = await CadenceClient.ConnectAsync(fixture.Settings));
+                clients.Add(workerClient3 = await CadenceClient.ConnectAsync(fixture.Settings));
+
+                await workerClient1.RegisterActivityAsync<ActivityWorker1>();
+                await workerClient1.RegisterWorkflowAsync<WorkflowWorker1>();
+                await workerClient1.StartWorkerAsync();
+
+                await workerClient2.RegisterActivityAsync<ActivityWorker2>();
+                await workerClient2.RegisterWorkflowAsync<WorkflowWorker2>();
+                await workerClient2.StartWorkerAsync();
+
+                await workerClient3.RegisterActivityAsync<ActivityWorker3>();
+                await workerClient3.RegisterWorkflowAsync<WorkflowWorker3>();
+                await workerClient3.StartWorkerAsync();
+
+                // Execute each of the worker workflows WITHOUT the associated activities 
+                // from each client (both the working and non-working clients).
+
+                foreach (var client in clients)
+                {
+                    var stub = client.NewWorkflowStub<IWorkflowWorker1>();
+
+                    Assert.True(await stub.RunAsync(testActivity: false));
+                }
+
+                foreach (var client in clients)
+                {
+                    var stub = client.NewWorkflowStub<IWorkflowWorker2>();
+
+                    Assert.True(await stub.RunAsync(testActivity: false));
+                }
+
+                foreach (var client in clients)
+                {
+                    var stub = client.NewWorkflowStub<IWorkflowWorker3>();
+
+                    Assert.True(await stub.RunAsync(testActivity: false));
+                }
+
+                // Re-run the workflows calling the activities this time.
+
+                foreach (var client in clients)
+                {
+                    var stub = client.NewWorkflowStub<IWorkflowWorker1>();
+
+                    Assert.True(await stub.RunAsync(testActivity: true));
+                }
+
+                //foreach (var client in clients)
+                //{
+                //    var stub = client.NewWorkflowStub<IWorkflowWorker2>();
+
+                //    Assert.True(await stub.RunAsync(testActivity: true));
+                //}
+
+                //foreach (var client in clients)
+                //{
+                //    var stub = client.NewWorkflowStub<IWorkflowWorker3>();
+
+                //    Assert.True(await stub.RunAsync(testActivity: true));
+                //}
+            }
+            finally
+            {
+                foreach (var client in clients)
+                {
+                    client.Dispose();
+                }
+
+                workerClient1 = null;
+                workerClient2 = null;
+                workerClient3 = null;
+            }
+        }
     }
 }
