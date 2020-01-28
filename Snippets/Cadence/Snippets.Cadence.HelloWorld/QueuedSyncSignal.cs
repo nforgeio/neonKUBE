cefa9a417e1e300e1aae58fdd44048a834629887
Jeff Lill <jeff@lilltek.com>
2020-01-28 08:59:08 -0800

    [cadence]: Sync signal docs in progress

---
+++ Snippets/Cadence/Snippets.Cadence.HelloWorld/QueuedSyncSignal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Neon.Cadence;
+using Neon.Common;
+
+namespace Snippets_QueuedSignalWorkflow
+{
+    #region code
+    [WorkflowInterface(TaskList = "my-tasks")]
+    public interface IOrderWorkflow : IWorkflow
+    {
+        [WorkflowMethod]
+        Task ProcessOrderAsync();
+
+        [SignalMethod("cancel", Synchronous = true)]
+        Task<string> CancelOrderAsync(string reason);
+    }
+
+    [Workflow]
+    public class OrderWorkflow : WorkflowBase, IOrderWorkflow
+    {
+        private WorkflowQueue<SignalRequest<string>> queue;
+
+        public async Task ProcessOrderAsync()
+        {
+            queue = await Workflow.NewQueueAsync<SignalRequest<string>>();
+
+            var signal = await queue.DequeueAsync();
+            var reason = signal.Arg<string>("reason");
+
+            await signal.ReplyAsync("Order cancelled");
+        }
+
+        public async Task<string> CancelOrderAsync(string reason)
+        {
+            await queue.EnqueueAsync(new SignalRequest<string>());
+            return default(string);
+        }
+    }
+    #endregion
+}
---
+++ Snippets/Cadence/Snippets.Cadence.HelloWorld/SimpleSyncSignal.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Neon.Cadence;
+using Neon.Common;
+
+namespace Snippets_SimpleSignalWorkflow
+{
+    #region code
+    [WorkflowInterface(TaskList = "my-tasks")]
+    public interface IOrderWorkflow : IWorkflow
+    {
+        [WorkflowMethod]
+        Task ProcessOrderAsync();
+
+        [SignalMethod("cancel", Synchronous = true)]
+        Task<string> CancelOrderAsync(string reason);
+    }
+
+    [Workflow]
+    public class OrderWorkflow : WorkflowBase, IOrderWorkflow
+    {
+        private bool    cancelPending  = false;
+        private string  cancelReason   = null;
+        private bool    canCancel      = true;
+
+        public async Task ProcessOrderAsync()
+        {
+            // Implements order processing.  This is probably included
+            // one or more loops that poll [canCancel] while it's still
+            // possible to cancel the order.
+        }
+
+        public async Task<string> CancelOrderAsync(string reason)
+        {
+            if (!canCancel)
+            {
+                return "Order can no longer be be cancelled";
+            }
+
+            cancelPending = true;
+            cancelReason  = reason;
+
+            return "Order cancelled";
+        }
+    }
+    #endregion
+}
