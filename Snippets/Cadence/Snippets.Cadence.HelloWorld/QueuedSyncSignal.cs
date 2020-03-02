231d7169491632c857d7124b8cbe5e943fe7d971
Jeff Lill <jeff@lilltek.com>
2020-03-02 15:27:23 -0800

    [doc]: Impproved the synchronous signal documentation.

--- Snippets/Cadence/Snippets.Cadence.HelloWorld/QueuedSyncSignal.cs
+++ Snippets/Cadence/Snippets.Cadence.HelloWorld/QueuedSyncSignal.cs
@@ -25,23 +25,42 @@ namespace Snippets_QueuedSignalWorkflow
 
         public async Task ProcessOrderAsync()
         {
+            // Create queue to receive signal requests from the 
+            // [CancelOrderAsync()] synchronous signal method.
+
             queue = await Workflow.NewQueueAsync<SignalRequest<string>>();
+
+            // Wait for a signal request to be dequeued, and obtain the 
+            // "reason" argument.  This will be the "reason" parameter 
+            // value passed to [CancelOrderAsync()] below.
 
             var signal = await queue.DequeueAsync();
             var reason = signal.Arg<string>("reason");
 
-            await signal.ReplyAsync("Order cancelled");
+            // This line actually specifies the result to be be returned
+            // to the external code that sent the synchronous signal.
+
+            await signal.ReplyAsync($"Order cancelled due to: {reason}");
         }
 
         public async Task<string> CancelOrderAsync(string reason)
         {
-            await queue.EnqueueAsync(new SignalRequest<string>());
+            // This line creates a [SignalRequest] instance that somewhat
+            // magically initializes the [SignalRequest.Args] dictionary
+            // with the names and values of the parameters passed to this
+            // method.
+
+            var signalRequest = new SignalRequest<string>();
+
+            // Enqueue the signal request such that workflow method above
+            // can process it as part of the workflow logic.
+
+            await queue.EnqueueAsync(signalRequest);
 
             // Throwing this exception indicates to the Cadence client
             // that the signal result will be sent as a reply from
-            // the workflow code via the [SignalRequest] enqueued
-            // above rather than via a result returned by this
-            // signal method.
+            // the workflow code via the [SignalRequest] rather than 
+            // via a result returned by this signal method.
 
             throw new WaitForSignalReplyException();
         }
