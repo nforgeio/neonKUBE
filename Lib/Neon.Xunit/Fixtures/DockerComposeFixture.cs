31a295a5c80dfd2e525103db4378b6782e8a9e5b
Jeff Lill <jeff@lilltek.com>
2020-05-20 14:29:40 -0700

    #846: BREAKING: Change DockerStackFixture to DockerComposeFixtur

---
+++ Lib/Neon.Xunit/Fixtures/DockerComposeFixture.cs
@@ -0,0 +1,321 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    DockerComposeFixture.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2005-2020 by neonFORGE, LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Neon.Common;
+using Neon.IO;
+using Neon.Retry;
+using Org.BouncyCastle.Utilities;
+
+namespace Neon.Xunit
+{
+    /// <summary>
+    /// Used to run a <b>docker-compose</b> application on the current machine as a test 
+    /// fixture while tests are being performed and then deletes the applicatiuon when 
+    /// the fixture is disposed.
+    /// </summary>
+    /// <remarks>
+    /// <note>
+    /// <para>
+    /// <b>IMPORTANT:</b> The base Neon <see cref="TestFixture"/> implementation <b>DOES NOT</b>
+    /// support parallel test execution because fixtures may impact global machine state
+    /// like starting a Docker container, modifying the local DNS <b>hosts</b> file, configuring
+    /// environment variables or initializing a test database.
+    /// </para>
+    /// <para>
+    /// You should explicitly disable parallel execution in all test assemblies that
+    /// rely on test fixtures by adding a C# file called <c>AssemblyInfo.cs</c> with:
+    /// </para>
+    /// <code language="csharp">
+    /// [assembly: CollectionBehavior(DisableTestParallelization = true, MaxParallelThreads = 1)]
+    /// </code>
+    /// </note>
+    /// </remarks>
+    /// <threadsafety instance="true"/>
+    public class DockerComposeFixture : TestFixture
+    {
+        private string      composeFile;
+        private bool        keepOpen;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DockerComposeFixture()
+        {
+        }
+
+        /// <summary>
+        /// Finalizer.
+        /// </summary>
+        ~DockerComposeFixture()
+        {
+            Dispose(false);
+        }
+
+        /// <summary>
+        /// Returns the running application name or <c>null</c> if the compose file
+        /// has not been started.
+        /// </summary>
+        public string ApplicationName { get; private set; }
+
+        /// <summary>
+        /// <para>
+        /// Starts the fixture by running a Docker compose application.
+        /// </para>
+        /// <note>
+        /// You'll need to call <see cref="StartAsComposed(string, string, bool)"/>
+        /// instead when this fixture is being added to a <see cref="ComposedFixture"/>.
+        /// </note>
+        /// </summary>
+        /// <param name="name">Specifies the application name.</param>
+        /// <param name="composeFile">Specifies the contents of the <b>docker-compose.yml</b> file defining the application.</param>
+        /// <param name="keepOpen">
+        /// Optionally indicates that the application should continue to run after the fixture is disposed.  
+        /// This defaults to <c>false</c>.
+        /// </param>
+        /// <returns>
+        /// <see cref="TestFixtureStatus.Started"/> if the fixture wasn't previously started and
+        /// this method call started it or <see cref="TestFixtureStatus.AlreadyRunning"/> if the 
+        /// fixture was already running.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if this is not called from  within the <see cref="Action"/> method 
+        /// passed <see cref="ITestFixture.Start(Action)"/>
+        /// </exception>
+        /// <remarks>
+        /// <note>
+        /// You must specify a valid application <paramref name="name"/> so that the fixure
+        /// can remove any existing application with the same name before starting the new instance
+        /// of the application.  This is very useful during test debugging when the test might be 
+        /// interrupted during debugging or when <paramref name="keepOpen"/><c>=true</c>.
+        /// </note>
+        /// </remarks>
+        public TestFixtureStatus Start(string name, string composeFile, bool keepOpen = false)
+        {
+            return base.Start(
+                () =>
+                {
+                    StartAsComposed(name, composeFile, keepOpen);
+                });
+        }
+
+        /// <summary>
+        /// Used to start the fixture within a <see cref="ComposedFixture"/>.
+        /// </summary>
+        /// <param name="name">Specifies the application name.</param>
+        /// <param name="composeFile">Specifies the contents of the <b>docker-compose.yml</b> file defining the application.</param>
+        /// <param name="keepOpen">
+        /// Optionally indicates that the application should continue to run after the fixture is disposed.  
+        /// This defaults to <c>false</c>.
+        /// </param>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown if this is not called from  within the <see cref="Action"/> method 
+        /// passed <see cref="ITestFixture.Start(Action)"/>
+        /// </exception>
+        /// <remarks>
+        /// <note>
+        /// You must specify a valid application <paramref name="name"/> so that the fixure
+        /// can remove any existing application with the same name before starting the new instance
+        /// of the application.  This is very useful during test debugging when the test might be 
+        /// interrupted during debugging or when <paramref name="keepOpen"/><c>=true</c>.
+        /// </note>
+        /// </remarks>
+        public void StartAsComposed(string name, string composeFile, bool keepOpen = false)
+        {
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(composeFile), nameof(composeFile));
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(name), nameof(name));
+
+            base.CheckWithinAction();
+
+            if (IsRunning)
+            {
+                return;
+            }
+
+            this.ApplicationName = name;
+            this.composeFile     = composeFile;
+            this.keepOpen        = keepOpen;
+
+            StartApplication();
+        }
+
+        /// <summary>
+        /// Releases all associated resources.
+        /// </summary>
+        /// <param name="disposing">Pass <c>true</c> if we're disposing, <c>false</c> if we're finalizing.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (!base.IsDisposed)
+                {
+                    Reset();
+                }
+
+                GC.SuppressFinalize(this);
+            }
+        }
+
+        /// <summary>
+        /// Starts the application using the instance fields.
+        /// </summary>
+        private void StartApplication()
+        {
+            // Handle the special case where an earlier run of this application was
+            // not stopped because the developer was debugging and interrupted the
+            // the unit tests before the fixture was disposed or an application with
+            // the same name is already running for some other reason.
+
+            StopApplication();
+
+            // Start the application.  Note that we're going to write the compose file
+            // to a temporary file to accomplish this.
+
+            using (var tempFile = new TempFile(".yml"))
+            {
+                File.WriteAllText(tempFile.Path, composeFile);
+
+                var result = NeonHelper.ExecuteCapture("docker-compose.exe", new string[] { "-f", tempFile.Path, "--project-name", ApplicationName, "up", "--detach" });
+
+                if (result.ExitCode != 0)
+                {
+                    throw new Exception($"Cannot launch application [{ApplicationName}] - [exitcode={result.ExitCode}]: {result.ErrorText}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Stops any existing docker-compose application running with the same name as
+        /// the current application.
+        /// </summary>
+        private void StopApplication()
+        {
+            // We're going to list any existing compose containers and look for any
+            // with names like:
+            //
+            //      APPLICATION-NAME_*
+            //
+            // We're going to assume that an existing docker-compose application is
+            // currently running if we find any of these and remove the application.
+
+            var result = NeonHelper.ExecuteCapture("docker.exe", new object[] { "ps", "--all", "--format", "{{.Names}}" });
+
+            Covenant.Assert(result.ExitCode == 0, result.ErrorText);
+
+            var appNamePrefix  = $"{ApplicationName}_";
+            var containerNames = new List<string>();
+
+            using (var reader = new StringReader(result.AllText))
+            {
+                foreach (var line in reader.Lines())
+                {
+                    if (line.StartsWith(appNamePrefix))
+                    {
+                        containerNames.Add(line);
+                    }
+                }
+            }
+
+            if (containerNames.Count > 0)
+            {
+                // Looks like the application is running, so we'll need to stop it.
+                // We have a couple problems to deal with:
+                //
+                //      1. Normally you'd need the original compose file to bring the
+                //         application down cleanly via docker-compose.  The problem is
+                //         that we may not have the original compose file anymore because
+                //         the application could have been started long ago.
+                //
+                //         We could save the compose file as a temp file somewhere but
+                //         we're not going to do that because of #2 below.
+                //
+                //      2. It seems like many container images don't handle SIGTERM signals
+                //         and rely on the hosting environment to wait for seconds before
+                //         killing the container processes.  Docker defaults to waiting for
+                //         10 seconds.  This delay is annoying, and to make it worse, compose
+                //         seems to wait separately for each container it's closing, so 
+                //         stopping a three container application may take 30 seconds.
+                //
+                // To deal with both #1 and #2 above we're going to simply [rm --force] the application's
+                // containers explicitly as well as removing any lingering application networks.
+
+                // Remove the napplication containers:
+
+                result = NeonHelper.ExecuteCapture("docker.exe", new object[] { "rm", "--force", containerNames });
+
+                Covenant.Assert(result.ExitCode == 0, result.ErrorText);
+            }
+
+            // Remove any application (possibly orphaned) networks:
+
+            var networkNames = new List<string>();
+
+            result = NeonHelper.ExecuteCapture("docker.exe", new object[] { "network", "ls", "--format", "{{.Name}}" });
+
+            Covenant.Assert(result.ExitCode == 0, result.ErrorText);
+
+            using (var reader = new StringReader(result.AllText))
+            {
+                foreach (var line in reader.Lines())
+                {
+                    if (line.StartsWith(appNamePrefix))
+                    {
+                        networkNames.Add(line);
+                    }
+                }
+            }
+
+            if (networkNames.Count > 0)
+            {
+                result = NeonHelper.ExecuteCapture("docker.exe", new object[] { "network", "rm", networkNames });
+
+                Covenant.Assert(result.ExitCode == 0, result.ErrorText);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override void Reset()
+        {
+            // Remove the application if it's running and [keepOpen] is disabled.
+
+            if (!keepOpen)
+            {
+                StopApplication();
+            }
+
+            base.Reset();
+        }
+
+        /// <summary>
+        /// Restarts the application.  This is a handy way to deploy a fresh instance with the
+        /// same properties while running unit tests.
+        /// </summary>
+        public void Restart()
+        {
+            Reset();
+            StartApplication();
+        }
+    }
+}
