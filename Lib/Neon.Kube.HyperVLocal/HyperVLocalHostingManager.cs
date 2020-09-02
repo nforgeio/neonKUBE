46f7dce50ac7be3def85343ff11058cd70d99d29
Jeff Lill <jeff@lilltek.com>
2020-09-02 11:57:48 -0700

    #980: Hyper-V: Node network config doesn't happen sometimes

--- Lib/Neon.Kube.HyperVLocal/HyperVLocalHostingManager.cs
+++ Lib/Neon.Kube.HyperVLocal/HyperVLocalHostingManager.cs
@@ -633,15 +633,13 @@ namespace Neon.Kube
                     // to the node VM.
 
                     node.Status = $"mount: neon-node-prep iso";
-
-                    tempIso = KubeHelper.CreateNodePrepIso(node.Cluster.Definition, node.Metadata, secureSshPassword);
+                    tempIso     = KubeHelper.CreateNodePrepIso(node.Cluster.Definition, node.Metadata, secureSshPassword);
                     hyperv.InsertVmDvd(vmName, tempIso.Path);
 
                     // Start the VM for the first time with the mounted ISO.  The network
                     // configuration will happen automatically by the time we can connect.
 
                     node.Status = $"start: virtual machine (first boot)";
-
                     hyperv.StartVm(vmName);
 
                     // Update the node credentials to use the secure password and then wait for the node to boot.
