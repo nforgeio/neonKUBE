80d18212f39a5fd226cbced54451e6f1c1b27ded
Jeff Lill <jeff@lilltek.com>
2020-09-06 16:01:32 -0700

    [images]: Added the [aws-cli] image

---
+++ Images/aws-cli/publish.ps1
@@ -0,0 +1,48 @@
+﻿#------------------------------------------------------------------------------
+# FILE:         publish.ps1
+# CONTRIBUTOR:  Jeff Lill
+# COPYRIGHT:    Copyright (c) 2005-2020 by neonFORGE, LLC.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Builds the [nkubedev/aws-cli] image and pushes it to Docker Hub.
+#
+# NOTE: You must be logged into Docker Hub.
+#
+# Usage: powershell -file ./publish.ps1 [-all]
+
+param 
+(
+	[switch]$allVersions = $false,
+    [switch]$nopush = $false
+)
+
+#----------------------------------------------------------
+# Global Includes
+$image_root = "$env:NF_ROOT\\Images"
+. $image_root/includes.ps1
+#----------------------------------------------------------
+
+function Build
+{
+	# NOTE: This is a development-only image and is never pushed to [nkubeio].
+
+	# Build and publish the images.
+
+	. ./build.ps1
+    PushImage "nkubedev/aws-cli:latest"
+}
+
+$noImagePush = $nopush
+
+Build 
