d5f5b645a05e198f7b79458622eff33ad6bde8d0
Jeff Lill <jeff@lilltek.com>
2020-10-03 16:40:45 -0700

    #1007: Port SshProxy to Neon.SSH.NET

---
+++ Lib/Neon.SSH.NET/NeonExtensions/SshLinuxProxy.cs
@@ -0,0 +1,91 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    SshLinuxProxy.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2005-2020 by neonFORGE, LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Neon.Common;
+using Neon.Cryptography;
+using Neon.Diagnostics;
+using Neon.IO;
+using Neon.Net;
+using Neon.Retry;
+using Neon.Time;
+
+using ICSharpCode.SharpZipLib.Zip;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using Renci.SshNet;
+using Renci.SshNet.Common;
+
+namespace Neon.SSH
+{
+    /// <summary>
+    /// <para>
+    /// Uses an SSH/SCP connection to provide access to Linux machines to access
+    /// files, run commands, etc.  This is a simplified version of <see cref="SshLinuxProxy{TMetadata}"/> 
+    /// that doesn't require the specification of a metadata type.
+    /// </para>
+    /// <note>
+    /// This is class is <b>not intended</b> to be a <b>general purpose SSH wrapper</b> 
+    /// at this time.  It currently assumes that the remote side is running some variant
+    /// of Linux and it makes some global changes including disabling SUDO password prompts
+    /// for all users as well as creating some global directories.
+    /// </note>
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This class includes methods to invoke Linux commands on the node,
+    /// </para>
+    /// <para>
+    /// Call <see cref="SshLinuxProxy{TMetaData}.Dispose()"/> or <see cref="SshLinuxProxy{TMetaData}.Disconnect()"/> to close the connection.
+    /// </para>
+    /// <note>
+    /// You can use <see cref="SshLinuxProxy{TMetaData}.Clone()"/> to make a copy of a proxy that can be
+    /// used to perform parallel operations against the same machine.
+    /// </note>
+    /// </remarks>
+    /// <threadsafety instance="false"/>
+    public class SshLinuxProxy : SshLinuxProxy<object>, IDisposable
+    {
+        /// <summary>
+        /// Constructs a <see cref="SshLinuxProxy"/>.
+        /// </summary>
+        /// <param name="name">The display name for the server.</param>
+        /// <param name="address">The private cluster IP address for the server.</param>
+        /// <param name="credentials">The credentials to be used for establishing SSH connections.</param>
+        /// <param name="logWriter">The optional <see cref="TextWriter"/> where operation logs will be written.</param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown if <paramref name="name"/> or if <paramref name="credentials"/> is <c>null</c>.
+        /// </exception>
+        public SshLinuxProxy(string name, IPAddress address, SshCredentials credentials, TextWriter logWriter = null)
+            : base(name, address, credentials, logWriter)
+        {
+        }
+    }
+}
