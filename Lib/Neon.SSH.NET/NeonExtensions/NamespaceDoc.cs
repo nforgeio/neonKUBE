69ec472a397e81bbd9ff638755a0c65a72387108
Jeff Lill <jeff@lilltek.com>
2020-10-03 17:25:16 -0700

    RELEASE: neonLIBRARY v2.3.0

---
+++ Lib/Neon.SSH.NET/NeonExtensions/NamespaceDoc.cs
@@ -0,0 +1,41 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    NamespaceDoc.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2005-2020 by neonFORGE, LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Neon.Common;
+
+using Renci.SshNet;
+
+namespace Neon.SSH
+{
+    /// <summary>
+    /// This namespace includes the <see cref="SshLinuxProxy{TMetadata}"/>, <see cref="SshLinuxProxy"/>
+    /// and related classes that wrap and extend the base SSH.NET class with additional support for
+    /// managing remote Linux machines via SSH.
+    /// </summary>
+    public class NamespaceDoc
+    {
+    }
+}
