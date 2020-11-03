e08aad846086fdcc6fd519005fa2cb1775350299
Jeff Lill <jeff@lilltek.com>
2020-11-02 20:49:19 -0800

    [doc]: NeonService snippets

--- Snippets/Snippets.NeonService/Program-FullMealDeal.cs
+++ Snippets/Snippets.NeonService/Program-FullMealDeal.cs
@@ -1,15 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 using Neon.Common;
 using Neon.Service;
 
-using Prometheus;
+using Prometheus;               // From the [prometheus-net] nuget package
+using Prometheus.DotNetRuntime; // From the [prometheus-net.DotNetRuntime] nuget package
 
 namespace Service_FullMealDeal
 {
     public static class Program
     {
+        public async static Task Main(string[] args)
+        {
+            var service = new MyService();
+
+            service.Dependencies.Uris.Add(new Uri("http://waitforthis.com"));
+
+            service.MetricsOptions.Mode = MetricsMode.Scrape;
+            service.MetricsOptions.GetCollector = () => 
+                DotNetRuntimeStatsBuilder
+                    .Default()
+                    .StartCollecting();
+
+            await new MyService().RunAsync();
+        }
+    }
+
+    public class MyService : NeonService
+    {
+        public static Counter runTimeCounter = Metrics.CreateCounter("run-time", "Service run time in seconds.");
+
+        public MyService() : base("my-service")
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        protected async override Task<int> OnRunAsync()
+        {
+            while (!Terminator.CancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                runTimeCounter.Inc();
+            }
+
+            return 0;
+        }
     }
 }
--- Snippets/Snippets.NeonService/Program-Metrics.cs
+++ Snippets/Snippets.NeonService/Program-Metrics.cs
@@ -1,15 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 using Neon.Common;
 using Neon.Service;
 
-using Prometheus;
+using Prometheus;       // From the [prometheus-net] nuget package
 
-namespace Service_WithMetrics
+namespace Service_Metrics
 {
     public static class Program
     {
+        public async static Task Main(string[] args)
+        {
+            // You can enable Prometheus metrics with just one line of code!
+            //
+            // This enables scraping mode where the service is configured as an
+            // exporter that Prometheus can scrape periodically on port [9088],
+            // the standard NeonService port.
+            //
+            // You can customize this port using the [MetricsOptions.Port]
+            // property to avoid port conflicts by we recommend standarizing 
+            // on the default port when running in a container where port
+            // conflicts won't be an issue.
+
+            var service = new MyService();
+
+            service.MetricsOptions.Mode = MetricsMode.Scrape;
+
+            await new MyService().RunAsync();
+        }
+    }
+
+    public class MyService : NeonService
+    {
+        // Define a custom Prometheus counter.  This value will be able to be
+        // tracked on Prometheus related dashboards, alert rules, etc.
+        public static Counter runTimeCounter = Metrics.CreateCounter("run-time", "Service run time in seconds.");
+
+        public MyService() : base("my-service")
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        protected async override Task<int> OnRunAsync()
+        {
+            // We're just increment the runtime counter once a second until
+            // see see the termination signal.
+
+            while (!Terminator.CancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                runTimeCounter.Inc();
+            }
+
+            return 0;
+        }
     }
 }
