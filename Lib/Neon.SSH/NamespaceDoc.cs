2658356f8cd8d89bd9f4da4eef2b41dc5b3c1c1e
Jeff Lill <jeff@lilltek.com>
2020-11-04 15:10:34 -0800

    Neon.SSH documentation updates

--- Lib/Neon.SSH/NamespaceDoc.cs
+++ Lib/Neon.SSH/NamespaceDoc.cs
@@ -31,15 +31,24 @@ using Renci.SshNet;
 namespace Neon.SSH
 {
     /// <summary>
-    /// <note>
-    /// <b>EXPERIMENTAL:</b> The types in this namespace are probably not generally useful at this time,
-    /// and may never be.  We reserve the right to modify or refactor these at will.
-    /// </note>
     /// <para>
     /// This namespace includes the <see cref="LinuxSshProxy{TMetadata}"/>, <see cref="LinuxSshProxy"/>
-    /// and related classes that wrap and extend the base SSH.NET class with additional support for
-    /// managing remote Linux machines via SSH.
+    /// and related classes that wrap and extend the base SSH.NET library clients with additional support for
+    /// managing remote Linux machines via SSH including executing commands, scripts, uploading/downloading files, 
+    /// and performing idempotent operations.  Remote command executions and their results can also be logged
+    /// locally via a <see cref="TextWriter"/> (using a completely non-standard but still useful logging format).
     /// </para>
+    /// <para>
+    /// The other major type is <see cref="CommandBundle"/>.  Command bundles provide a way to upload a 
+    /// script or executable to a temporary working directory and then run the script or program in the 
+    /// context of the working directory so the script or program will have access to the files.Command 
+    /// bundle executions can also tolerate transient network disconnections.
+    /// </para>
+    /// <note>
+    /// This package has been tested against remote machines running Ubuntu 18.04+ and will probably run
+    /// fine on many other Debian-based distributions.  Redhat and other non-Debian distributions probably
+    /// won't be compatible.
+    /// </note>
     /// </summary>
     public class NamespaceDoc
     {
