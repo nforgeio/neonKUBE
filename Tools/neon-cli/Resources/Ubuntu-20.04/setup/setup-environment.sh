79aa51831ef11648e16cd8d7427e2b979e7825c8
Jeff Lill <jeff@lilltek.com>
2021-01-03 17:18:15 -0800

    Update copyright statements

--- Tools/neon-cli/Resources/Ubuntu-20.04/setup/setup-environment.sh
+++ Tools/neon-cli/Resources/Ubuntu-20.04/setup/setup-environment.sh
@@ -2,7 +2,7 @@
 #------------------------------------------------------------------------------
 # FILE:         setup-environment.sh
 # CONTRIBUTOR:  Jeff Lill
-# COPYRIGHT:    Copyright (c) 2005-2020 by neonFORGE LLC.  All rights reserved.
+# COPYRIGHT:    Copyright (c) 2005-2021 by neonFORGE LLC.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
