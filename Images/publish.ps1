e99936f21c9555b4c40213e821034033e7eef848
Jeff Lill <jeff@lilltek.com>
2021-01-19 18:07:43 -0800

    [images}: Removed unnecessary logging related images.

--- Images/publish.ps1
+++ Images/publish.ps1
@@ -115,8 +115,7 @@ if ($base)
 {
     # Other base images:
 
-    Publish "$image_root\\td-agent"
-    Publish "$image_root\\td-agent-bit"
+    # Its lonely here!
 }
 
 if ($other)
@@ -137,6 +136,4 @@ if ($services)
     Publish "$image_root\\test-cadence"
     Publish "$image_root\\test-temporal"
     Publish "$image_root\\neon-cluster-manager"
-    Publish "$image_root\\neon-log-collector"
-    Publish "$image_root\\neon-log-host"
 }
