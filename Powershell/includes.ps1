b731c5c0b77fcbcd27d5973052f49e9a83cb77c0
Jeff Lill <jeff@lilltek.com>
2021-02-23 21:46:33 -0800

    [build]: 1Password scripts

--- Powershell/includes.ps1
+++ Powershell/includes.ps1
@@ -52,3 +52,167 @@ function GitBranch
 
     return $branch
 }
+
+#------------------------------------------------------------------------------
+# Signs into into 1Password so you'll be able to access secrets.  This will start
+# a 30 minute session that will allow you to use the 1Password [op] CLI without
+# requiring additional credentials.  We recommend that you call this at the 
+# beginning of all CI/CD operations and then immediately grab all of the secrets 
+# you'll need for that operation.  This way you won't need to worry about the
+# 30 minute session expiring.
+#
+# This script requires the following environment variables to be defined:
+#
+#   NEON_OP_DOMAIN
+#   NEON_OP_EMAIL_ADDRESS
+#   NEON_OP_SECRET_KEY
+#   NEON_OP_SIGNIN_ADDRESS
+#   NEON_OP_MASTER_PASSWORD     * Optional
+#
+# NEON_OP_MASTER_PASSWORD is optional and you'll be prompted for this (once for 
+# the current Powershell session) if this isn't defined.  The other variables
+# are initialized by [$/buildenv.cmd] so re-run that as administrator if necessary.
+
+function OpSignin
+{
+    $errorHelp = " environment variable is not defined.  Please run [$/buildenv.cmd] as administrator."
+
+    if ($env:NEON_OP_DOMAIN -eq "")
+    {
+        "[NEON_OP_DOMAIN]" + $errorHelp
+        exit 1
+    }
+
+    if ($env:NEON_OP_EMAIL_ADDRESS -eq "")
+    {
+        "[NEON_OP_EMAIL_ADDRESS]" + $errorHelp
+        exit 1
+    }
+
+    if ($env:NEON_OP_SECRET_KEY -eq "")
+    {
+        "[NEON_OP_SECRET_KEY]" + $errorHelp
+        exit 1
+    }
+
+    if ($env:NEON_OP_SIGNIN_ADDRESS -eq "")
+    {
+        "[NEON_OP_EMAIL_ADDRESS]" + $errorHelp
+        exit 1
+    }
+
+    if ($env:NEON_OP_SESSION_TOKEN -eq "" -or $env:NEON_OP_SESSION_TOKEN -eq $null)
+    {
+        ""
+        $env:NEON_OP_SESSION_TOKEN = $(& op signin --raw $env:NC_OP_DOMAIN)
+        ""
+    }
+}
+
+#------------------------------------------------------------------------------
+# Handles returning the user's default vault when one isn't specified.
+#
+# Usage: OpGetVault NAME [VAULT]
+#
+# ARGUMENTS:
+#
+#       NAME        - Specifies the password item name in 1Password
+#       VAULT       - Optionally specifies the 1Password vault holding
+#                     the item.  This defaults to the user's defaults
+#                     vault.
+
+function OpGetVault
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0, Mandatory=1)]
+        [AllowEmptyString()]
+        [string]$vault
+    )
+
+    if (($vault -eq $null) -or ($vault -eq ""))
+    {
+        $vault = $env:NC_USER
+
+        if (($vault -eq $null) -or ($vault -eq ""))
+        {
+            ""
+            "*** ERROR: NC_USER environment variable doesn't exist.  You may need to re-run [buildenv.cmd] as administrator."
+            ""
+            exit 1
+        }
+
+        $vault = "user-" + $vault
+    }
+
+    return $vault
+}
+
+#------------------------------------------------------------------------------
+# Retrieves [password] field from from a password in the user's 1Password account.  
+# This will be written to STDOUT as plaintext.
+#
+# Usage: OpGetPassword NAME [VAULT]
+#
+# ARGUMENTS:
+#
+#       NAME        - Specifies the password item name in 1Password
+#       VAULT       - Optionally specifies the 1Password vault holding
+#                     the item.  This defaults to the user's defaults
+#                     vault.
+#
+# Each user will be provisioned with a default vault named [user-USERNAME]
+# where USERNAME is the from the user's primary neonFORGE Office email,
+# as in [sally] from [sally@neonforge.com].  USERNAME is persisted as the
+# NC_USER environment variable by the neonCLOUD [build.env.cmd] script. 
+# Note that this variable is required when no vault is specified.
+
+function OpGetPassword
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0, Mandatory=1)]
+        [string]$passwordName,
+        [Parameter(Position=1, Mandatory=0)]
+        [string]$vault = $null
+    )
+
+    $vault = OpGetVault $vault
+
+    op --session $env:NEON_OP_SESSION_TOKEN get item $passwordName --vault $vault --fields password
+}
+
+
+#------------------------------------------------------------------------------
+# Retrieves the [value] field from from the user's 1Password account.  This will
+# be written to STDOUT as plaintext.
+#
+# Usage: OpGetValue NAME [VAULT]
+#
+# ARGUMENTS:
+#
+#       NAME        - Specifies the password item name in 1Password
+#       VAULT       - Optionally specifies the 1Password vault holding
+#                     the item.  This defaults to the user's defaults
+#                     vault.
+#
+# Each user will be provisioned with a default vault named [user-USERNAME]
+# where USERNAME is the from the user's primary neonFORGE Office email,
+# as in [sally] from [sally@neonforge.com].  USERNAME is persisted as the
+# NC_USER environment variable by the neonCLOUD [build.env.cmd] script. 
+# Note that this variable is required when no vault is specified.
+
+function OpGetValue
+{
+    [CmdletBinding()]
+    param (
+        [Parameter(Position=0, Mandatory=1)]
+        [string]$passwordName,
+        [Parameter(Position=1, Mandatory=0)]
+        [string]$vault = $null
+    )
+
+    $vault = OpGetVault $vault
+
+    op --session $env:NEON_OP_SESSION_TOKEN get item $passwordName --vault $vault --fields value
+}
