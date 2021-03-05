b1c934dbd8810a5a8d263f239243c63203996558
Jeff Lill <jeff@lilltek.com>
2021-03-05 02:04:56 -0800

    #1146: Removed the Neon.SSH.NET sources

--- Lib/Neon.SSH.NET/Sftp/ISftpResponseFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text;
-
-namespace Renci.SshNet.Sftp
-{
-    internal interface ISftpResponseFactory
-    {
-        SftpMessage Create(uint protocolVersion, byte messageType, Encoding encoding);
-    }
-}
