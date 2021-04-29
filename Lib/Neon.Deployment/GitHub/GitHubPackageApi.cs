51cfa1dc1417044d42fbccc6b4f8f55f084dbf45
Marcus Bowyer <marcus@loopie.io>
2021-04-29 08:28:06 -0700

    implemented github methods

--- Lib/Neon.Deployment/GitHub/GitHubPackageApi.cs
+++ Lib/Neon.Deployment/GitHub/GitHubPackageApi.cs
@@ -19,9 +19,19 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
 
 using Neon.Common;
+using Neon.Net;
+
+using AngleSharp;
+using AngleSharp.Html.Parser;
+using AngleSharp.Html.Dom;
+using System.Linq;
+using AngleSharp.Dom;
 
 namespace Neon.Deployment
 {
@@ -47,11 +57,52 @@ namespace Neon.Deployment
     {
         private const string githubRestUri = "https://api.github.com/";
 
+        private JsonClient jsonClient;
+
+        private IBrowsingContext browsingContext;
+
         /// <summary>
         /// Internal constructor.
         /// </summary>
         internal GitHubPackageApi()
         {
+            GitHub.GetCredentials();
+            GitHub.EnsureCredentials();
+
+            // Create json client for interacting with the GitHub API.
+            jsonClient = new JsonClient()
+            {
+                BaseAddress = new Uri(githubRestUri),
+            };
+
+            jsonClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("token", GitHub.PersonalAccessToken);
+            jsonClient.DefaultRequestHeaders.Add("User-Agent", "neondevbot");
+
+            // Create an AngleSharp client for screen scraping.
+            
+            // Load default configuration
+            var config = Configuration.Default.WithDefaultLoader().WithDefaultCookies();
+            
+            // Create a new browsing context
+            browsingContext = BrowsingContext.New(config);
+
+            Login();
+        }
+
+        /// <summary>
+        /// Logs in to GitHub.
+        /// </summary>
+        /// <returns></returns>
+        public void Login()
+        {
+            var document = browsingContext.OpenAsync("https://github.com/login").Result;
+            var form = browsingContext.Active.QuerySelector<IHtmlFormElement>("#login > div.auth-form-body.mt-3 > form");
+            var resultDocument = form.SubmitAsync(new { login = GitHub.UserCredentials.Username, password = GitHub.UserCredentials.Password }).Result;
+
+            if (resultDocument.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception("Login failed");
+            }
         }
 
         /// <summary>
@@ -60,14 +111,54 @@ namespace Neon.Deployment
         /// <param name="organization">The GitHub organization name.</param>
         /// <param name="pattern">The matching pattern (see <see cref="NeonHelper.FileWildcardRegex(string)"/>).</param>
         /// <param name="packageType">Optionally specifies the package type.  This defaults to <see cref="GitHubPackageType.Container"/>.</param>
+        /// <param name="visibility">Optionally specifies the visibility of the package.  This defaults to <see cref="GitHubPackageVisibility.All"/></param>
         /// <returns>The list of package information as a list of <see cref="GitHubPackage"/> instance.</returns>
-        public List<GitHubPackage> List(string organization, string pattern = null, string packageType = GitHubPackageType.Container)
+        public async Task<List<GitHubPackage>> List(
+            string organization, 
+            string pattern = null, 
+            string packageType = GitHubPackageType.Container,
+            string visibility = GitHubPackageVisibility.All)
         {
-            GitHub.EnsureCredentials();
+            var packages = new List<GitHubPackage>();
 
-            // $todo(marcusbooyah): This will need to be screen-scraped.
+            var document = await browsingContext.OpenAsync($"https://github.com/orgs/{organization}/packages?type={packageType}&q={pattern}&visibility={visibility}");
 
-            throw new NotImplementedException();
+            var packageslist = document.QuerySelector<IHtmlUnorderedListElement>("#org-packages > div.Box > ul");
+
+            if (packageslist == null || packageslist.Children.Count() == 0)
+            {
+                return packages;
+            }
+
+            var done = false;
+
+            while (!done)
+            {
+                foreach (var p in packageslist.Children)
+                {
+                    packages.Add(
+                        new GitHubPackage()
+                        {
+                            Name = p.QuerySelector<IHtmlAnchorElement>("div > div.flex-auto > a").Title
+                        });
+                }
+
+                var next = document.QuerySelector<IHtmlAnchorElement>("#org-packages > div.paginate-container > div > a.next_page");
+
+                if (next != null
+                    && !next.ClassList.Contains("disabled"))
+                {
+                    document = await next.NavigateAsync();
+
+                    packageslist = document.QuerySelector<IHtmlUnorderedListElement>("#org-packages > div.Box > ul");
+                }
+                else
+                {
+                    done = true;
+                }
+            }
+
+            return packages;
         }
 
         /// <summary>
@@ -77,7 +168,7 @@ namespace Neon.Deployment
         /// <param name="organization">The GitHub organization name.</param>
         /// <param name="nameOrPattern">The package name or matching pattern (see <see cref="NeonHelper.FileWildcardRegex(string)"/>).</param>
         /// <param name="packageType">Optionally specifies the package type.  This defaults to <see cref="GitHubPackageType.Container"/>.</param>
-        public void Delete(string organization, string nameOrPattern, string packageType = GitHubPackageType.Container)
+        public async Task DeleteAsync(string organization, string nameOrPattern, string packageType = GitHubPackageType.Container)
         {
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(organization), nameof(organization));
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(nameOrPattern), nameof(nameOrPattern));
@@ -85,14 +176,7 @@ namespace Neon.Deployment
 
             GitHub.EnsureCredentials();
 
-            // $todo(marcusbooyah): GitHub has a REST API for this:
-            //
-            //      https://docs.github.com/en/rest/reference/packages#delete-a-package-for-an-organization
-            //
-            // The examples don't show this but it looks like you'll just add the GITHUB_PAT in the
-            // [Authentication] header as a bearer token.
-
-            throw new NotImplementedException();
+            await jsonClient.DeleteAsync($"/orgs/{organization}/packages/{packageType}/{nameOrPattern}");
         }
 
         /// <summary>
@@ -101,18 +185,40 @@ namespace Neon.Deployment
         /// </summary>
         /// <param name="organization">The GitHub organization name.</param>
         /// <param name="nameOrPattern">The package name or matching pattern.</param>
+        /// <param name="visibility">The visibility to set the package to.</param>
         /// <param name="packageType">Optionally specifies the package type.  This defaults to <see cref="GitHubPackageType.Container"/>.</param>
-        public void MakePublic(string organization, string nameOrPattern, string packageType = GitHubPackageType.Container)
+        public async Task SetVisibilityAsync(
+            string organization, 
+            string nameOrPattern,
+            string visibility,
+            string packageType = GitHubPackageType.Container)
         {
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(organization), nameof(organization));
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(nameOrPattern), nameof(nameOrPattern));
+            Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(visibility), nameof(visibility));
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(packageType), nameof(packageType));
 
             GitHub.EnsureCredentials();
 
-            // $todo(marcusbooyah): This will need to be screen-scraped.
+            var page = await browsingContext.OpenAsync($"https://github.com/orgs/{organization}/packages/container/{nameOrPattern}/settings");
 
-            throw new NotImplementedException();
+            var form = page.QuerySelector<IHtmlFormElement>("#js-pjax-container > div > div.container-lg.p-responsive.clearfix > div.container-lg.d-flex > div.col-9 > div.Box.Box--danger > ul > li:nth-child(1) > div > details > details-dialog > div.Box-body.overflow-auto > form");
+
+            var selector = form.QuerySelector<IHtmlInputElement>("#visibility_public");
+            selector.IsChecked = visibility == GitHubPackageVisibility.Public;
+
+            selector = form.QuerySelector<IHtmlInputElement>("#visibility_private");
+            selector.IsChecked = visibility == GitHubPackageVisibility.Private;
+
+            selector = form.QuerySelector<IHtmlInputElement>("#visibility_internal");
+            selector.IsChecked = visibility == GitHubPackageVisibility.Internal;
+
+            var resultDocument = await form.SubmitAsync(new { verify = nameOrPattern });
+
+            if (resultDocument.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception($"Failed to make package {visibility}.");
+            }
         }
     }
 }
---
+++ Lib/Neon.Deployment/GitHub/GitHubPackageVisibility.cs
@@ -0,0 +1,51 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    GitHubPackageVisibility.cs
+// CONTRIBUTOR: Marcus Bowyer
+// COPYRIGHT:	Copyright (c) 2005-2021 by neonFORGE LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Diagnostics.Contracts;
+using System.IO;
+
+using Neon.Common;
+
+namespace Neon.Deployment
+{
+    /// <summary>
+    /// Enumerates the supported GitHub package visibility types.
+    /// </summary>
+    public static class GitHubPackageVisibility
+    {
+        /// <summary>
+        /// All packages.
+        /// </summary>
+        public const string All = "all";
+
+        /// <summary>
+        /// Public packages.
+        /// </summary>
+        public const string Public = "public";
+
+        /// <summary>
+        /// Private packages.
+        /// </summary>
+        public const string Private = "private";
+
+        /// <summary>
+        /// Internal packages.
+        /// </summary>
+        public const string Internal = "internal";
+    }
+}
