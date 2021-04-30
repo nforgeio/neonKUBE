d86393519a2811fd1649c9550c316f3c844cfdc1
Marcus Bowyer <marcus@loopie.io>
2021-04-30 13:14:05 -0700

    comment this out for now

--- Images/publish.ps1
+++ Images/publish.ps1
@@ -143,6 +143,6 @@ if ($services)
 {
     Publish "$image_root\\neon-allow-testing"
     Publish "$image_root\\neon-cluster-operator"
-    Publish "$image_root\\neon-setup-grafana"
-    Publish "$image_root\\neon-setup-harbor"
+    #Publish "$image_root\\neon-setup-grafana"
+    #Publish "$image_root\\neon-setup-harbor"
 }
