9be77a8292862a064e9fce61349a3224b160e1ef
Marcus Bowyer <marcus@loopie.io>
2021-04-30 13:54:04 -0700

    added grafana and harbor setup jobs

---
+++ Services/neon-setup-harbor/Program.cs
@@ -0,0 +1,34 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Program.cs
+// CONTRIBUTOR: Marcus Bowyer
+// COPYRIGHT:   Copyright (c) 2005-2021 by neonFORGE LLC.  All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Neon.Common;
+using Neon.Kube;
+using Neon.Service;
+
+using k8s;
+using k8s.Models;
+
+namespace NeonSetupHarbor
+{
+    /// <summary>
+    /// The Neon cluster initialization operator.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The program entrypoint.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>The tracking <see cref="Task"/>.</returns>
+        public static async Task Main(string[] args)
+        {
+            await new Service(NeonServices.SetupHarbor, serviceMap: NeonServiceMap.Production).RunAsync();
+        }
+    }
+}
