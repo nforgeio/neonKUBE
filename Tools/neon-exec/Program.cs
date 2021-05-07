398a0508f9a32b4d9ad9441e7b048aa1dccf2c5f
Jeff Lill <jeff@lilltek.com>
2021-05-07 15:03:36 -0700

    [tools]: added [neon-exec]

---
+++ Tools/neon-exec/Program.cs
@@ -0,0 +1,135 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Program.cs
+// CONTRIBUTOR: Jeff Lill
+// COPYRIGHT:	Copyright (c) 2005-2021 by neonFORGE LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeonCli
+{
+    /// <summary>
+    /// Hosts the program entrypoint.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The program entrypoint.
+        /// </summary>
+        /// <param name="args">The command line arguments. (the "--test" argument implements a primitve test mode).</param>
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var testMode = args.Any(arg => arg == "--test");
+
+                // Discover the file name of this executable and the related .INI file.
+
+                var assembly = Assembly.GetExecutingAssembly();
+                var exePath  = assembly.CodeBase;
+
+                if (exePath.StartsWith("file://"))
+                {
+                    exePath = exePath.Substring(8);
+                }
+
+                var exeFolder = Path.GetDirectoryName(exePath);
+                var exeName   = Path.GetFileNameWithoutExtension(exePath);
+                var iniPath   = Path.Combine(exeFolder, $"{exeName}.ini");
+
+                if (testMode)
+                {
+                    // We'll overwrite the INI file with the path to [test.cmd].
+
+                    File.WriteAllText(iniPath, $"{exeFolder}/test.cmd");
+                }
+
+                // The first line of the INI file should be the path to the
+                // CMD script being executed.
+
+                if (!File.Exists(iniPath))
+                {
+                    Console.Error.WriteLine($"*** ERROR: INI file at [{iniPath}] does not exist.");
+                    Environment.Exit(1);
+                }
+
+                var cmdPath = File.ReadAllLines(iniPath).First().Trim();
+
+                if (String.IsNullOrWhiteSpace(cmdPath))
+                {
+                    Console.Error.WriteLine($"*** ERROR: [{iniPath}] does not specifiy a script path.");
+                    Environment.Exit(1);
+                }
+
+                if (!File.Exists(cmdPath))
+                {
+                    Console.Error.WriteLine($"*** ERROR: Script file at [{cmdPath}] does not exist.");
+                    Environment.Exit(1);
+                }
+
+                // Start the script.
+
+                var processInfo = new ProcessStartInfo("cmd.exe")
+                {
+                    Arguments        = $"/c \"{cmdPath}\"",
+                    UseShellExecute  = false,
+                    WorkingDirectory = Path.GetDirectoryName(cmdPath)
+                };
+
+                var process = new Process()
+                {
+                    StartInfo = processInfo
+                };
+
+                process.Start();
+                process.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine($"*** {e.GetType().FullName}: {e.Message}");
+                Environment.Exit(1);
+            }
+
+            Environment.Exit(0);
+        }
+
+        /// <summary>
+        /// Appends a line of text to a fixed log file at <b>C:\Temp\log.txt</b>.  This is
+        /// used internally for debugging.
+        /// </summary>
+        /// <param name="line">The line of text.</param>
+        private static void Log(string line = null)
+        {
+            var path   = @"C:\Temp\log.txt";
+            var folder = Path.GetDirectoryName(path);
+
+            line = line ?? string.Empty;
+
+            Directory.CreateDirectory(folder);
+            File.AppendAllLines(path, new string[] { line });
+        }
+    }
+}
---
+++ Tools/neon-exec/README.md
@@ -0,0 +1,25 @@
+﻿# neon-exec
+
+**NOTE:** I couldn't get this to work.  It appears that Windows won't automatically start an app requiring elevated permissions, even when UAC is disabled.
+
+Sometimes it's necessary to automatically run a CMD script with elevated permissions when the user logs into a Windows session.  
+Starting a script automatcically can be done by copying the CMD file into:
+```
+C:\Users\devbot.JOBRUNNER\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+```
+but the problem is that it's not possible to set _Run As Administrator_ on script files.  This tool can be used to 
+workaround this limitation.
+
+**Here's how to configure this**
+
+1. Relocate your CMD script somewhere outside of the Windows startup folder
+2. Copy the **neon-exec.exe** binary from one of the bin folders (doesn't matter which one)
+3. Copy **neon-exec.exe** to the Windows startup folder 
+4. Add a **neon-exec.ini** file to the startup folder and add the fully qualified path to
+   your CMD file as the first line
+5. Configure **neon-exec.exe** to _Run As Administrator_
+
+That's it.  If you need to do this for multiple scripts, you can copy new exe and ini files
+with different names to the folder, like **myapp.exe** and **myapp.ini**.
+
+**NOTE:** This app looks for an INI file with the same name as the executable.
