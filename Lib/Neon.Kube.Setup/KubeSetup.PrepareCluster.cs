fa72dfb70248bc6973dd47bedc4e0f4fed70524e
Marcus Bowyer <marcus@loopie.io>
2021-06-20 21:10:06 -0700

    added subdomain creation for WSL2 clusters

--- Lib/Neon.Kube.Setup/KubeSetup.PrepareCluster.cs
+++ Lib/Neon.Kube.Setup/KubeSetup.PrepareCluster.cs
@@ -39,6 +39,7 @@ using Neon.Collections;
 using Neon.Common;
 using Neon.Cryptography;
 using Neon.IO;
+using Neon.Net;
 using Neon.Retry;
 using Neon.SSH;
 using Neon.Tasks;
@@ -76,6 +77,7 @@ namespace Neon.Kube
         /// Optionally specifies that the operation is to be performed in <b>automation mode</b>, where the
         /// current neonDESKTOP state will not be impacted.
         /// </param>
+        /// <param name="headendUri">Optionally override the headend service URI</param>
         /// <returns>The <see cref="ISetupController"/>.</returns>
         /// <exception cref="KubeException">Thrown when there's a problem.</exception>
         public static ISetupController CreateClusterPrepareController(
@@ -86,7 +88,8 @@ namespace Neon.Kube
             bool                        unredacted            = false, 
             bool                        debugMode             = false, 
             string                      baseImageName         = null,
-            bool                        automate              = false)
+            bool                        automate              = false,
+            string                      headendUri            = "https://headend.neoncloud.io")
         {
             Covenant.Requires<ArgumentNullException>(clusterDefinition != null, nameof(clusterDefinition));
             Covenant.Requires<ArgumentNullException>(!string.IsNullOrEmpty(nodeImageUri), nameof(nodeImageUri));
@@ -190,6 +193,7 @@ namespace Neon.Kube
             controller.Add(KubeSetupProperty.HostingManager, hostingManager);
             controller.Add(KubeSetupProperty.HostingEnvironment, hostingManager.HostingEnvironment);
             controller.Add(KubeSetupProperty.AutomationFolder, automationFolder);
+            controller.Add(KubeSetupProperty.HeadendUri, headendUri);
 
             // Configure the cluster preparation steps.
 
@@ -333,6 +337,26 @@ namespace Neon.Kube
                 (state, node) =>
                 {
                     node.PrepareNode(controller);
+                });
+
+            controller.AddGlobalStep("generate neoncluster.io domain",
+                async (controller) =>
+                {
+                    var hostingEnvironment = controller.Get<HostingEnvironment>(KubeSetupProperty.HostingEnvironment);
+
+                    if (hostingEnvironment == HostingEnvironment.Wsl2)
+                    {
+                        var enterpriseHelper = NeonHelper.ServiceContainer.GetService<IEnterpriseHelper>();
+                        var dnsAddress = enterpriseHelper.GetWsl2Address().ToString();
+
+                        using (var jsonClient = new JsonClient())
+                        {
+                            var headendUri = new Uri(controller.Get<string>(KubeSetupProperty.HeadendUri));
+                            jsonClient.BaseAddress = headendUri;
+                            clusterLogin.ClusterDefinition.Domain = await jsonClient.GetAsync<string>($"/cluster/domain?ipAddress={dnsAddress}");
+                            clusterLogin.Save();
+                        }
+                    }
                 });
 
             // Some hosting managers may have to some additional work after
