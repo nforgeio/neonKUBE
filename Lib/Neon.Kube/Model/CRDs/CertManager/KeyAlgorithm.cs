d097d2c40a4a7ecbb5af185ec5c60be867dd2842
Marcus Bowyer <marcus@loopie.io>
2021-10-06 13:12:40 -0700

    certmanager CRDs

---
+++ Lib/Neon.Kube/Model/CRDs/CertManager/KeyAlgorithm.cs
@@ -0,0 +1,60 @@
+﻿//-----------------------------------------------------------------------------
+// FILE:	    KeyAlgorithm.cs
+// CONTRIBUTOR: Marcus Bowyer
+// COPYRIGHT:	Copyright (c) 2005-2021 by neonFORGE LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+using Neon.Common;
+using Neon.Net;
+
+namespace Neon.Kube
+{
+    /// <summary>
+    /// The private key algorithm of the corresponding private key for this certificate. If provided, 
+    /// allowed values are either `RSA` or `ECDSA` If `algorithm` is specified and `size` is not provided,
+    /// key size of 256 will be used for `ECDSA` key algorithm and key size of 2048 will be used for 
+    /// `RSA` key algorithm.
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum KeyAlgorithm
+    {
+        /// <summary>
+        /// RSA#1
+        /// </summary>
+        [EnumMember(Value = "RSA")]
+        RSA = 0,
+
+        /// <summary>
+        /// ECDSA#8
+        /// </summary>
+        [EnumMember(Value = "ECDSA")]
+        ECDSA
+    }
+}
