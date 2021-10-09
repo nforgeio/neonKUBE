637e5132a28ac847f831296e7bd196cacda99947
Jeff Lill <jeff@lilltek.com>
2021-10-09 13:50:41 -0700

    KubernetesFixture: in progress

--- Lib/Neon.Kube.Xunit/KubernetesFixture.cs
+++ Lib/Neon.Kube.Xunit/KubernetesFixture.cs
@@ -30,15 +30,67 @@ using System.IO;
 namespace Neon.Kube.Xunit
 {
     /// <summary>
-    /// Fixture for testing against Kubernetes clusters including optionally
-    /// deploying a neonKUBE cluster.
+    /// Fixture for testing against an existing Kubernetes cluster as well as 
+    /// a neonKUBE cluster deployed by the fixture.
     /// </summary>
     /// <remarks>
+    /// <note>
     /// <para>
+    /// <b>IMPORTANT:</b> The base Neon <see cref="TestFixture"/> implementation <b>DOES NOT</b>
+    /// support parallel test execution because fixtures may impact global machine state
+    /// like starting a Docker container, modifying the local DNS <b>hosts</b> file, or 
+    /// configuring a test database.
+    /// </para>
+    /// <para>
+    /// You should explicitly disable parallel execution in all test assemblies that
+    /// rely on test fixtures by adding a C# file called <c>AssemblyInfo.cs</c> with:
+    /// </para>
+    /// <code language="csharp">
+    /// [assembly: CollectionBehavior(DisableTestParallelization = true, MaxParallelThreads = 1)]
+    /// </code>
+    /// <para>
+    /// and then define your test classes like:
+    /// </para>
+    /// <code language="csharp">
+    /// public class MyTests
+    /// {
+    ///     [Collection(TestCollection.NonParallel)]
+    ///     [CollectionDefinition(TestCollection.NonParallel, DisableParallelization = true)]
+    ///     [Fact]
+    ///     public void Test()
+    ///     {
+    ///     }
+    /// }
+    /// </code>
+    /// </note>
+    /// <para>
+    /// This fixture can be used to run tests against any existing Kubernetes cluster
+    /// as well as a new neonKUBE cluster deployed by the fixture.  The idea here is
+    /// that you'll have your unit test inherit from 
+    /// </para>
+    /// <para>
+    /// <b>To connect to an existing cluster</b>, you'll need to call one of the <see cref="Connect(K8SConfiguration, string, string)"/>,
+    /// <see cref="Connect(KubernetesClientConfiguration)"/>, or <see cref="Connect(string, string, string)"/>
+    /// methods to connect to an existing cluster within the constructor.
+    /// </para>
+    /// <para>
+    /// <b>To deploy a temporary neonKUBE cluster</b>, you'll need to call one of
+    /// the <see cref="Deploy(ClusterDefinition)"/>, <see cref="Deploy(FileInfo)"/>, or
+    /// <see cref="Deploy(string)"/> methods within the constructor to provision
+    /// and setup the cluster using the specified cluster definition.  The <see cref="ClusterDefinition"/>
+    /// property will be set in this case.
+    /// </para>
+    /// <para>
+    /// The <b>Connect()</b> and <b>Deploy()</b> methods return <see cref="TestFixtureStatus.Started"/>
+    /// the first time one of these methods have been called on a fixture instance or after a
+    /// <see cref="Reset"/> call or <see cref="TestFixtureStatus.AlreadyRunning"/> when the
+    /// fixture is already managing a cluster.
     /// </para>
     /// </remarks>
     public class KubernetesFixture : TestFixture
     {
+        private bool        deployed = false;
+
         /// <summary>
         /// Constructor.
         /// </summary>
@@ -68,38 +120,97 @@ namespace Neon.Kube.Xunit
         /// <param name="kubeconfigPath">Optionally specifies a specific kubeconfig file.</param>
         /// <param name="currentContext">Optionally overrides the current context.</param>
         /// <param name="masterUrl">Optionally overrides the URI for the API server endpoint.</param>
-        /// <returns>The connected <see cref="Kubernetes"/> client.  This will also be available from <see cref="Client"/>.</returns>
-        public Kubernetes Connect(string kubeconfigPath = null, string currentContext = null, string masterUrl = null)
+        /// <returns>
+        /// <see cref="TestFixtureStatus.Started"/> the first time one of the <b>Connect()</b> methods have been called 
+        /// on a fixture instance or after a <see cref="Reset"/> call or <see cref="TestFixtureStatus.AlreadyRunning"/>
+        /// when the fixture is already managing a cluster.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown a <b>Deploy()</b> method has already been called on the fixture.  This fixture
+        /// does not support mixing <b>Connect()</b> and <b>Deploy()</b> calls.
+        /// </exception>
+        public TestFixtureStatus Connect(string kubeconfigPath = null, string currentContext = null, string masterUrl = null)
         {
-            return Client = new Kubernetes(KubernetesClientConfiguration.BuildConfigFromConfigFile(kubeconfigPath, currentContext, masterUrl));
+            if (Client != null)
+            {
+                if (deployed)
+                {
+                    throw new InvalidOperationException("[Deploy()] has already been called on this fixture.");
+                }
+
+                return TestFixtureStatus.AlreadyRunning;
+            }
+
+            Client = new Kubernetes(KubernetesClientConfiguration.BuildConfigFromConfigFile(kubeconfigPath, currentContext, masterUrl));
+
+            return TestFixtureStatus.Started;
         }
 
         /// <summary>
-        /// Connects the Kubernetes cluster specified by <see cref="KubernetesClientConfiguration"/>.  You 
-        /// override the current context and API server endpoint using the remaining optional parameters.
+        /// Connects the Kubernetes cluster specified by <see cref="KubernetesClientConfiguration"/>.
         /// </summary>
         /// <param name="kubeconfig">Specifies the <see cref="KubernetesClientConfiguration"/>.</param>
-        /// <returns>The connected <see cref="Kubernetes"/> client.  This will also be available from <see cref="Client"/>.</returns>
-        public Kubernetes Connect(KubernetesClientConfiguration kubeconfig)
+        /// <returns>
+        /// <see cref="TestFixtureStatus.Started"/> the first time one of the <b>Connect()</b> methods have been called 
+        /// on a fixture instance or after a <see cref="Reset"/> call or <see cref="TestFixtureStatus.AlreadyRunning"/>
+        /// when the fixture is already managing a cluster.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown a <b>Deploy()</b> method has already been called on the fixture.  This fixture
+        /// does not support mixing <b>Connect()</b> and <b>Deploy()</b> calls.
+        /// </exception>
+        public TestFixtureStatus Connect(KubernetesClientConfiguration kubeconfig)
         {
             Covenant.Requires<ArgumentNullException>(kubeconfig != null, nameof(kubeconfig));
 
-            return Client = new Kubernetes(kubeconfig);
+            if (Client != null)
+            {
+                if (deployed)
+                {
+                    throw new InvalidOperationException("[Deploy()] has already been called on this fixture.");
+                }
+
+                return TestFixtureStatus.AlreadyRunning;
+            }
+
+            Client = new Kubernetes(kubeconfig);
+
+            return TestFixtureStatus.Started;
         }
 
         /// <summary>
-        /// Connects the Kubernetes cluster specified by <see cref="K8SConfiguration"/>.  You  override the current  
+        /// Connects the Kubernetes cluster specified by <see cref="K8SConfiguration"/>.  You can override the current  
         /// context and API server endpoint using the remaining optional parameters.
         /// </summary>
         /// <param name="k8sConfig">The configuration.</param>
         /// <param name="currentContext">Optionally overrides the current context.</param>
         /// <param name="masterUrl">Optionally overrides the URI for the API server endpoint.</param>
-        /// <returns>The connected <see cref="Kubernetes"/> client.  This will also be available from <see cref="Client"/>.</returns>
-        public Kubernetes Connect(K8SConfiguration k8sConfig, string currentContext = null, string masterUrl = null)
+        /// <returns>
+        /// <see cref="TestFixtureStatus.Started"/> the first time one of the <b>Connect()</b> methods have been called 
+        /// on a fixture instance or after a <see cref="Reset"/> call or <see cref="TestFixtureStatus.AlreadyRunning"/>
+        /// when the fixture is already managing a cluster.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown a <b>Deploy()</b> method has already been called on the fixture.  This fixture
+        /// does not support mixing <b>Connect()</b> and <b>Deploy()</b> calls.
+        /// </exception>
+        public TestFixtureStatus Connect(K8SConfiguration k8sConfig, string currentContext = null, string masterUrl = null)
         {
             Covenant.Requires<ArgumentNullException>(k8sConfig != null, nameof(k8sConfig));
 
-            return Client = new Kubernetes(KubernetesClientConfiguration.BuildConfigFromConfigObject(k8sConfig, currentContext, masterUrl));
+            if (Client != null)
+            {
+                if (deployed)
+                {
+                    throw new InvalidOperationException("[Deploy()] has already been called on this fixture.");
+                }
+
+                return TestFixtureStatus.AlreadyRunning;
+            }
+
+            Client = new Kubernetes(KubernetesClientConfiguration.BuildConfigFromConfigObject(k8sConfig, currentContext, masterUrl));
+
+            return TestFixtureStatus.Started;
         }
 
         /// <summary>
@@ -107,11 +218,32 @@ namespace Neon.Kube.Xunit
         /// </summary>
         /// <param name="clusterDefinition">The cluster definition model.</param>
         /// <returns>The connected <see cref="Kubernetes"/> client.  This will also be available from <see cref="Client"/>.</returns>
-        public Kubernetes Deploy(ClusterDefinition clusterDefinition)
+        /// <returns>
+        /// <see cref="TestFixtureStatus.Started"/> the first time one of the <b>Delpoy()</b> methods have been called 
+        /// on a fixture instance or after a <see cref="Reset"/> call or <see cref="TestFixtureStatus.AlreadyRunning"/>
+        /// when the fixture is already managing a cluster.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown a <b>Connect()</b> method has already been called on the fixture.  This fixture
+        /// does not support mixing <b>Connect()</b> and <b>Deploy()</b> calls.
+        /// </exception>
+        public TestFixtureStatus Deploy(ClusterDefinition clusterDefinition)
         {
             Covenant.Requires<ArgumentNullException>(clusterDefinition != null, nameof(clusterDefinition));
 
+            if (Client != null)
+            {
+                if (!deployed)
+                {
+                    throw new InvalidOperationException("[Connect()] has already been called on this fixture.");
+                }
+
+                return TestFixtureStatus.AlreadyRunning;
+            }
+
             throw new NotImplementedException();
+
+            return TestFixtureStatus.Started;
         }
 
         /// <summary>
@@ -119,11 +251,32 @@ namespace Neon.Kube.Xunit
         /// </summary>
         /// <param name="clusterDefinitionYaml">The cluster definition YAML.</param>
         /// <returns>The connected <see cref="Kubernetes"/> client.  This will also be available from <see cref="Client"/>.</returns>
-        public Kubernetes Deploy(string clusterDefinitionYaml)
+        /// <returns>
+        /// <see cref="TestFixtureStatus.Started"/> the first time one of the <b>Delpoy()</b> methods have been called 
+        /// on a fixture instance or after a <see cref="Reset"/> call or <see cref="TestFixtureStatus.AlreadyRunning"/>
+        /// when the fixture is already managing a cluster.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown a <b>Connect()</b> method has already been called on the fixture.  This fixture
+        /// does not support mixing <b>Connect()</b> and <b>Deploy()</b> calls.
+        /// </exception>
+        public TestFixtureStatus Deploy(string clusterDefinitionYaml)
         {
             Covenant.Requires<ArgumentNullException>(clusterDefinitionYaml != null, nameof(clusterDefinitionYaml));
 
+            if (Client != null)
+            {
+                if (!deployed)
+                {
+                    throw new InvalidOperationException("[Connect()] has already been called on this fixture.");
+                }
+
+                return TestFixtureStatus.AlreadyRunning;
+            }
+
             throw new NotImplementedException();
+
+            return TestFixtureStatus.Started;
         }
 
         /// <summary>
@@ -131,22 +284,60 @@ namespace Neon.Kube.Xunit
         /// </summary>
         /// <param name="clusterDefinitionPath">Path to the cluster definition YAML file.</param>
         /// <returns>The connected <see cref="Kubernetes"/> client.  This will also be available from <see cref="Client"/>.</returns>
-        public Kubernetes Deploy(FileInfo clusterDefinitionPath)
+        /// <returns>
+        /// <see cref="TestFixtureStatus.Started"/> the first time one of the <b>Delpoy()</b> methods have been called 
+        /// on a fixture instance or after a <see cref="Reset"/> call or <see cref="TestFixtureStatus.AlreadyRunning"/>
+        /// when the fixture is already managing a cluster.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// Thrown a <b>Connect()</b> method has already been called on the fixture.  This fixture
+        /// does not support mixing <b>Connect()</b> and <b>Deploy()</b> calls.
+        /// </exception>
+        public TestFixtureStatus Deploy(FileInfo clusterDefinitionPath)
         {
             Covenant.Requires<ArgumentNullException>(clusterDefinitionPath != null, nameof(clusterDefinitionPath));
 
+            if (Client != null)
+            {
+                if (!deployed)
+                {
+                    throw new InvalidOperationException("[Connect()] has already been called on this fixture.");
+                }
+
+                return TestFixtureStatus.AlreadyRunning;
+            }
+
             throw new NotImplementedException();
+
+            return TestFixtureStatus.Started;
         }
 
         /// <summary>
-        /// Returns the <see cref="Kubernetes"/> client instance that can be used to
-        /// control the attached cluster.
+        /// Returns the standard <see cref="Kubernetes"/> client instance that can be used to
+        /// manage the attached cluster.  This property is set when a cluster is connected or
+        /// deployed.
         /// </summary>
         public Kubernetes Client { get; private set; }
+
+        /// <summary>
+        /// Returns the cluster definition for cluster deployed by this fixture or <c>null</c>
+        /// when the fixture just connected to the cluster.
+        /// </summary>
+        public ClusterDefinition ClusterDefinition { get; private set; }
 
         /// <inheritdoc/>
         public override void Reset()
         {
+            if (deployed)
+            {
+                // 1. Stop and remove the cluster
+                // 2. Remove the temporary cluster folder
+            }
+
+            deployed          = false;
+            Client            = null;
+            ClusterDefinition = null;
+
             base.Reset();
         }
     }
