08334ca50d1dc859f509b0ec813014d4e6f491c9
jefflill <jeff@lill.io>
2022-01-15 22:07:42 -0800

    [cluster-setup]: Add the ContainerRegistry resources from the cluster definition.

--- Lib/Neon.Kube.Setup/KubeSetup.Operations.cs
+++ Lib/Neon.Kube.Setup/KubeSetup.Operations.cs
@@ -4111,9 +4111,7 @@ $@"- name: StorageType
                         clusterRegistry.Spec.Username    = registry.Username;
                         clusterRegistry.Spec.Password    = registry.Password;
 
-                        // $todo(jefflill): FINISH THIS!
-
-                        // await k8s.UpsertClusterCustomObjectAsync(clusterRegistry, registry.Name);
+                        await k8s.UpsertClusterCustomObjectAsync(clusterRegistry, registry.Name);
                     }
                 });
         }
