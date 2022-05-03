4f849ab5c2dec32ec3b2d2e3cb7e51b75d6a22cc
jefflill <jeff@lill.io>
2022-05-03 10:39:47 -0700

    [build]: fixed warning

--- Lib/Neon.Common/Retry/IRetryPolicy.cs
+++ Lib/Neon.Common/Retry/IRetryPolicy.cs
@@ -15,6 +15,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#pragma warning disable CS0067 // Event is never used
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
