abf3691817302f97ad35f1b04da341491ea3d723
Marcus Bowyer <marcus@loopie.io>
2022-06-07 18:19:28 -0700

    added some data to the home page for not simon

--- Services/neon-dashboard/AppState/AppState.Kube.cs
+++ Services/neon-dashboard/AppState/AppState.Kube.cs
@@ -42,6 +42,53 @@ namespace NeonDashboard
         public class __Kube : AppStateBase
         {
             /// <summary>
+            /// Event action for updates to Kube properties.
+            /// </summary>
+            public event Action OnChange;
+            private void NotifyStateChanged() => OnChange?.Invoke();
+
+            /// <summary>
+            /// The total number of nodes in the current cluster.
+            /// </summary>
+            public int TotalNodes { get; private set; }
+
+            /// <summary>
+            /// The total number of active nodes in the cluster.
+            /// </summary>
+            public int ActiveNodes { get; private set; }
+
+            /// <summary>
+            /// The number of failed nodes in the cluster.
+            /// </summary>
+            public int FailedNodes { get; private set; }
+
+            /// <summary>
+            /// The date that the cluster was created.
+            /// </summary>
+            public DateTime CreationTimestamp { get; private set; }
+
+            /// <summary>
+            /// The list of nodes. This contains node related metadata.
+            /// </summary>
+            public V1NodeList Nodes;
+
+            private static List<string> negativeNodeConditions = new List<string>()
+            {
+                "KubeletUnhealthy",
+                "ContainerRuntimeUnhealthy",
+                "KernelDeadlock",
+                "NetworkUnavailable",
+                "MemoryPressure",
+                "DiskPressure",
+                "PIDPressure",
+            };
+
+            private static List<string> positiveNodeConditions = new List<string>()
+            {
+                "Ready"
+            };            
+            
+            /// <summary>
             /// Constructor.
             /// </summary>
             /// <param name="state"></param>
@@ -50,7 +97,7 @@ namespace NeonDashboard
             {
             }
 
-            public async Task<V1NodeList> GetNodesAsync()
+            public async Task GetNodesStatusAsync()
             {
                 var key = "nodes";
 
@@ -59,7 +106,7 @@ namespace NeonDashboard
                     var value = await Cache.GetAsync<V1NodeList>(key);
                     if (value != null)
                     {
-                        return value;
+                        Nodes = value;
                     }
                 }
                 catch (Exception e)
@@ -67,11 +114,13 @@ namespace NeonDashboard
                     Logger.LogError(e);
                 }
 
-                var nodes = await K8s.ListNodeAsync();
+                Nodes = await K8s.ListNodeAsync();
 
-                _ = Cache.SetAsync("nodes", nodes);
+                TotalNodes = Nodes.Items.Count();
+                FailedNodes = Nodes.Items.Where(node => node.Status.Conditions.Any(condition => negativeNodeConditions.Contains(condition.Type) && condition.Status == "True")).Count();
+                ActiveNodes = Nodes.Items.Where(node => node.Status.Conditions.Any(condition => condition.Type == "Ready" && condition.Status == "True")).Count();
 
-                return nodes;
+                NotifyStateChanged();
             }
 
             public async Task<NodeMetricsList> GetNodeMetricsAsync()
@@ -94,6 +143,8 @@ namespace NeonDashboard
                 var nodeMetricsList = await K8s.GetKubernetesNodesMetricsAsync();
 
                 _ = Cache.SetAsync(key, nodeMetricsList);
+
+                NotifyStateChanged();
 
                 return nodeMetricsList;
             }
