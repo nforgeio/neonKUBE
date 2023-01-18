10a9ba84f51280350573ef7acb6d2df0c848d562
Jeff Lill <jeff@lilltek.com>
2023-01-18 12:47:57 -0800

    [refactor]: Neon.Deployment.MaintainerProfileClient --> MaintainerProfile

--- Tools/neon-cli/Commands/Cluster/ClusterSetupCommand.cs
+++ Tools/neon-cli/Commands/Cluster/ClusterSetupCommand.cs
@@ -155,7 +155,7 @@ OPTIONS:
             // We need to inject an implementation for [PreprocessReader] so it will be able to
             // perform the lookups.
 
-            NeonHelper.ServiceContainer.AddSingleton<IProfileClient>(new MaintainerProfileClient());
+            NeonHelper.ServiceContainer.AddSingleton<IProfileClient>(new MaintainerProfile());
 
             var contextName       = KubeContextName.Parse(commandLine.Arguments[0]);
             var kubeCluster       = KubeHelper.Config.GetCluster(contextName.Cluster);
