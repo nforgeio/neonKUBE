6789c070ad5395e93302ea72a6d476e47e6078b3
Jeff Lill <jeff@lilltek.com>
2023-04-15 19:10:09 -0700

    [formatting]

--- Tools/operator-cli/Commands/Generate/GenerateRbacCommand.cs
+++ Tools/operator-cli/Commands/Generate/GenerateRbacCommand.cs
@@ -19,18 +19,16 @@ using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.NamingConventionBinder;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Neon.Kube.Operator.Rbac;
-
 using k8s;
-using Neon.Kube.Operator;
-using System.IO;
 using k8s.Models;
-using Neon.Common;
-using System.Runtime.InteropServices;
+
+using Neon.Kube.Operator.Rbac;
+using Neon.Kube.Operator;
 
 namespace OperatorCli.Commands.Generate
 {
@@ -43,59 +41,58 @@ namespace OperatorCli.Commands.Generate
             this.AddOption(new Option<string>(new[] { "--assembly-path" })
             {
                 Description = "The assembly path.",
-                IsRequired = false,
-                Name = "AssemblyPath"
+                IsRequired  = false,
+                Name        = "AssemblyPath"
             });
 
             this.AddOption(new Option<string>(new[] { "--output-path" })
             {
                 Description = "The output path.",
-                IsRequired = false,
-                Name = "OutputPath"
+                IsRequired  = false,
+                Name        = "OutputPath"
             });
 
             this.AddOption(new Option<string>(new[] { "--deployed-namespace" })
             {
                 Description = "The namespace that the operator is deployed.",
-                IsRequired = false,
-                Name = "DeployedNamespace"
+                IsRequired  = false,
+                Name        = "DeployedNamespace"
             });
 
             this.AddOption(new Option<string>(new[] { "--watch-namespace" })
             {
                 Description = "The namespace that the operator should watch in csv format.",
-                IsRequired = false,
-                Name = "WatchNamespace"
+                IsRequired  = false,
+                Name        = "WatchNamespace"
             });
 
             this.AddOption(new Option<string>(new[] { "--name" })
             {
                 Description = "The operator name.",
-                IsRequired = false,
-                Name = "Name"
+                IsRequired  = false,
+                Name        = "Name"
             });
 
             this.AddOption(new Option<bool>(new[] { "--cert-manager-enabled" })
             {
                 Description = "enable cert manager.",
-                IsRequired = false,
-                Name = "CertManagerEnabled"
+                IsRequired  = false,
+                Name        = "CertManagerEnabled"
             });
 
             this.AddOption(new Option<string>(new[] { "--manage-custom-resource-definitions" })
             {
                 Description = "Manage custom resources.",
-                IsRequired = false,
-                Name = "ManageCustomResourceDefinitions"
+                IsRequired  = false,
+                Name        = "ManageCustomResourceDefinitions"
             });
 
             this.AddOption(new Option<bool>(new[] { "--assembly-scanning-enabled" })
             {
                 Description = "enable assembly scanning.",
-                IsRequired = false,
-                Name = "AssemblyScanningEnabled"
+                IsRequired  = false,
+                Name        = "AssemblyScanningEnabled"
             });
-
         }
 
         private int HandleCommand(GenerateRbacCommandArgs args)
@@ -118,23 +115,26 @@ namespace OperatorCli.Commands.Generate
                 WatchNamespace     = args.WatchNamespace,
                 
             };
-            var rbac = new RbacBuilder(assemblyPath, operatorSettings);
+
+            var rbac          = new RbacBuilder(assemblyPath, operatorSettings);
+            var consoleOutput = new StringBuilder();
+            
             rbac.Build();
 
-            var consoleOutput = new StringBuilder();
 
             foreach (var sa in rbac.ServiceAccounts)
             {
                 var saString = KubernetesYaml.Serialize(sa);
+
                 consoleOutput.AppendLine("---");
                 consoleOutput.AppendLine(saString);
-
                 File.WriteAllText($"{outputPath}/serviceaccount-{sa.Name()}-{sa.Namespace()}.yaml", saString);
             }
 
             foreach (var cr in rbac.ClusterRoles)
             {
                 var crString = KubernetesYaml.Serialize(cr);
+
                 consoleOutput.AppendLine("---");
                 consoleOutput.AppendLine(crString);
 
@@ -144,6 +144,7 @@ namespace OperatorCli.Commands.Generate
             foreach (var crb in rbac.ClusterRoleBindings)
             {
                 var crbString = KubernetesYaml.Serialize(crb);
+
                 consoleOutput.AppendLine("---");
                 consoleOutput.AppendLine(crbString);
 
@@ -153,18 +154,18 @@ namespace OperatorCli.Commands.Generate
             foreach (var r in rbac.Roles)
             {
                 var rString = KubernetesYaml.Serialize(r);
+
                 consoleOutput.AppendLine("---");
                 consoleOutput.AppendLine(rString);
-
                 File.WriteAllText($"{outputPath}/role-{r.Name()}-{r.Namespace()}.yaml", rString);
             }
 
             foreach (var rb in rbac.RoleBindings)
             {
                 var rbString = KubernetesYaml.Serialize(rb);
+
                 consoleOutput.AppendLine("---");
                 consoleOutput.AppendLine(rbString);
-
                 File.WriteAllText($"{outputPath}/rolebinding-{rb.Name()}-{rb.Namespace()}.yaml", rbString);
             }
 
@@ -181,7 +182,6 @@ namespace OperatorCli.Commands.Generate
             public bool CertManagerEnabled { get; set; } = true;
             public bool ManageCustomResourceDefinitions { get; set; } = true;
             public bool AssemblyScanningEnabled { get; set; } = true;
-
         }
     }
 }
