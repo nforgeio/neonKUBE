c1451557443927c1ab930a5fb115ddd5342637d6
Jeff Lill <jeff@lilltek.com>
2023-04-19 15:23:48 -0700

    [build]: moved debug build properties from operator-cli.csproj to Directory.Build.props and standardize on:
    Optimize = false
    DebugSymbols = true
    DebugType = embedded

--- Tools/operator-cli/Program.cs
+++ Tools/operator-cli/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------------
+// FILE:	    Program.cs
+// CONTRIBUTOR: Marcus Bowyer
+// COPYRIGHT:   Copyright © 2005-2023 by NEONFORGE LLC.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.CommandLine;
 using System.CommandLine.Builder;
@@ -24,38 +41,29 @@ using Neon.Tasks;
 using k8s;
 using k8s.Models;
 
-using Prometheus;
 using OperatorCli.Commands.Generate;
+using Prometheus;
 
 namespace OperatorCli
 {
     /// <summary>
-    /// Program entry
+    /// Hosts the program entrypoint.
     /// </summary>
     public static class Program
     {
-
         /// <summary>
-        /// Main.
+        /// The program entry point.
         /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public static async Task Main(string[] args)
+        /// <param name="args">Specifiees the command line argument.</param>
+        public static void Main(string[] args)
         {
-            // Build the commands from what's registered in the DI container.
-            await SyncContext.Clear;
-            var rootCommand = new RootCommand();
-
-            var generateCommand = new GenerateCommand();
-
-            rootCommand.AddCommand(generateCommand);
-
+            var rootCommand         = new RootCommand();
+            var generateCommand     = new GenerateCommand();
             var generateRbacCommand = new GenerateRbacCommand();
-
-            generateCommand.AddCommand(generateRbacCommand);
-
             var generateCrdsCommand = new GenerateCrdsCommand();
 
+            rootCommand.AddCommand(generateCommand);
+            generateCommand.AddCommand(generateRbacCommand);
             generateCommand.AddCommand(generateCrdsCommand);
 
             var commandLineBuilder = new CommandLineBuilder(rootCommand);
