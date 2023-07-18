0155cd38af75490da2313ad23941ce8b622439ec
Jeff Lill <jeff@lilltek.com>
2023-07-17 18:53:39 -0700

    bump NEONKUBE version: 0.10.0-beta

--- Lib/Neon.Kube/KubeVersions.cs
+++ Lib/Neon.Kube/KubeVersions.cs
@@ -136,7 +136,7 @@ namespace Neon.Kube
         /// caused by differing per-developer version numbers.
         /// </note>
         /// </remarks>
-        public const string NeonKube = "0.9.2-alpha";
+        public const string NeonKube = "0.10.0-beta";
 
         /// <summary>
         /// Returns the branch part of the NEONKUBE version.  This will be blank for release
