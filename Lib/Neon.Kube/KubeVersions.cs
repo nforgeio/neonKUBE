511d7421edee58d6fd667be8c1865a1f2ce0d0da
Jeff Lill <jeff@lilltek.com>
2023-08-01 14:00:49 -0700

    NEONKUBE: revert version to: 0.10.0-beta.1

--- Lib/Neon.Kube/KubeVersions.cs
+++ Lib/Neon.Kube/KubeVersions.cs
@@ -136,7 +136,7 @@ namespace Neon.Kube
         /// caused by differing per-developer version numbers.
         /// </note>
         /// </remarks>
-        public const string NeonKube = "0.10.0-beta.2";
+        public const string NeonKube = "0.10.0-beta.1";
 
         /// <summary>
         /// Returns the branch part of the NEONKUBE version.  This will be blank for release
