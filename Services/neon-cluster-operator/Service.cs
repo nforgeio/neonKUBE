c384ce258f662685f90c03dee3c6a46b81c30490
Jeff Lill <jeff@lilltek.com>
2024-04-22 17:37:17 -0700

    [merge]

--- Services/neon-cluster-operator/Service.cs
+++ Services/neon-cluster-operator/Service.cs
@@ -120,7 +120,7 @@ namespace NeonClusterOperator
     /// </remarks>
     [RbacRule<V1ConfigMap>(Verbs = RbacVerb.All, Scope = EntityScope.Cluster)]
     [RbacRule<V1Secret>(Verbs = RbacVerb.All, Scope = EntityScope.Cluster)]
-    [RbacRule<V1Pod>(Verbs = RbacVerb.List, Scope = EntityScope.Namespaced)]
+    [RbacRule<V1Pod>(Verbs = RbacVerb.List, Scope = EntityScope.Cluster)]
     public partial class Service : NeonService
     {
         private const int dexPort = 5557;
