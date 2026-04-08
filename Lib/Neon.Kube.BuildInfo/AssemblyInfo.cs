ef4fc5c5004b737b097a9a4eb524761de3373bac
Jeff Lill <jeff@lilltek.com>
2026-04-08 09:25:12 -0700

    Archive upgrade changes

--- Lib/Neon.Kube.BuildInfo/AssemblyInfo.cs
+++ Lib/Neon.Kube.BuildInfo/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// FILE:        ThisAssembly.cs
+// FILE:        AssemblyInfo.cs
 // CONTRIBUTOR: Jeff Lill
 // COPYRIGHT:   Copyright © 2005-2025 by NEONFORGE LLC.  All rights reserved.
 //
